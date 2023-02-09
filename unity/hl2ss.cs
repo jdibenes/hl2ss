6ffc3e5c40a5cb159ae0c60dce6f109e3c0c45d2
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-02-08 23:21:56 -0500

    Update hl2ss.cs

--- unity/hl2ss.cs
+++ unity/hl2ss.cs
@@ -78,8 +78,11 @@ public class hl2ss : MonoBehaviour
     [Tooltip("Enable Spatial Input stream. Allowed only if InitializeStreams is called from the cpp code and must be disabled otherwise.")]
     public bool enableSI = false;
 
-    [Tooltip("Enable Remoce Configuration. Has no effect if InitializeStreams is called from the cpp code.")]
+    [Tooltip("Enable Remote Configuration interface. Has no effect if InitializeStreams is called from the cpp code.")]
     public bool enableRC = true;
+
+    [Tooltip("Enable Spatial Mapping interface. Has no effect if InitializeStreams is called from the cpp code.")]
+    public bool enableSM = true;
 
     [Tooltip("Set to BasicMaterial to support semi-transparent primitives.")]
     public Material m_material;
@@ -98,7 +101,7 @@ public class hl2ss : MonoBehaviour
         m_loop = false;
         m_mode = false;
 
-        if (!skipInitialization) { InitializeStreams((enableRM ? 1U : 0U) | (enablePV ? 2U : 0U) | (enableMC ? 4U : 0U) | (enableRC ? 16U : 0U)); }
+        if (!skipInitialization) { InitializeStreams((enableRM ? 1U : 0U) | (enablePV ? 2U : 0U) | (enableMC ? 4U : 0U) | (enableRC ? 16U : 0U) | (enableSM ? 32U : 0U)); }
 
         byte[] ipaddress = new byte[16 * 2];
         GetLocalIPv4Address(ipaddress, ipaddress.Length);
