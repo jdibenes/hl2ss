3efd02c5947de32d3e4294f7546f8625a440a2f5
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-04-29 16:20:27 -0400

    Update client_rm_imu.py

--- viewer/client_rm_imu.py
+++ viewer/client_rm_imu.py
@@ -1,12 +1,13 @@
 #------------------------------------------------------------------------------
 # This script receives IMU samples from the HoloLens and prints them.
 # Sensor details:
-# Accelerometer:  93 samples per frame, sample rate ~1100 Hz
-# Gyroscope:     315 samples per frame, sample rate ~6550 Hz
-# Magnetometer:   11 samples per frame, sample rate   ~50 Hz
+# Accelerometer:  93 samples per frame, sample rate ~1100 Hz effective ~12 Hz
+# Gyroscope:     315 samples per frame, sample rate ~7500 Hz effective ~24 Hz
+# Magnetometer:   11 samples per frame, sample rate   ~50 Hz effective  ~5 Hz
 # The streams support three operating modes: 0) samples, 1) samples + rig pose,
 # 2) query calibration (single transfer), except for the magnetometer stream
-# which does not support mode 2. Press esc to stop.
+# which does not support mode 2.
+# Press esc to stop.
 #------------------------------------------------------------------------------
 
 from pynput import keyboard
@@ -43,6 +44,7 @@ mode = hl2ss.StreamMode.MODE_1
 if (mode == hl2ss.StreamMode.MODE_2):
     data = hl2ss.download_calibration_rm_imu(host, port)
     print('Calibration data')
+    print('Extrinsics')
     print(data.extrinsics)
     quit()
 
@@ -61,12 +63,13 @@ client.open()
 
 while (enable):
     data = client.get_next_packet()
-    print('Pose at time {ts}'.format(ts=data.timestamp))
+    print(f'Pose at time {data.timestamp}')
     print(data.pose)
     imu_data = hl2ss.unpack_rm_imu(data.payload)
     count = imu_data.get_count()
     sample = imu_data.get_frame(0)
-    print(f'Got {count} samples at time {data.timestamp}, first sample is (ticks = {sample.vinyl_hup_ticks} | {sample.soc_ticks}, x = {sample.x}, y = {sample.y}, z = {sample.z}, temperature={sample.temperature})')
+    print(f'Got {count} samples at time {data.timestamp}')
+    print(f'First sample is (ticks = {sample.vinyl_hup_ticks} | {sample.soc_ticks}, x = {sample.x}, y = {sample.y}, z = {sample.z}, temperature={sample.temperature})')
 
 client.close()
 listener.join()
