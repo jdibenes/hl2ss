2fe16fdfafd1d0b64af5c71510a04edc342ebfda
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-04-29 16:36:45 -0400

    Update client_si.py

--- viewer/client_si.py
+++ viewer/client_si.py
@@ -1,8 +1,6 @@
 #------------------------------------------------------------------------------
 # This script receives spatial input data from the HoloLens, which comprises:
-# 1) Head pose, 2) Eye ray, 3) Hand tracking. The HoloLens sends this data at
-# display framerate (60 Hz). If the display framerate drops, so does this
-# stream.
+# 1) Head pose, 2) Eye ray, 3) Hand tracking. Sample rate is 30 Hz.
 #------------------------------------------------------------------------------
 
 import hl2ss
@@ -23,14 +21,14 @@ client.open()
 data = client.get_next_packet()
 si = hl2ss.unpack_si(data.payload)
 
-print('Tracking status at time {ts}'.format(ts=data.timestamp))
+print(f'Tracking status at time {data.timestamp}')
 
 if (si.is_valid_head_pose()):
     head_pose = si.get_head_pose()
     print('Head pose')
-    print(head_pose.position)
-    print(head_pose.forward)
-    print(head_pose.up)
+    print(f'Position: {head_pose.position}')
+    print(f'Forward: {head_pose.forward}')
+    print(f'Up: {head_pose.up}')
     # right = cross(up, -forward)
     # up => y, forward => -z, right => x
 else:
@@ -39,19 +37,23 @@ else:
 if (si.is_valid_eye_ray()):
     eye_ray = si.get_eye_ray()
     print('Eye ray')
-    print(eye_ray.origin)
-    print(eye_ray.direction)
+    print(f'Origin: {eye_ray.origin}')
+    print(f'Direction: {eye_ray.direction}')
 else:
     print('No eye tracking data')
+
+# See
+# https://learn.microsoft.com/en-us/uwp/api/windows.perception.people.jointpose?view=winrt-22621
+# for hand data details
 
 if (si.is_valid_hand_left()):
     hand_left = si.get_hand_left()
     pose = hand_left.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
     print('Left wrist pose')
-    print(pose.orientation)
-    print(pose.position)
-    print(pose.radius)
-    print(pose.accuracy)
+    print(f'Orientation: {pose.orientation}') # Quaternion: x, y, z, w
+    print(f'Position: {pose.position}')
+    print(f'Radius: {pose.radius}')
+    print(f'Accuracy: {pose.accuracy}') # 0: High, 1: Approximate
 else:
     print('No left hand data')
 
@@ -59,10 +61,10 @@ if (si.is_valid_hand_right()):
     hand_right = si.get_hand_right()
     pose = hand_right.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
     print('Right wrist pose')
-    print(pose.orientation)
-    print(pose.position)
-    print(pose.radius)
-    print(pose.accuracy)
+    print(f'Orientation: {pose.orientation}')  # Quaternion: x, y, z, w
+    print(f'Position: {pose.position}')
+    print(f'Radius: {pose.radius}')
+    print(f'Accuracy: {pose.accuracy}')  # 0: High, 1: Approximate
 else:
     print('No right hand data')
 
