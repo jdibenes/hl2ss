5c7b807d243e39874303f92f073bef348a348bd1
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-04-30 18:04:25 -0400

    Update sample_pv_si.py

--- viewer/sample_pv_si.py
+++ viewer/sample_pv_si.py
@@ -1,9 +1,7 @@
 #------------------------------------------------------------------------------
 # This script receives video frames and spatial input data from the HoloLens.
-# It projects the received hand joint positions onto the video frame. First,
-# the intrinsics of the camera are obtained from a mode 2 transfer. Then, the 
-# video stream is set to mode 1 to include the camera pose. The camera pose of
-# the frame is used to project the hand joints. The streams are independent.
+# The received head pointer, hand joint positions and gaze point are projected
+# onto the video frame.
 # Press esc to stop.
 #------------------------------------------------------------------------------
 
@@ -39,6 +37,7 @@ bitrate = hl2ss.get_video_codec_bitrate(width, height, framerate, hl2ss.get_vide
 
 # Marker properties
 radius = 5
+head_color = (  0,   0, 255)
 hand_color = (  0, 255,   0)
 gaze_color = (255,   0, 255)
 thickness = -1
@@ -46,17 +45,22 @@ thickness = -1
 # Buffer length in seconds
 buffer_length = 5
 
+# Spatial Mapping manager settings
+triangles_per_cubic_meter = 1000
+mesh_threads = 2
+sphere_center = [0, 0, 0]
+sphere_radius = 5
+
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    enable = True
-
-    def project_points(image, P, points, radius, color, thickness):
+    def project_points(image, points, P, radius, color, thickness):
         for x, y in hl2ss_3dcv.project(points, P):
-            ix = int(x)
-            iy = int(y)
+            ix, iy = int(x), int(y)
             if (ix >= 0 and iy >= 0 and ix < image.shape[1] and iy < image.shape[0]):
                 cv2.circle(image, (ix, iy), radius, color, thickness)
+
+    enable = True
 
     def on_press(key):
         global enable
@@ -67,13 +71,12 @@ if __name__ == '__main__':
     listener.start()
 
     volumes = hl2ss.sm_bounding_volume()
-    volumes.add_sphere([0, 0, 0], 5)
+    volumes.add_sphere(sphere_center, sphere_radius)
 
-    sm_manager = hl2ss_sa.sm_mp_manager(host, 1000, 2)
+    sm_manager = hl2ss_sa.sm_mp_manager(host, triangles_per_cubic_meter, mesh_threads)
     sm_manager.open()
     sm_manager.set_volumes(volumes)
     sm_manager.get_observed_surfaces()
-    #sm_manager.close()
     
     hl2ss.start_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO)
 
@@ -92,14 +95,8 @@ if __name__ == '__main__':
     sink_pv.get_attach_response()
     sink_si.get_attach_response()
 
-    pv_intrinsics = hl2ss.create_pv_intrinsics_placeholder()
-    image = np.zeros((height, width, 3), dtype=np.uint8)
-
     while (enable):
         sm_manager.get_observed_surfaces()
-
-        cv2.imshow('Video', image)
-        cv2.waitKey(1)
 
         sink_pv.acquire()
 
@@ -112,36 +109,38 @@ if __name__ == '__main__':
             continue
 
         image = data_pv.payload.image
+        si = hl2ss.unpack_si(data_si.payload)
 
-        pv_intrinsics = hl2ss.update_pv_intrinsics(pv_intrinsics, data_pv.payload.focal_length, data_pv.payload.principal_point)
+        pv_intrinsics = hl2ss.create_pv_intrinsics(data_pv.payload.focal_length, data_pv.payload.principal_point)
         pv_extrinsics = np.eye(4, 4, dtype=np.float32)
         pv_intrinsics, pv_extrinsics = hl2ss_3dcv.pv_fix_calibration(pv_intrinsics, pv_extrinsics)
 
-        projection = hl2ss_3dcv.world_to_reference(data_pv.pose) @ hl2ss_3dcv.rignode_to_camera(pv_extrinsics) @ hl2ss_3dcv.camera_to_image(pv_intrinsics)
-        si = hl2ss.unpack_si(data_si.payload)
+        world_to_image = hl2ss_3dcv.world_to_reference(data_pv.pose) @ hl2ss_3dcv.rignode_to_camera(pv_extrinsics) @ hl2ss_3dcv.camera_to_image(pv_intrinsics)
+
+        if (si.is_valid_head_pose()):
+            head_pose = si.get_head_pose()
+            d = sm_manager.cast_rays(hl2ss_utilities.si_ray_to_vector(head_pose.position, head_pose.forward))
+            if (np.isfinite(d)):
+                head_point = hl2ss_utilities.si_ray_to_point(head_pose.position, head_pose.forward, d)
+                project_points(image, head_point, world_to_image, radius, head_color, thickness)
+
         if (si.is_valid_hand_left()):
-            project_points(image, projection, hl2ss_utilities.si_unpack_hand(si.get_hand_left()).positions, radius, hand_color, thickness)
-        if (si.is_valid_hand_right()):
-            project_points(image, projection, hl2ss_utilities.si_unpack_hand(si.get_hand_right()).positions, radius, hand_color, thickness)
+            left_points = hl2ss_utilities.si_unpack_hand(si.get_hand_left()).positions
+            project_points(image, left_points, world_to_image, radius, hand_color, thickness)
         
-        if (not si.is_valid_eye_ray()):
-            continue
+        if (si.is_valid_hand_right()):
+            right_points = hl2ss_utilities.si_unpack_hand(si.get_hand_right()).positions
+            project_points(image, right_points, world_to_image, radius, hand_color, thickness)
 
-        ray = si.get_eye_ray()
-        ray = np.hstack((ray.origin.reshape((1, -1)), ray.direction.reshape((1, -1))))
+        if (si.is_valid_eye_ray()):
+            eye_ray = si.get_eye_ray()
+            d = sm_manager.cast_rays(hl2ss_utilities.si_ray_to_vector(eye_ray.origin, eye_ray.direction))
+            if (np.isfinite(d)):
+                gaze_point = hl2ss_utilities.si_ray_to_point(eye_ray.origin, eye_ray.direction, d)
+                project_points(image, gaze_point, world_to_image, radius, gaze_color, thickness)
 
-        d = sm_manager.cast_rays(ray)
-        if (np.isinf(d)):
-            continue
-
-        point = ray[0, 0:3] + d*ray[0, 3:6]
-        image_point = hl2ss_3dcv.project(point, projection)
-        x = int(image_point[0])
-        y = int(image_point[1])
-        if (x < 0 or y < 0 or x >= width or y >= height):
-            continue
-
-        image = cv2.circle(image, (x, y), radius, gaze_color, thickness)
+        cv2.imshow('Video', image)
+        cv2.waitKey(1)
         
     sm_manager.close()
 
@@ -152,3 +151,4 @@ if __name__ == '__main__':
     listener.join()
 
     hl2ss.stop_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO)
+
