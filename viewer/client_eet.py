9ba1024c5bafbdac6a66d7427457fffa14beb6de
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-04-30 20:36:06 -0400

    update clients

--- viewer/client_eet.py
+++ viewer/client_eet.py
@@ -1,6 +1,10 @@
 #------------------------------------------------------------------------------
-# This script receives extended eye tracking data from the HoloLens.
+# This script receives extended eye tracking data from the HoloLens and prints
+# it.
+# Press esc to stop.
 #------------------------------------------------------------------------------
+
+from pynput import keyboard
 
 import hl2ss
 
@@ -18,27 +22,39 @@ fps = 90
 
 #------------------------------------------------------------------------------
 
+enable = True
+
+def on_press(key):
+    global enable
+    enable = key != keyboard.Key.esc
+    return enable
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
 client = hl2ss.rx_eet(host, port, hl2ss.ChunkSize.EXTENDED_EYE_TRACKER, fps)
 client.open()
 
-data = client.get_next_packet()
-eet = hl2ss.unpack_eet(data.payload)
+while (enable):
+    data = client.get_next_packet()
+    eet = hl2ss.unpack_eet(data.payload)
 
-# See
-# https://learn.microsoft.com/en-us/windows/mixed-reality/develop/native/extended-eye-tracking-native
-# for details
+    # See
+    # https://learn.microsoft.com/en-us/windows/mixed-reality/develop/native/extended-eye-tracking-native
+    # for details
 
-print(f'Tracking status at time {data.timestamp}')
-print('Pose')
-print(data.pose)
-print(f'Calibration valid: {eet.calibration_valid}')
-print(f'Combined eye gaze: Valid={eet.combined_ray_valid} Origin={eet.combined_ray.origin} Direction={eet.combined_ray.direction}')
-print(f'Left eye gaze: Valid={eet.left_ray_valid} Origin={eet.left_ray.origin} Direction={eet.left_ray.direction}')
-print(f'Right eye gaze: Valid={eet.right_ray_valid} Origin={eet.right_ray.origin} Direction={eet.right_ray.direction}')
+    print(f'Tracking status at time {data.timestamp}')
+    print('Pose')
+    print(data.pose)
+    print(f'Calibration valid: {eet.calibration_valid}')
+    print(f'Combined eye gaze: Valid={eet.combined_ray_valid} Origin={eet.combined_ray.origin} Direction={eet.combined_ray.direction}')
+    print(f'Left eye gaze: Valid={eet.left_ray_valid} Origin={eet.left_ray.origin} Direction={eet.left_ray.direction}')
+    print(f'Right eye gaze: Valid={eet.right_ray_valid} Origin={eet.right_ray.origin} Direction={eet.right_ray.direction}')
 
-# "...not supported by HoloLens 2 at this time"
-print(f'Left eye openness: Valid={eet.left_openness_valid} Value={eet.left_openness}')
-print(f'Right eye openness: Valid={eet.right_openness_valid} Value={eet.right_openness}')
-print(f'Vergence distance: Valid={eet.vergence_distance_valid} Value={eet.vergence_distance}')
+    # "...not supported by HoloLens 2 at this time"
+    print(f'Left eye openness: Valid={eet.left_openness_valid} Value={eet.left_openness}')
+    print(f'Right eye openness: Valid={eet.right_openness_valid} Value={eet.right_openness}')
+    print(f'Vergence distance: Valid={eet.vergence_distance_valid} Value={eet.vergence_distance}')
 
 client.close()
+listener.join()
--- viewer/client_si.py
+++ viewer/client_si.py
@@ -1,7 +1,10 @@
 #------------------------------------------------------------------------------
 # This script receives spatial input data from the HoloLens, which comprises:
-# 1) Head pose, 2) Eye ray, 3) Hand tracking. Sample rate is 30 Hz.
+# 1) Head pose, 2) Eye ray, 3) Hand tracking, and prints it. 30 Hz sample rate.
+# Press esc to stop.
 #------------------------------------------------------------------------------
+
+from pynput import keyboard
 
 import hl2ss
 
@@ -15,57 +18,69 @@ port = hl2ss.StreamPort.SPATIAL_INPUT
 
 #------------------------------------------------------------------------------
 
+enable = True
+
+def on_press(key):
+    global enable
+    enable = key != keyboard.Key.esc
+    return enable
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
 client = hl2ss.rx_si(host, port, hl2ss.ChunkSize.SPATIAL_INPUT)
 client.open()
 
-data = client.get_next_packet()
-si = hl2ss.unpack_si(data.payload)
+while (enable):
+    data = client.get_next_packet()
+    si = hl2ss.unpack_si(data.payload)
 
-print(f'Tracking status at time {data.timestamp}')
+    print(f'Tracking status at time {data.timestamp}')
 
-if (si.is_valid_head_pose()):
-    head_pose = si.get_head_pose()
-    print('Head pose')
-    print(f'Position: {head_pose.position}')
-    print(f'Forward: {head_pose.forward}')
-    print(f'Up: {head_pose.up}')
-    # right = cross(up, -forward)
-    # up => y, forward => -z, right => x
-else:
-    print('No head pose data')
+    if (si.is_valid_head_pose()):
+        head_pose = si.get_head_pose()
+        print('Head pose')
+        print(f'Position: {head_pose.position}')
+        print(f'Forward: {head_pose.forward}')
+        print(f'Up: {head_pose.up}')
+        # right = cross(up, -forward)
+        # up => y, forward => -z, right => x
+    else:
+        print('No head pose data')
 
-if (si.is_valid_eye_ray()):
-    eye_ray = si.get_eye_ray()
-    print('Eye ray')
-    print(f'Origin: {eye_ray.origin}')
-    print(f'Direction: {eye_ray.direction}')
-else:
-    print('No eye tracking data')
+    if (si.is_valid_eye_ray()):
+        eye_ray = si.get_eye_ray()
+        print('Eye ray')
+        print(f'Origin: {eye_ray.origin}')
+        print(f'Direction: {eye_ray.direction}')
+    else:
+        print('No eye tracking data')
 
-# See
-# https://learn.microsoft.com/en-us/uwp/api/windows.perception.people.jointpose?view=winrt-22621
-# for hand data details
+    # See
+    # https://learn.microsoft.com/en-us/uwp/api/windows.perception.people.jointpose?view=winrt-22621
+    # for hand data details
 
-if (si.is_valid_hand_left()):
-    hand_left = si.get_hand_left()
-    pose = hand_left.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
-    print('Left wrist pose')
-    print(f'Orientation: {pose.orientation}') # Quaternion: x, y, z, w
-    print(f'Position: {pose.position}')
-    print(f'Radius: {pose.radius}')
-    print(f'Accuracy: {pose.accuracy}') # 0: High, 1: Approximate
-else:
-    print('No left hand data')
+    if (si.is_valid_hand_left()):
+        hand_left = si.get_hand_left()
+        pose = hand_left.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
+        print('Left wrist pose')
+        print(f'Orientation: {pose.orientation}') # Quaternion: x, y, z, w
+        print(f'Position: {pose.position}')
+        print(f'Radius: {pose.radius}')
+        print(f'Accuracy: {pose.accuracy}') # 0: High, 1: Approximate
+    else:
+        print('No left hand data')
 
-if (si.is_valid_hand_right()):
-    hand_right = si.get_hand_right()
-    pose = hand_right.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
-    print('Right wrist pose')
-    print(f'Orientation: {pose.orientation}')  # Quaternion: x, y, z, w
-    print(f'Position: {pose.position}')
-    print(f'Radius: {pose.radius}')
-    print(f'Accuracy: {pose.accuracy}')  # 0: High, 1: Approximate
-else:
-    print('No right hand data')
+    if (si.is_valid_hand_right()):
+        hand_right = si.get_hand_right()
+        pose = hand_right.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
+        print('Right wrist pose')
+        print(f'Orientation: {pose.orientation}')  # Quaternion: x, y, z, w
+        print(f'Position: {pose.position}')
+        print(f'Radius: {pose.radius}')
+        print(f'Accuracy: {pose.accuracy}')  # 0: High, 1: Approximate
+    else:
+        print('No right hand data')
 
 client.close()
+listener.join()
