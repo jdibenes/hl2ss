623577fc2a7c6d4f725b93de25676cb3c2ac1aef
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-04-30 20:03:30 -0400

    Update client_eet.py

--- viewer/client_eet.py
+++ viewer/client_eet.py
@@ -22,7 +22,7 @@ client = hl2ss.rx_eet(host, port, hl2ss.ChunkSize.EXTENDED_EYE_TRACKER, fps)
 client.open()
 
 data = client.get_next_packet()
-data.payload = hl2ss.unpack_eet(data.payload)
+eet = hl2ss.unpack_eet(data.payload)
 
 # See
 # https://learn.microsoft.com/en-us/windows/mixed-reality/develop/native/extended-eye-tracking-native
@@ -31,14 +31,14 @@ data.payload = hl2ss.unpack_eet(data.payload)
 print(f'Tracking status at time {data.timestamp}')
 print('Pose')
 print(data.pose)
-print(f'Calibration valid: {data.payload.calibration_valid}')
-print(f'Combined eye gaze: Valid={data.payload.combined_ray_valid} Origin={data.payload.combined_ray.origin} Direction={data.payload.combined_ray.direction}')
-print(f'Left eye gaze: Valid={data.payload.left_ray_valid} Origin={data.payload.left_ray.origin} Direction={data.payload.left_ray.direction}')
-print(f'Right eye gaze: Valid={data.payload.right_ray_valid} Origin={data.payload.right_ray.origin} Direction={data.payload.right_ray.direction}')
+print(f'Calibration valid: {eet.calibration_valid}')
+print(f'Combined eye gaze: Valid={eet.combined_ray_valid} Origin={eet.combined_ray.origin} Direction={eet.combined_ray.direction}')
+print(f'Left eye gaze: Valid={eet.left_ray_valid} Origin={eet.left_ray.origin} Direction={eet.left_ray.direction}')
+print(f'Right eye gaze: Valid={eet.right_ray_valid} Origin={eet.right_ray.origin} Direction={eet.right_ray.direction}')
 
 # "...not supported by HoloLens 2 at this time"
-print(f'Left eye openness: Valid={data.payload.left_openness_valid} Value={data.payload.left_openness}')
-print(f'Right eye openness: Valid={data.payload.right_openness_valid} Value={data.payload.right_openness}')
-print(f'Vergence distance: Valid={data.payload.vergence_distance_valid} Value={data.payload.vergence_distance}')
+print(f'Left eye openness: Valid={eet.left_openness_valid} Value={eet.left_openness}')
+print(f'Right eye openness: Valid={eet.right_openness_valid} Value={eet.right_openness}')
+print(f'Vergence distance: Valid={eet.vergence_distance_valid} Value={eet.vergence_distance}')
 
 client.close()
