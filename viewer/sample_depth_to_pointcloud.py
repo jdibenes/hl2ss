7fcf09eee24cb95437f0607c65746aae94888c7f
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-05-02 19:07:22 -0400

    Update sample_depth_to_pointcloud.py

--- viewer/sample_depth_to_pointcloud.py
+++ viewer/sample_depth_to_pointcloud.py
@@ -12,7 +12,7 @@ import hl2ss_3dcv
 # HoloLens address
 host = '192.168.1.7'
 
-# Calibration folder
+# Calibration folder (must exist but can be empty)
 calibration_path = '../calibration'
 
 # Max depth in meters
@@ -20,29 +20,25 @@ max_depth = 3.0
 
 #------------------------------------------------------------------------------
 
-# Get camera calibration
-
+# Get camera calibration ------------------------------------------------------
+# Calibration data will be downloaded if it's not in the calibration folder
 calibration = hl2ss_3dcv.get_calibration_rm(host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, calibration_path)
-
-# Compute depth-to-rgb registration constants
-
 xy1, scale = hl2ss_3dcv.rm_depth_compute_rays(calibration.uv2xy, calibration.scale)
 
-# Get single depth image
-
+# Get single depth image ------------------------------------------------------
 rx_depth = hl2ss.rx_decoded_rm_depth_longthrow(host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, hl2ss.StreamMode.MODE_0, hl2ss.PngFilterMode.Paeth)
 rx_depth.open()
 data = rx_depth.get_next_packet()
 rx_depth.close()
 
-# Convert depth to 3D points
-
+# Convert depth to 3D points --------------------------------------------------
 depth = hl2ss_3dcv.rm_depth_normalize(data.payload.depth, scale)
 xyz = hl2ss_3dcv.rm_depth_to_points(depth, xy1)
 xyz = hl2ss_3dcv.block_to_list(xyz)
-xyz = xyz[(xyz[:, 2] > 0) & (xyz[:, 2] < max_depth), :] 
+d = hl2ss_3dcv.block_to_list(depth).reshape((-1,))
+xyz = xyz[(d > 0) & (d < max_depth), :]
 
+# Visualize pointcloud --------------------------------------------------------
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(xyz)
-
 o3d.visualization.draw_geometries([pcd])
