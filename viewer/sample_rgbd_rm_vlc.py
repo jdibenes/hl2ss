c9f5e841ababcdf60aa8f3c092094f7c6785553f
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-05-03 01:03:59 -0400

    Update sample_rgbd_rm_vlc.py

--- viewer/sample_rgbd_rm_vlc.py
+++ viewer/sample_rgbd_rm_vlc.py
@@ -1,14 +1,17 @@
 #------------------------------------------------------------------------------
-# This script demonstrates how to create aligned "rgbd" images, which can be
+# This script demonstrates how to create aligned "RGBD" images, which can be
 # used with Open3D, from the depth and grayscale cameras of the HoloLens.
 #------------------------------------------------------------------------------
 
+from pynput import keyboard
+
+import multiprocessing as mp
 import numpy as np
 import open3d as o3d
 import cv2
 import hl2ss_imshow
 import hl2ss
-import hl2ss_utilities
+import hl2ss_mp
 import hl2ss_3dcv
 
 #------------------------------------------------------------------------------
@@ -16,78 +19,136 @@ import hl2ss_3dcv
 # HoloLens address
 host = '192.168.1.7'
 
-# Grayscale camera port
+# Calibration path (must exist but can be empty)
+calibration_path = '../calibration'
+
+# Port
 port = hl2ss.StreamPort.RM_VLC_RIGHTFRONT
 
 # Video encoding profile
-profile = hl2ss.VideoProfile.H264_BASE
+profile = hl2ss.VideoProfile.H265_MAIN
 
 # Encoded stream average bits per second
 # Must be > 0
-bitrate = 2*1024*1024
+bitrate = 1*1024*1024
 
-# Calibration folder
-calibration_path = '../calibration'
+# Buffer length in seconds
+buffer_length = 10
 
 # Max depth in meters
 max_depth = 3.0 
 
 #------------------------------------------------------------------------------
 
-# Get camera calibrations
+if __name__ == '__main__':
+    # Keyboard events ---------------------------------------------------------
+    enable = True
 
-calibration_vlc = hl2ss_3dcv.get_calibration_rm(host, port, calibration_path)
-calibration_lt = hl2ss_3dcv.get_calibration_rm(host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, calibration_path)
+    def on_press(key):
+        global enable
+        enable = key != keyboard.Key.space
+        return enable
 
-# Get single depth and pv images
-# To keep this example simple, the images are captured one after the other but this will not work properly for dynamic scenes
-# Use the multiprocessing producer to obtain image pairs that are closest in time
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
 
-client = hl2ss.rx_decoded_rm_depth_longthrow(host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, hl2ss.StreamMode.MODE_1, hl2ss.PngFilterMode.Paeth)
-client.open()
-data_lt = client.get_next_packet()
-client.close()
+    # Get RM VLC and RM Depth Long Throw calibration --------------------------
+    # Calibration data will be downloaded if it's not in the calibration folder
+    calibration_vlc = hl2ss_3dcv.get_calibration_rm(host, port, calibration_path)
+    calibration_lt = hl2ss_3dcv.get_calibration_rm(host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, calibration_path)
 
-client = hl2ss.rx_decoded_rm_vlc(host, port, hl2ss.ChunkSize.RM_VLC, hl2ss.StreamMode.MODE_1, profile, bitrate)
-client.open()
-data_vlc = client.get_next_packet()
-client.close()
+    uv2xy = hl2ss_3dcv.compute_uv2xy(calibration_lt.intrinsics, hl2ss.Parameters_RM_DEPTH_LONGTHROW.WIDTH, hl2ss.Parameters_RM_DEPTH_LONGTHROW.HEIGHT)
+    xy1, scale = hl2ss_3dcv.rm_depth_compute_rays(uv2xy, calibration_lt.scale)
+    
+    # Create Open3D integrator and visualizer ---------------------------------
+    o3d_lt_intrinsics = o3d.camera.PinholeCameraIntrinsic(hl2ss.Parameters_RM_DEPTH_LONGTHROW.WIDTH, hl2ss.Parameters_RM_DEPTH_LONGTHROW.HEIGHT, calibration_lt.intrinsics[0, 0], calibration_lt.intrinsics[1, 1], calibration_lt.intrinsics[2, 0], calibration_lt.intrinsics[2, 1])
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    pcd = o3d.geometry.PointCloud()
+    first_pcd = True
 
-# Compute depth-to-rgb registration constants
+    # Start RM VLC and RM Depth Long Throw streams ----------------------------
+    producer = hl2ss_mp.producer()
+    producer.configure_rm_vlc(True, host, port, hl2ss.ChunkSize.RM_VLC, hl2ss.StreamMode.MODE_1, profile, bitrate)
+    producer.configure_rm_depth_longthrow(True, host, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, hl2ss.StreamMode.MODE_1, hl2ss.PngFilterMode.Paeth)
+    producer.initialize(port, hl2ss.Parameters_RM_VLC.FPS * buffer_length)
+    producer.initialize(hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.Parameters_RM_DEPTH_LONGTHROW.FPS * buffer_length)
+    producer.start(port)
+    producer.start(hl2ss.StreamPort.RM_DEPTH_LONGTHROW)
 
-uv2xy = hl2ss_3dcv.compute_uv2xy(calibration_lt.intrinsics, hl2ss.Parameters_RM_DEPTH_LONGTHROW.WIDTH, hl2ss.Parameters_RM_DEPTH_LONGTHROW.HEIGHT)
-xy1, scale = hl2ss_3dcv.rm_depth_compute_rays(uv2xy, calibration_lt.scale)
+    consumer = hl2ss_mp.consumer()
+    manager = mp.Manager()    
+    sink_vlc = consumer.create_sink(producer, port, manager, None)
+    sink_depth = consumer.create_sink(producer, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, manager, ...)
 
-# Generate RGBD pair
+    sink_vlc.get_attach_response()
+    sink_depth.get_attach_response()
 
-depth = hl2ss_3dcv.rm_depth_undistort(data_lt.payload.depth, calibration_lt.undistort_map)
-depth = hl2ss_3dcv.rm_depth_normalize(depth, scale)
-color = cv2.remap(data_vlc.payload, calibration_vlc.undistort_map[:, :, 0], calibration_vlc.undistort_map[:, :, 1], cv2.INTER_LINEAR)
+    # Main Loop ---------------------------------------------------------------
+    while (enable):
+        # Wait for RM Depth Long Throw frame ----------------------------------
+        sink_depth.acquire()
 
-lt_points         = hl2ss_3dcv.rm_depth_to_points(xy1, depth)
-lt_to_world       = hl2ss_3dcv.camera_to_rignode(calibration_lt.extrinsics) @ hl2ss_3dcv.reference_to_world(data_lt.pose)
-world_to_lt       = hl2ss_3dcv.world_to_reference(data_lt.pose) @ hl2ss_3dcv.rignode_to_camera(calibration_lt.extrinsics)
-world_to_pv_image = hl2ss_3dcv.world_to_reference(data_vlc.pose) @ hl2ss_3dcv.rignode_to_camera(calibration_vlc.extrinsics) @ hl2ss_3dcv.camera_to_image(calibration_vlc.intrinsics)
-world_points      = hl2ss_3dcv.transform(lt_points, lt_to_world)
-pv_uv             = hl2ss_3dcv.project(world_points, world_to_pv_image)
-color             = cv2.remap(color, pv_uv[:, :, 0], pv_uv[:, :, 1], cv2.INTER_LINEAR)
+        # Get RM Depth Long Throw frame and nearest (in time) RM VLC frame ----
+        _, data_depth = sink_depth.get_most_recent_frame()
+        if ((data_depth is None) or (not hl2ss.is_valid_pose(data_depth.pose))):
+            continue
 
-mask_uv = hl2ss_3dcv.slice_to_block((pv_uv[:, :, 0] < 0) | (pv_uv[:, :, 0] >= hl2ss.Parameters_RM_VLC.WIDTH) | (pv_uv[:, :, 1] < 0) | (pv_uv[:, :, 1] >= hl2ss.Parameters_RM_VLC.HEIGHT))
-depth[mask_uv] = 0
+        _, data_vlc = sink_vlc.get_nearest(data_depth.timestamp)
+        if ((data_vlc is None) or (not hl2ss.is_valid_pose(data_vlc.pose))):
+            continue
 
-# Show RGBD image
+        # Preprocess frames ---------------------------------------------------
+        depth = hl2ss_3dcv.rm_depth_undistort(data_depth.payload.depth, calibration_lt.undistort_map)
+        depth = hl2ss_3dcv.rm_depth_normalize(depth, scale)
+        color = cv2.remap(data_vlc.payload, calibration_vlc.undistort_map[:, :, 0], calibration_vlc.undistort_map[:, :, 1], cv2.INTER_LINEAR)
 
-image = np.hstack((depth[:, :, 0] / max_depth, color / 255)) # Depth scaled for visibility
-cv2.imshow('RGBD', image)
-cv2.waitKey(0)
+        # Generate aligned RGBD image -----------------------------------------
+        lt_points          = hl2ss_3dcv.rm_depth_to_points(xy1, depth)
+        lt_to_world        = hl2ss_3dcv.camera_to_rignode(calibration_lt.extrinsics) @ hl2ss_3dcv.reference_to_world(data_depth.pose)
+        world_to_lt        = hl2ss_3dcv.world_to_reference(data_depth.pose) @ hl2ss_3dcv.rignode_to_camera(calibration_lt.extrinsics)
+        world_to_vlc_image = hl2ss_3dcv.world_to_reference(data_vlc.pose) @ hl2ss_3dcv.rignode_to_camera(calibration_vlc.extrinsics) @ hl2ss_3dcv.camera_to_image(calibration_vlc.intrinsics)
+        world_points       = hl2ss_3dcv.transform(lt_points, lt_to_world)
+        vlc_uv             = hl2ss_3dcv.project(world_points, world_to_vlc_image)
+        color              = cv2.remap(color, vlc_uv[:, :, 0], vlc_uv[:, :, 1], cv2.INTER_LINEAR)
+        color              = hl2ss_3dcv.slice_to_block(color)
 
-# Create Open3D RGBD Image
+        mask_uv = hl2ss_3dcv.slice_to_block((vlc_uv[:, :, 0] < 0) | (vlc_uv[:, :, 0] >= hl2ss.Parameters_RM_VLC.WIDTH) | (vlc_uv[:, :, 1] < 0) | (vlc_uv[:, :, 1] >= hl2ss.Parameters_RM_VLC.HEIGHT))
+        depth[mask_uv] = 0
 
-o3d_rgb = o3d.geometry.Image(hl2ss_3dcv.rm_vlc_to_rgb(color))
-o3d_depth = o3d.geometry.Image(depth)
+        # Display RGBD --------------------------------------------------------
+        image = np.hstack((depth / 8, color / 255)) # Depth scaled for visibility
+        cv2.imshow('RGBD', image)
+        cv2.waitKey(1)
 
-rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(o3d_rgb, o3d_depth, depth_scale=1, depth_trunc=max_depth, convert_rgb_to_intensity=False)
-intrinsics = o3d.camera.PinholeCameraIntrinsic(hl2ss.Parameters_RM_DEPTH_LONGTHROW.WIDTH, hl2ss.Parameters_RM_DEPTH_LONGTHROW.HEIGHT, calibration_lt.intrinsics[0, 0], calibration_lt.intrinsics[1, 1], calibration_lt.intrinsics[2, 0], calibration_lt.intrinsics[2, 1])
+        # Convert to Open3D RGBD image ----------------------------------------
+        color = hl2ss_3dcv.rm_vlc_to_rgb(color)
 
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsics)
-o3d.visualization.draw_geometries([pcd])
+        color_image = o3d.geometry.Image(color)
+        depth_image = o3d.geometry.Image(depth)
+
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_image, depth_image, depth_scale=1, depth_trunc=max_depth, convert_rgb_to_intensity=False)
+
+        tmp_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, o3d_lt_intrinsics)
+
+        # Display pointcloud --------------------------------------------------
+        pcd.points = tmp_pcd.points
+        pcd.colors = tmp_pcd.colors
+
+        if (first_pcd):
+            vis.add_geometry(pcd)
+            first_pcd = False
+        else:
+            vis.update_geometry(pcd)
+
+        vis.poll_events()
+        vis.update_renderer()
+
+    # Stop RM VLC and RM Depth Long Throw streams -----------------------------
+    sink_vlc.detach()
+    sink_depth.detach()
+    producer.stop(port)
+    producer.stop(hl2ss.StreamPort.RM_DEPTH_LONGTHROW)
+
+    # Stop keyboard events ----------------------------------------------------
+    listener.join()
