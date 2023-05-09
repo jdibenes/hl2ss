d9ea7f09b0aff7b15cd57b619beda1d9179c280a
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-05-09 02:57:30 -0400

    Update simple_sequencer.py

--- viewer/simple_sequencer.py
+++ viewer/simple_sequencer.py
@@ -1,25 +1,51 @@
-
-# Sequencer example
+#------------------------------------------------------------------------------
+# Sequencer example. The sequencer associates data from two or more files.
+# Here, it is used to pair RM VLC LEFTFRONT and RIGHTFRONT frames with PV 
+# frames, all previously recorded using simple recorder.
+#------------------------------------------------------------------------------
 
 import cv2
 import hl2ss
 import hl2ss_io
 
-rd_pv = hl2ss_io.create_rd(True, './data/personal_video.bin', hl2ss.ChunkSize.SINGLE_TRANSFER, 'bgr24')
-rd_vlc = hl2ss_io.sequencer(True, './data/rm_vlc_leftfront.bin', hl2ss.ChunkSize.SINGLE_TRANSFER, None)
+# Settings --------------------------------------------------------------------
 
+# Directory containing the recorded data
+path = './data'
+
+#------------------------------------------------------------------------------
+
+# Create readers --------------------------------------------------------------
+rd_pv = hl2ss_io.create_rd(True, f'./data/{hl2ss.get_port_name(hl2ss.StreamPort.PERSONAL_VIDEO)}.bin', hl2ss.ChunkSize.SINGLE_TRANSFER, 'bgr24')
+rd_lf = hl2ss_io.sequencer(True, f'./data/{hl2ss.get_port_name(hl2ss.StreamPort.RM_VLC_LEFTFRONT)}.bin', hl2ss.ChunkSize.SINGLE_TRANSFER, None)
+rd_rf = hl2ss_io.sequencer(True, f'./data/{hl2ss.get_port_name(hl2ss.StreamPort.RM_VLC_RIGHTFRONT)}.bin', hl2ss.ChunkSize.SINGLE_TRANSFER, None)
+
+# Open readers ----------------------------------------------------------------
 rd_pv.open()
-rd_vlc.open()
+rd_lf.open()
+rd_rf.open()
 
+# Main loop -------------------------------------------------------------------
 while (True):
+    # Get PV frame ------------------------------------------------------------
     data_pv = rd_pv.read()
     if (data_pv is None):
         break
-    data_vlc = rd_vlc.read(data_pv.timestamp)
-    if (data_vlc is not None):
-        cv2.imshow('vlc', data_vlc.payload)
-    cv2.imshow('pv', data_pv.payload.image)
+
+    # Find RM VLC frames corresponding to the current PV frame ----------------
+    data_lf = rd_lf.read(data_pv.timestamp) # Get nearest (in time) lf frame
+    data_rf = rd_rf.read(data_pv.timestamp) # Get nearest (in time) rf frame
+
+    # Display frames ----------------------------------------------------------
+    if (data_lf is not None):
+        cv2.imshow('RM VLC LF', data_lf.payload)
+    if (data_rf is not None):
+        cv2.imshow('RM VLC RF', data_rf.payload)
+
+    cv2.imshow('PV', data_pv.payload.image)
     cv2.waitKey(1)
 
+# Close readers ---------------------------------------------------------------
 rd_pv.close()
-rd_vlc.close()
+rd_lf.close()
+rd_rf.close()
