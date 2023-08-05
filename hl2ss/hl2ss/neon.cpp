1e2df87bfdc01021cc42f8bb4defd371826ed288
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-05 03:47:12 -0400

    kernels: depth invalidate, u16 ab to u8 via sqrt narrowing

--- hl2ss/hl2ss/neon.cpp
+++ hl2ss/hl2ss/neon.cpp
@@ -8,11 +8,52 @@
 //-----------------------------------------------------------------------------
 
 // OK
+void Neon_ZHTInvalidate(u16 const* pDepth, u16 *pOut)
+{
+	uint16x8_t threshold = vdupq_n_u16(RM_ZHT_MASK);
+
+	for (int i = 0; i < (RM_ZHT_PIXELS / 32); ++i)
+	{
+	uint16x8x4_t d = vld1q_u16_x4(pDepth);
+
+	d.val[0] = vandq_u16(d.val[0], vcltq_u16(d.val[0], threshold));
+	d.val[1] = vandq_u16(d.val[1], vcltq_u16(d.val[1], threshold));
+	d.val[2] = vandq_u16(d.val[2], vcltq_u16(d.val[2], threshold));
+	d.val[3] = vandq_u16(d.val[3], vcltq_u16(d.val[3], threshold));
+
+	vst1q_u16_x4(pOut, d);
+
+	pDepth += 32;
+	pOut   += 32;
+	}
+}
+
+// OK
+void Neon_AbToNV12(u16 const* pAb, u8* pOut)
+{
+	for (int i = 0; i < (RM_ZHT_PIXELS / 32); ++i)
+	{
+	uint16x8x4_t d = vld1q_u16_x4(pAb);
+	uint8x8x4_t b;
+
+	b.val[0] = vmovn_u16(vcvtq_u16_f16(vsqrtq_f16(vcvtq_f16_u16(d.val[0]))));
+	b.val[1] = vmovn_u16(vcvtq_u16_f16(vsqrtq_f16(vcvtq_f16_u16(d.val[1]))));
+	b.val[2] = vmovn_u16(vcvtq_u16_f16(vsqrtq_f16(vcvtq_f16_u16(d.val[2]))));
+	b.val[3] = vmovn_u16(vcvtq_u16_f16(vsqrtq_f16(vcvtq_f16_u16(d.val[3]))));
+
+	vst1_u8_x4(pOut, b);
+
+	pAb  += 32;
+	pOut += 32;
+	}
+}
+
+// OK
 void Neon_ZHTToNV12(u16 const* pDepth, u16 const* pAb, u8* pNV12)
 {
 	uint16x8_t threshold = vdupq_n_u16(RM_ZHT_MASK);
 
-	for (int i = 0; i < ((RM_ZHT_HEIGHT * RM_ZHT_WIDTH) / 32); ++i)
+	for (int i = 0; i < (RM_ZHT_PIXELS / 32); ++i)
 	{
 	uint16x8x4_t d = vld1q_u16_x4(pDepth);
 	uint8x8x4_t  b;
@@ -51,7 +92,7 @@ void Neon_ZLTToBGRA8(u8 const* pSigma, u16 const* pDepth, u16 const* pAb, u32* p
 {
 	uint16x8_t threshold = vdupq_n_u16(RM_ZLT_MASK);
 
-	for (int i = 0; i < ((RM_ZLT_HEIGHT * RM_ZLT_WIDTH) / 32); ++i)
+	for (int i = 0; i < (RM_ZLT_PIXELS / 32); ++i)
 	{
 	uint8x8x4_t  s = vld1_u8_x4(pSigma);
 	uint16x8x4_t d = vld1q_u16_x4(pDepth);
@@ -68,5 +109,5 @@ void Neon_ZLTToBGRA8(u8 const* pSigma, u16 const* pDepth, u16 const* pAb, u32* p
 	pBGRA8 += 16;
 	}
 
-	memcpy(pBGRA8, pAb, RM_ZLT_HEIGHT * RM_ZLT_WIDTH * sizeof(u16));
+	memcpy(pBGRA8, pAb, RM_ZLT_ABSIZE);
 }
--- hl2ss/hl2ss/neon.h
+++ hl2ss/hl2ss/neon.h
@@ -3,5 +3,7 @@
 
 #include "types.h"
 
+void Neon_ZHTInvalidate(u16 const* pDepth, u16* pOut);
+void Neon_AbToNV12(u16 const* pAb, u8* pOut);
 void Neon_ZHTToNV12(u16 const* pDepth, u16 const* pAb, u8* pNV12);
 void Neon_ZLTToBGRA8(u8 const* pSigma, u16 const* pDepth, u16 const* pAb, u32* pBGRA8);
