ca15afa106c21a2bcc0d416c430895436ae10f3d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-05 03:44:42 -0400

    Update research_mode.h

--- hl2ss/hl2ss/research_mode.h
+++ hl2ss/hl2ss/research_mode.h
@@ -12,11 +12,17 @@ int const RM_ZHT_WIDTH  = 512;
 int const RM_ZHT_HEIGHT = 512;
 int const RM_ZHT_FPS    = 45;
 int const RM_ZHT_MASK   = 4090;
+int const RM_ZHT_PIXELS = RM_ZHT_WIDTH * RM_ZHT_HEIGHT;
+int const RM_ZHT_ZSIZE  = RM_ZHT_PIXELS * sizeof(uint16_t);
+int const RM_ZHT_ABSIZE = RM_ZHT_PIXELS * sizeof(uint16_t);
 
 int const RM_ZLT_WIDTH  = 320;
 int const RM_ZLT_HEIGHT = 288;
 int const RM_ZLT_FPS    = 5;
 int const RM_ZLT_MASK   = 128;
+int const RM_ZLT_PIXELS = RM_ZLT_HEIGHT * RM_ZLT_WIDTH;
+int const RM_ZLT_ZSIZE  = RM_ZLT_PIXELS * sizeof(uint16_t);
+int const RM_ZLT_ABSIZE = RM_ZLT_PIXELS * sizeof(uint16_t);
 
 bool ResearchMode_WaitForCameraConsent();
 bool ResearchMode_WaitForIMUConsent();
