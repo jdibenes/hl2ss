1d32dece9120e6415d335ccdb4028afaf4e53c6c
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-09 19:24:54 -0400

    Update research_mode.h

--- hl2ss/hl2ss/research_mode.h
+++ hl2ss/hl2ss/research_mode.h
@@ -7,6 +7,8 @@
 int const RM_VLC_WIDTH  = 640;
 int const RM_VLC_HEIGHT = 480;
 int const RM_VLC_FPS    = 30;
+int const RM_VLC_PIXELS = RM_VLC_WIDTH * RM_VLC_HEIGHT;
+int const RM_VLC_SIZE   = RM_VLC_PIXELS * sizeof(uint8_t);
 
 int const RM_ZHT_WIDTH  = 512;
 int const RM_ZHT_HEIGHT = 512;
@@ -23,6 +25,8 @@ int const RM_ZLT_MASK   = 128;
 int const RM_ZLT_PIXELS = RM_ZLT_HEIGHT * RM_ZLT_WIDTH;
 int const RM_ZLT_ZSIZE  = RM_ZLT_PIXELS * sizeof(uint16_t);
 int const RM_ZLT_ABSIZE = RM_ZLT_PIXELS * sizeof(uint16_t);
+
+uint8_t const NV12_ZERO_CHROMA = 0x80;
 
 bool ResearchMode_WaitForCameraConsent();
 bool ResearchMode_WaitForIMUConsent();
