7718293e608873f93d007ed352bbd6248a628141
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-09 21:08:31 -0400

    Update hl2ss_imshow.py

--- viewer/hl2ss_imshow.py
+++ viewer/hl2ss_imshow.py
@@ -5,6 +5,8 @@ import cv2
 #------------------------------------------------------------------------------
 # Extension: Workaround for the cv2/av imshow issue on some platforms (Ubuntu)
 #------------------------------------------------------------------------------
+# https://github.com/opencv/opencv/issues/21952
+# https://github.com/PyAV-Org/PyAV/issues/978
 
 try:
     cv2.namedWindow('_cv2_21952_av_978_workaround')
