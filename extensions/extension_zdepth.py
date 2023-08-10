45d772b22b9f4ae689883eabc33fed87b51c76f3
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-09 20:35:02 -0400

    Update extension_zdepth.py

--- extensions/extension_zdepth.py
+++ extensions/extension_zdepth.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 from glob import glob
 
-path_Zdepth = '../hl2ss/hl2ss/Zdepth'
-path_zstd = '../hl2ss/hl2ss/Zdepth/zstd'
+path_Zdepth = '../3rdparty/Zdepth'
+path_zstd   = '../3rdparty/Zdepth/zstd'
 
 Zdepth_cpp = [x for x in glob(f'{path_Zdepth}/src/*.cpp')]
-Zdepth_c = [x for x in glob(f'{path_Zdepth}/src/*.c')]
-zstd_cpp = [x for x in glob(f'{path_zstd}/src/*.cpp')]
-zstd_c = [x for x in glob(f'{path_zstd}/src/*.c')]
+Zdepth_c   = [x for x in glob(f'{path_Zdepth}/src/*.c')]
+zstd_cpp   = [x for x in glob(f'{path_zstd}/src/*.cpp')]
+zstd_c     = [x for x in glob(f'{path_zstd}/src/*.c')]
 
 sources = Zdepth_cpp + Zdepth_c + zstd_cpp + zstd_c + ['./pyzdepth.cpp']
 
