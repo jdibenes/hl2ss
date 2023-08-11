2c53bd22aa2f3190f770cb7c108343e2ccd42e25
jdibenes <58836755+jdibenes@users.noreply.github.com>
2023-08-10 22:45:04 -0400

    Update simple_player.py

--- viewer/simple_player.py
+++ viewer/simple_player.py
@@ -20,12 +20,12 @@ filename = os.path.join(path, f'{hl2ss.get_port_name(port)}.bin')
 
 # Stream type is detected automatically
 # The format parameter is only used for PV and is ignored for other types
-reader = hl2ss_io.create_rd(True, filename, hl2ss.ChunkSize.SINGLE_TRANSFER, 'bgr24')
+reader = hl2ss_io.create_rd(filename, hl2ss.ChunkSize.SINGLE_TRANSFER, 'bgr24')
 reader.open()
 
 # Display decoded contents ----------------------------------------------------
 while (True):
-    data = reader.read() # get_next_packet() equivalent
+    data = reader.get_next_packet() # get_next_packet() equivalent
     if (data is None):
         break # End of file
     
