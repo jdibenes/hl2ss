5c919a50c9dccb2082b9c492a326608c24c8feb0
jdibenes <jdibenes@stevens.edu>
2023-08-15 22:25:11 -0400

    Update hl2ss_redis.py

--- etc/bbn/hl2ss_redis.py
+++ etc/bbn/hl2ss_redis.py
@@ -1,96 +1,51 @@
 
-
-
-
-
-
-
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import asyncio
+import cv2
+import numpy as np
 import websockets.client
-
-
-
-
-
-
-
-
-
-
-
-# start stop pv
-    if (is_rs_host(host)):
-        return
-    
-# connect
- if (is_rs_host(host)):
-        c = _rs_gatherer()
-        c.open(host, port, None)
-
+import BBN_redis_frame_load as holoframe
+import hl2ss
 
 
 #//////////////////////////////////////////////////////////////////////////////
 # Extension: redis-streamer (NYU)
 #//////////////////////////////////////////////////////////////////////////////
 
-#------------------------------------------------------------------------------
-# GOP Tagging
-#------------------------------------------------------------------------------
-
-class _extension_gop:
-    def __init__(self, gop_size):
-        self.aliased_index = 0
-        self.gop_size = gop_size
-
-    def extend(self, data):
-        data.extend(struct.pack('<B', self.aliased_index))
-        self.aliased_index = (self.aliased_index + 1) % self.gop_size
-
-
-#------------------------------------------------------------------------------
-# API redis-streamer
-#------------------------------------------------------------------------------
-
-def is_rs_host(host):
-    return ':' in host
-
-
-def _rs_get_stream_url_push(host, port):
-    return f'ws://{host}/data/{get_port_name(port)}/push?header=0'
-
-
-def _rs_get_stream_url_pull(host, port):
-    return f'ws://{host}/data/{get_port_name(port)}/pull?header=0'
+REDIS_STREAMER_PORT = '8000'
 
 
 #------------------------------------------------------------------------------
 # Network Client (Websockets)
 #------------------------------------------------------------------------------
+
+class _rs_stream_name:
+    OF = {
+        hl2ss.StreamPort.RM_VLC_LEFTLEFT      : "gll",
+        hl2ss.StreamPort.RM_VLC_LEFTFRONT     : "glf",
+        hl2ss.StreamPort.RM_VLC_RIGHTFRONT    : "grf",
+        hl2ss.StreamPort.RM_VLC_RIGHTRIGHT    : "grr",
+        hl2ss.StreamPort.RM_DEPTH_LONGTHROW   : "depthlt",
+        hl2ss.StreamPort.RM_IMU_ACCELEROMETER : "imuaccel",
+        hl2ss.StreamPort.RM_IMU_GYROSCOPE     : "imugyro",
+        hl2ss.StreamPort.RM_IMU_MAGNETOMETER  : "imumag",
+        hl2ss.StreamPort.PERSONAL_VIDEO       : "main",
+        hl2ss.StreamPort.MICROPHONE           : "mic0",
+        hl2ss.StreamPort.SPATIAL_INPUT        : "si",
+        # EET
+    }
+
+
+def is_rs_host(host):
+    return ":" in host
+
+
+def _rs_get_stream_url_push(host, port):
+    return f"ws://{host}/data/{_rs_stream_name.OF[port]}/push?header=0"
+
+
+def _rs_get_stream_url_pull(host, port):
+    return f"ws://{host}/data/{_rs_stream_name.OF[port]}/pull?header=0&latest=1"
+
 
 class _rs_client:
     def open(self, host, port, max_size):
@@ -122,46 +77,180 @@ class _rs_gatherer:
         self._client.open(host, port, max_size)
 
     def _fetch(self):
-        data = self._client.recv()
-        raw_packet = data[:-1]
-        aliased_index = struct.unpack('<B', data[-1:])[0]
-        return (aliased_index, raw_packet)
-    
+        return self._client.recv()
+
     def get_next_packet(self):
-        aliased_index, data = self._fetch()
-        while (not self._genlock):
-            if (aliased_index == 0): 
-                self._genlock = True
-            else:
-                aliased_index, data = self._fetch()
-        return unpack_packet(data)
-    
+        data = self._fetch()
+        d = holoframe.load(data)
+        ft = d["frame_type"]
+
+        if ft == holoframe.SensorType.PV:
+            return hl2ss._packet(d["time"], hl2ss._PV_Frame(cv2.cvtColor(d["image"][:, :, ::-1], cv2.COLOR_RGB2BGR), [d["focalX"], d["focalY"]], [d["principalX"], d["principalY"]]), d["cam2world"].transpose())
+        if ft == holoframe.SensorType.GLF:
+            return hl2ss._packet(d["time"], cv2.rotate(d["image"], cv2.ROTATE_90_COUNTERCLOCKWISE), d["rig2world"].transpose())
+        if ft == holoframe.SensorType.GLL:
+            return hl2ss._packet(d["time"], cv2.rotate(d["image"], cv2.ROTATE_90_CLOCKWISE), d["rig2world"].transpose())
+        if ft == holoframe.SensorType.GRF:
+            return hl2ss._packet(d["time"], cv2.rotate(d["image"], cv2.ROTATE_90_CLOCKWISE), d["rig2world"].transpose())
+        if ft == holoframe.SensorType.GRR:
+            return hl2ss._packet(d["time"], cv2.rotate(d["image"], cv2.ROTATE_90_COUNTERCLOCKWISE), d["rig2world"].transpose())
+        if ft == holoframe.SensorType.DepthLT:
+            return hl2ss._packet(d["time"], hl2ss._RM_Depth_Frame(d["image"], d["infrared"].astype(np.uint16)), d["rig2world"].transpose())
+        if ft == holoframe.SensorType.Microphone:
+            return hl2ss._packet(d["time"], d["data"], None)
+        if ft == holoframe.SensorType.SpatialInput:
+            return hl2ss._packet(d["time"], d["data"], None)
+
     def close(self):
         self._client.close()
 
+
+#------------------------------------------------------------------------------
+# Mode 0 and Mode 1 Data Acquisition
+#------------------------------------------------------------------------------
+
+def _connect_client(host, port):
+    c = _rs_gatherer()
+    c.open(host, port, None)
+    return c
+
+
+def start_subsystem_pv(host, port):
+    pass
+
+
+def stop_subsystem_pv(host, port):
+    pass
+
+
+#------------------------------------------------------------------------------
+# Receiver Wrappers
+#------------------------------------------------------------------------------
+
+class _rx(hl2ss._context_manager):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def open(self):
+        self._client = _connect_client(self.host + ':' + REDIS_STREAMER_PORT, self.port)
         
+    def get_next_packet(self):
+        return self._client.get_next_packet()
 
-        def configure_rm_vlc(self, decoded, host, port, chunk, mode, divisor, profile, gop_size, bitrate):
-        self.configure(port, hl2ss.rx_decoded_rm_vlc(host, port, chunk, mode, divisor, profile, gop_size, bitrate) if (decoded) else hl2ss.rx_rm_vlc(host, port, chunk, mode, divisor, profile, gop_size, bitrate))
+    def close(self):
+        self._client.close()
 
-    def configure_rm_depth_ahat(self, decoded, host, port, chunk, mode, divisor, profile, gop_size, bitrate):
-        self.configure(port, hl2ss.rx_decoded_rm_depth_ahat(host, port, chunk, mode, divisor, profile, gop_size, bitrate) if (decoded) else hl2ss.rx_rm_depth_ahat(host, port, chunk, mode, divisor, profile, gop_size, bitrate))
-        
-    def configure_rm_depth_longthrow(self, decoded, host, port, chunk, mode, divisor, png_filter):
-        self.configure(port, hl2ss.rx_decoded_rm_depth_longthrow(host, port, chunk, mode, divisor, png_filter) if (decoded) else hl2ss.rx_rm_depth_longthrow(host, port, chunk, mode, divisor, png_filter))
 
-    def configure_rm_imu(self, host, port, chunk, mode):
-        self.configure(port, hl2ss.rx_rm_imu(host, port, chunk, mode))
+class _rx_rm_vlc(_rx):
+    def __init__(self, host, port, chunk, mode, divisor, profile, level, bitrate, options):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.mode = mode
+        self.divisor = divisor
+        self.profile = profile
+        self.level = level
+        self.bitrate = bitrate
+        self.options = options
 
-    def configure_pv(self, decoded, host, port, chunk, mode, width, height, framerate, divisor, profile, gop_size, bitrate, decoded_format):
-        self.configure(port, hl2ss.rx_decoded_pv(host, port, chunk, mode, width, height, framerate, divisor, profile, gop_size, bitrate, decoded_format) if (decoded) else hl2ss.rx_pv(host, port, chunk, mode, width, height, framerate, divisor, profile, gop_size, bitrate))
 
-    def configure_microphone(self, decoded, host, port, chunk, profile, level):
-        self.configure(port, hl2ss.rx_decoded_microphone(host, port, chunk, profile, level) if (decoded) else hl2ss.rx_microphone(host, port, chunk, profile, level))
+class _rx_rm_depth_ahat(_rx):
+    def __init__(self, host, port, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.mode = mode
+        self.divisor = divisor
+        self.profile_z = profile_z
+        self.profile_ab = profile_ab
+        self.level = level
+        self.bitrate = bitrate
+        self.options = options
 
-    def configure_si(self, host, port, chunk):
-        self.configure(port, hl2ss.rx_si(host, port, chunk))
 
-    def configure_eet(self, host, port, chunk, fps):
-        self.configure(port, hl2ss.rx_eet(host, port, chunk, fps))
-'''
+class _rx_rm_depth_longthrow(_rx):
+    def __init__(self, host, port, chunk, mode, divisor, png_filter):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.mode = mode
+        self.divisor = divisor
+        self.png_filter = png_filter
+
+
+class _rx_rm_imu(_rx):
+    def __init__(self, host, port, chunk, mode):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.mode = mode
+
+
+class _rx_pv(_rx):
+    def __init__(self, host, port, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.mode = mode
+        self.width = width
+        self.height = height
+        self.framerate = framerate
+        self.divisor = divisor
+        self.profile = profile
+        self.level = level
+        self.bitrate = bitrate
+        self.options = options
+
+
+class _rx_microphone(_rx):
+    def __init__(self, host, port, chunk, profile, level):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.profile = profile
+        self.level = level
+
+
+class _rx_si(_rx):
+    def __init__(self, host, port, chunk):
+        super().__init__(host, port)
+        self.chunk = chunk
+
+
+class _rx_eet(_rx):
+    def __init__(self, host, port, chunk, fps):
+        super().__init__(host, port)
+        self.chunk = chunk
+        self.fps = fps
+
+
+#------------------------------------------------------------------------------
+# Receivers
+#------------------------------------------------------------------------------
+
+def rx_rm_vlc(host, port, chunk=hl2ss.ChunkSize.RM_VLC, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile=hl2ss.VideoProfile.H264_HIGH, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+    return _rx_rm_vlc(host, port, chunk, mode, divisor, profile, level, bitrate, options)
+
+
+def rx_rm_depth_ahat(host, port, chunk=hl2ss.ChunkSize.RM_DEPTH_AHAT, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile_z=hl2ss.DepthProfile.SAME, profile_ab=hl2ss.VideoProfile.H264_HIGH, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+    return _rx_rm_depth_ahat(host, port, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options)
+
+
+def rx_rm_depth_longthrow(host, port, chunk=hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, mode=hl2ss.StreamMode.MODE_1, divisor=1, png_filter=hl2ss.PNGFilterMode.PAETH, decoded=True):
+    return _rx_rm_depth_longthrow(host, port, chunk, mode, divisor, png_filter)
+
+
+def rx_rm_imu(host, port, chunk=hl2ss.ChunkSize.RM_IMU, mode=hl2ss.StreamMode.MODE_1):
+    return _rx_rm_imu(host, port, chunk, mode)
+
+
+def rx_pv(host, port, chunk=hl2ss.ChunkSize.PERSONAL_VIDEO, mode=hl2ss.StreamMode.MODE_1, width=1920, height=1080, framerate=30, divisor=1, profile=hl2ss.VideoProfile.H264_HIGH, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded_format='bgr24'):
+    return _rx_pv(host, port, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options)
+
+
+def rx_microphone(host, port, chunk=hl2ss.ChunkSize.MICROPHONE, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2, decoded=True):
+    return _rx_microphone(host, port, chunk, profile, level)
+
+
+def rx_si(host, port, chunk=hl2ss.ChunkSize.SPATIAL_INPUT):
+    return _rx_si(host, port, chunk)
+
+
+def rx_eet(host, port, chunk=hl2ss.ChunkSize.EXTENDED_EYE_TRACKER, fps=30):
+    return _rx_eet(host, port, chunk, fps)
+
