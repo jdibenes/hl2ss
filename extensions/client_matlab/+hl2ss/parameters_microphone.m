e0779c2ae1493313c7f63725b2d257b8985b891c
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-02-08 01:53:14 -0500

    Update parameters_microphone.m

--- extensions/client_matlab/+hl2ss/parameters_microphone.m
+++ extensions/client_matlab/+hl2ss/parameters_microphone.m
@@ -2,11 +2,11 @@
 classdef parameters_microphone
 properties (Constant)
     ARRAY_CHANNELS     = 5
-    ARRAY_TOP_LEFT     = 0
-    ARRAY_TOP_CENTER   = 1
-    ARRAY_TOP_RIGHT    = 2
-    ARRAY_BOTTOM_LEFT  = 3
-    ARRAY_BOTTOM_RIGHT = 4
+    ARRAY_TOP_LEFT     = 1
+    ARRAY_TOP_CENTER   = 2
+    ARRAY_TOP_RIGHT    = 3
+    ARRAY_BOTTOM_LEFT  = 4
+    ARRAY_BOTTOM_RIGHT = 5
 
     SAMPLE_RATE    = 48000;
     CHANNELS       = 2;
