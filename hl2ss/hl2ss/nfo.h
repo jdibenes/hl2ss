86fc322a235667bd246894fd1376e41784596038
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-17 03:17:43 -0400

    nfo built in selection

--- hl2ss/hl2ss/nfo.h
+++ hl2ss/hl2ss/nfo.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <vector>
 
+#include <winrt/Windows.Foundation.h>
+
 void GetApplicationVersion(uint16_t data[4]);
 void GetLocalIPv4Address(std::vector<wchar_t> &address);
 void PrintSystemInfo();
+winrt::hstring GetBuiltInVideoCaptureId();
+winrt::hstring GetBuiltInAudioCaptureId();
