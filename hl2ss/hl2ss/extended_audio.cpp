2b6c410aa27808e9640ff9372730856d81033e2d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-18 18:33:33 -0400

    extended audio device selection

--- hl2ss/hl2ss/extended_audio.cpp
+++ hl2ss/hl2ss/extended_audio.cpp
@@ -1,16 +1,30 @@
 
+#include <mfapi.h>
+#include <combaseapi.h>
+#include "custom_media_types.h"
 #include "custom_audio_effect.h"
+#include "nfo.h"
 
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Media.MediaProperties.h>
 #include <winrt/Windows.Media.Devices.h>
+#include <winrt/Windows.Devices.Enumeration.h>
 #include <winrt/Windows.Media.Capture.h>
 #include <winrt/Windows.Media.Capture.Frames.h>
+#include <winrt/Windows.Data.Json.h>
 
+using namespace winrt::Windows::Media::MediaProperties;
 using namespace winrt::Windows::Media::Devices;
 using namespace winrt::Windows::Media::Capture;
 using namespace winrt::Windows::Media::Capture::Frames;
+using namespace winrt::Windows::Data::Json;
+
+struct source_format
+{
+    MediaFrameSource source;
+    MediaFrameFormat format;
+};
 
 //-----------------------------------------------------------------------------
 // Global Variables
@@ -24,26 +38,101 @@ static MediaFrameSource g_audioSource = nullptr;
 //-----------------------------------------------------------------------------
 
 // OK
-void ExtendedAudio_Open(MRCAudioOptions const& options)
+static bool ExtendedAudio_ParseSubtype(winrt::hstring const& s, AudioSubtype& v)
 {
+    if (s == L"PCM")   { v = AudioSubtype::AudioSubtype_S16; return true; }
+    if (s == L"Float") { v = AudioSubtype::AudioSubtype_F32; return true; }
+    return false;
+}
+
+// OK
+static bool ExtendedAudio_FindAudioSource(MediaCapture const& mediaCapture, MediaFrameSource& audioSource)
+{
+    std::vector<source_format> sources[2][2];
+
+    for (auto const& frameSource : mediaCapture.FrameSources())
+    {
+    auto source = frameSource.Value();
+    if (source.Info().MediaStreamType() != MediaStreamType::Audio) { continue; }
+    for (auto const& format : source.SupportedFormats())
+    {
+    auto aep = format.AudioEncodingProperties();
+    if (aep.SampleRate() != 48000) { continue; }
+    AudioSubtype subtype;
+    bool ok = ExtendedAudio_ParseSubtype(aep.Subtype(), subtype);
+    if (!ok) { continue; }
+    uint32_t channels = aep.ChannelCount();
+    if ((channels != 1) && (channels != 2)) { continue; }
+    sources[subtype][2 - channels].push_back({ source, format });
+    }
+    }
+
+    for (int i = 0; i < 2; ++i)
+    {
+    for (int j = 0; j < 2; ++j)
+    {
+    if (sources[i][j].size() <= 0) { continue; }
+    auto const& sf = sources[i][j][0];
+    audioSource = sf.source;
+    audioSource.SetFormatAsync(sf.format).get();
+    return true;
+    }
+    }
+
+    return false;
+}
+
+// OK
+void ExtendedAudio_QueryDevices(winrt::hstring& out)
+{
+    std::vector<winrt::hstring> ids;
+    std::vector<winrt::hstring> names;
+
+    GetAudioCaptureIdsAndNames(ids, names);
+
+    JsonObject root = JsonObject();
+    for (uint32_t i = 0; i < ids.size(); ++i)
+    {
+    JsonObject jsourceinfos = JsonObject();
+    jsourceinfos.Insert(L"Id", JsonValue::CreateStringValue(ids[i]));
+    jsourceinfos.Insert(L"Name", JsonValue::CreateStringValue(names[i]));
+
+    root.Insert(winrt::to_hstring(i), jsourceinfos);
+    }
+
+    out = root.ToString();
+}
+
+// OK
+bool ExtendedAudio_Open(MRCAudioOptions const& options)
+{
+    uint32_t index = (options.mixer_mode & 0x7FFFFFFC) >> 2;
     MediaCaptureInitializationSettings settings;
+    std::vector<winrt::hstring> ids;
+    bool ok;
 
-    g_mediaCapture = MediaCapture();
+    GetAudioCaptureIds(ids);
+    if (index >= ids.size()) { return false; }
+    winrt::hstring id = ids[index];
 
-    settings.AudioDeviceId(MediaDevice::GetDefaultAudioCaptureId(AudioDeviceRole::Default));
+    settings.AudioDeviceId(id);
     settings.StreamingCaptureMode(StreamingCaptureMode::Audio);
     settings.SharingMode(MediaCaptureSharingMode::SharedReadOnly);
     settings.MediaCategory(MediaCategory::Media);
 
+    g_mediaCapture = MediaCapture();
     g_mediaCapture.InitializeAsync(settings).get();
     g_mediaCapture.AddAudioEffectAsync(MRCAudioEffect(options)).get();
 
-    for (auto const& frameSource : g_mediaCapture.FrameSources())
+    ok = ExtendedAudio_FindAudioSource(g_mediaCapture, g_audioSource);
+    if (!ok)
     {
-    auto const& frameSourceInfo = frameSource.Value().Info();
-    if (frameSourceInfo.MediaStreamType() != MediaStreamType::Audio) { continue; }
-    g_audioSource = frameSource.Value();
+    g_mediaCapture.Close();
+    g_mediaCapture = nullptr;
+    return false;
     }
+
+    return true;
 }
 
 // OK
--- hl2ss/hl2ss/extended_audio.h
+++ hl2ss/hl2ss/extended_audio.h
@@ -4,6 +4,7 @@
 #include "custom_audio_effect.h"
 #include <winrt/Windows.Media.Capture.Frames.h>
 
-void ExtendedAudio_Open(MRCAudioOptions const& options);
+void ExtendedAudio_QueryDevices(winrt::hstring& out);
+bool ExtendedAudio_Open(MRCAudioOptions const& options);
 void ExtendedAudio_Close();
 winrt::Windows::Media::Capture::Frames::MediaFrameReader ExtendedAudio_CreateFrameReader();
