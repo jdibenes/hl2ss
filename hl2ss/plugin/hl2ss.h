7e820bfa4c0ae5cc98a24667e00486a4d688b87e
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-20 16:08:11 -0400

    Update hl2ss.h

--- hl2ss/plugin/hl2ss.h
+++ hl2ss/plugin/hl2ss.h
@@ -6,6 +6,10 @@
 
 #define UNITY_IMPORT extern "C" __declspec(dllimport)
 
+//-----------------------------------------------------------------------------
+// Main
+//-----------------------------------------------------------------------------
+
 UNITY_IMPORT
 void InitializeStreams(uint32_t enable);
 
@@ -20,3 +24,19 @@ void GetLocalIPv4Address(wchar_t* buffer, int size);
 
 UNITY_IMPORT
 int OverrideWorldCoordinateSystem(void* scs_ptr);
+
+//-----------------------------------------------------------------------------
+// Message Queue
+//-----------------------------------------------------------------------------
+
+UNITY_IMPORT
+uint32_t MQ_SI_Peek();
+
+UNITY_IMPORT
+void MQ_SI_Pop(uint32_t& command, uint8_t* data);
+
+UNITY_IMPORT
+void MQ_SO_Push(uint32_t id);
+
+UNITY_IMPORT
+void MQ_Restart();
