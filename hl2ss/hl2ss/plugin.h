c59923af683df683520e85addeeae35327ba4fbe
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-24 20:54:36 -0400

    Update plugin.h

--- hl2ss/hl2ss/plugin.h
+++ hl2ss/hl2ss/plugin.h
@@ -1,4 +1,8 @@
 
 #pragma once
 
-#define UNITY_EXPORT extern "C" __declspec(dllexport)
+#ifdef HL2SS_PLUGIN
+#define HL2SS_PLUGIN_EXPORT extern "C" __declspec(dllexport)
+#else
+#define HL2SS_PLUGIN_EXPORT 
+#endif
