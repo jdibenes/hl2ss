57964f329cf02c3593b3e6cbdeec2ca72c9ead5a
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-24 20:54:46 -0400

    Update ipc_mq.h

--- hl2ss/hl2ss/ipc_mq.h
+++ hl2ss/hl2ss/ipc_mq.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <stdint.h>
+#include "plugin.h"
 
 void MQ_Initialize();
 void MQ_Quit();
@@ -11,12 +12,12 @@ void MQX_Initialize();
 void MQX_Quit();
 void MQX_Cleanup();
 
-uint32_t MQ_SI_Peek();
-void MQ_SI_Pop(uint32_t& command, uint8_t* data);
-void MQ_SO_Push(uint32_t id);
-void MQ_Restart();
+HL2SS_PLUGIN_EXPORT uint32_t MQ_SI_Peek();
+HL2SS_PLUGIN_EXPORT void MQ_SI_Pop(uint32_t& command, uint8_t* data);
+HL2SS_PLUGIN_EXPORT void MQ_SO_Push(uint32_t id);
+HL2SS_PLUGIN_EXPORT void MQ_Restart();
 
-uint32_t MQX_CO_Peek();
-void MQX_CO_Pop(uint32_t& id);
-void MQX_CI_Push(uint32_t command, uint32_t size, uint8_t const* data);
-void MQX_Restart();
+HL2SS_PLUGIN_EXPORT uint32_t MQX_CO_Peek();
+HL2SS_PLUGIN_EXPORT void MQX_CO_Pop(uint32_t& id);
+HL2SS_PLUGIN_EXPORT void MQX_CI_Push(uint32_t command, uint32_t size, uint8_t const* data);
+HL2SS_PLUGIN_EXPORT void MQX_Restart();
