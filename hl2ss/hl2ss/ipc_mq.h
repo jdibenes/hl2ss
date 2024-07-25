d9408ae3bea4077059bc8a6afff80bad23c9497c
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-24 20:30:04 -0400

    cleanup mq

--- hl2ss/hl2ss/ipc_mq.h
+++ hl2ss/hl2ss/ipc_mq.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <stdint.h>
+
 void MQ_Initialize();
 void MQ_Quit();
 void MQ_Cleanup();
@@ -8,3 +10,13 @@ void MQ_Cleanup();
 void MQX_Initialize();
 void MQX_Quit();
 void MQX_Cleanup();
+
+uint32_t MQ_SI_Peek();
+void MQ_SI_Pop(uint32_t& command, uint8_t* data);
+void MQ_SO_Push(uint32_t id);
+void MQ_Restart();
+
+uint32_t MQX_CO_Peek();
+void MQX_CO_Pop(uint32_t& id);
+void MQX_CI_Push(uint32_t command, uint32_t size, uint8_t const* data);
+void MQX_Restart();
