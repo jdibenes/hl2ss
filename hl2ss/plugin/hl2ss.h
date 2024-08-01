1e50de7fe10dfffadc9201f4b9b25e1c39532612
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-31 21:05:46 -0400

    Update hl2ss.h

--- hl2ss/plugin/hl2ss.h
+++ hl2ss/plugin/hl2ss.h
@@ -4,51 +4,81 @@
 #include <stdint.h>
 #include "configuration.h"
 
-#define UNITY_IMPORT extern "C" __declspec(dllimport)
+#define HL2SS_PLUGIN_IMPORT extern "C" __declspec(dllimport)
 
 //-----------------------------------------------------------------------------
 // Main
 //-----------------------------------------------------------------------------
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void InitializeStreams(uint32_t enable);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void InitializeStreamsOnUI(uint32_t enable);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void DebugMessage(char const* str);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void GetLocalIPv4Address(wchar_t* buffer, int size);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 int OverrideWorldCoordinateSystem(void* scs_ptr);
 
 //-----------------------------------------------------------------------------
 // Message Queue
 //-----------------------------------------------------------------------------
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 uint32_t MQ_SI_Peek();
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQ_SI_Pop(uint32_t& command, uint8_t* data);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQ_SO_Push(uint32_t id);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQ_Restart();
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 uint32_t MQX_CO_Peek();
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQX_CO_Pop(uint32_t& id);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQX_CI_Push(uint32_t command, uint32_t size, uint8_t const* data);
 
-UNITY_IMPORT
+HL2SS_PLUGIN_IMPORT
 void MQX_Restart();
+
+//-----------------------------------------------------------------------------
+// Lock
+//-----------------------------------------------------------------------------
+
+HL2SS_PLUGIN_IMPORT
+void* NamedMutex_Create(wchar_t const* name);
+
+HL2SS_PLUGIN_IMPORT
+void NamedMutex_Destroy(void* p);
+
+HL2SS_PLUGIN_IMPORT
+int NamedMutex_Acquire(void* p, uint32_t timeout);
+
+HL2SS_PLUGIN_IMPORT
+int NamedMutex_Release(void* p);
+
+//-----------------------------------------------------------------------------
+// PV
+//-----------------------------------------------------------------------------
+
+HL2SS_PLUGIN_IMPORT
+void PersonalVideo_RegisterNamedMutex(wchar_t const* name);
+
+//-----------------------------------------------------------------------------
+// EV
+//-----------------------------------------------------------------------------
+
+HL2SS_PLUGIN_IMPORT
+void ExtendedVideo_RegisterNamedMutex(wchar_t const* name);
