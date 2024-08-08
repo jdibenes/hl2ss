3bdc9b08c9a22f0721fc3ae17b5655fff021d756
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-08 18:04:43 -0400

    Update hl2ss.h

--- extensions/client_cpp/hl2ss.h
+++ extensions/client_cpp/hl2ss.h
@@ -1354,15 +1354,15 @@ struct eet_frame
     uint32_t valid;
 };
 
-void unpack_rm_vlc(uint8_t* payload, uint8_t** image);
-void unpack_rm_depth_ahat(uint8_t* payload, uint16_t** depth, uint16_t** ab);
-void unpack_rm_depth_longthrow(uint8_t* payload, uint16_t** depth, uint16_t** ab);
-void unpack_rm_imu(uint8_t* payload, rm_imu_sample** samples);
-void unpack_pv(uint8_t* payload, size_t size, uint8_t** image, pv_intrinsics** intrinsics);
-void unpack_microphone_raw(uint8_t* payload, int16_t** samples);
-void unpack_microphone_aac(uint8_t* payload, float** samples);
-void unpack_si(uint8_t* payload, si_frame** si);
-void unpack_eet(uint8_t* payload, eet_frame** eet);
-void unpack_extended_audio_raw(uint8_t* payload, int16_t** samples);
-void unpack_extended_audio_aac(uint8_t* payload, float** samples);
+void unpack_rm_vlc(uint8_t* payload, uint8_t*& image);
+void unpack_rm_depth_ahat(uint8_t* payload, uint16_t*& depth, uint16_t*& ab);
+void unpack_rm_depth_longthrow(uint8_t* payload, uint16_t*& depth, uint16_t*& ab);
+void unpack_rm_imu(uint8_t* payload, rm_imu_sample*& samples);
+void unpack_pv(uint8_t* payload, size_t size, uint8_t*& image, pv_intrinsics*& intrinsics);
+void unpack_microphone_raw(uint8_t* payload, int16_t*& samples);
+void unpack_microphone_aac(uint8_t* payload, float*& samples);
+void unpack_si(uint8_t* payload, si_frame*& si);
+void unpack_eet(uint8_t* payload, eet_frame*& eet);
+void unpack_extended_audio_raw(uint8_t* payload, int16_t*& samples);
+void unpack_extended_audio_aac(uint8_t* payload, float*& samples);
 }
