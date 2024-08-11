7fd4972fc5fdb705299325d05f7ae7f1481ca1be
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-11 01:11:38 -0400

    Update nfo.h

--- hl2ss/hl2ss/nfo.h
+++ hl2ss/hl2ss/nfo.h
@@ -5,10 +5,14 @@
 #include <vector>
 
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 
 void GetApplicationVersion(uint16_t data[4]);
 void GetLocalIPv4Address(std::vector<wchar_t> &address);
 void PrintSystemInfo();
+void PrintProperties(winrt::Windows::Foundation::Collections::IMapView<winrt::hstring, winrt::Windows::Foundation::IInspectable> const& p);
+void PrintProperties(winrt::Windows::Foundation::Collections::IMapView<winrt::guid, winrt::Windows::Foundation::IInspectable> const& p);
+void PrintProperties(winrt::Windows::Foundation::Collections::IPropertySet const& p);
 winrt::hstring GetBuiltInVideoCaptureId();
 winrt::hstring GetBuiltInAudioCaptureId();
 void GetVideoCaptureIds(std::vector<winrt::hstring>& ids);
