ace703b290002967289eef27d070154f8abefbfe
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-12 21:32:02 -0400

    Update types.h

--- hl2ss/hl2ss/types.h
+++ hl2ss/hl2ss/types.h
@@ -17,3 +17,18 @@ union v8  { u8 b; s8 c; };
 union v16 { struct { v8  b0, b1; } b; u16 w; s16 s; };
 union v32 { struct { v16 w0, w1; } w; u32 d; s32 i; };
 union v64 { struct { v32 d0, d1; } d; u64 q; s64 l; };
+
+constexpr uint32_t bit(uint32_t b)
+{
+    return (1UL << b);
+}
+
+constexpr bool bit_test(uint32_t v, uint32_t b)
+{
+    return v & bit(b);
+}
+
+constexpr uint32_t bit_field(uint32_t v, uint32_t b, uint32_t mask)
+{
+    return (v >> b) & mask;
+}
