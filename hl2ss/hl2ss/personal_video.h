160c0a3c945839bdccd678ea6bcd098357ee8b46
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-12 21:31:14 -0400

    Update personal_video.h

--- hl2ss/hl2ss/personal_video.h
+++ hl2ss/hl2ss/personal_video.h
@@ -27,3 +27,8 @@ void PersonalVideo_SetExposurePriorityVideo(uint32_t enabled);
 void PersonalVideo_SetSceneMode(uint32_t mode);
 void PersonalVideo_SetIsoSpeed(uint32_t setauto, uint32_t value);
 void PersonalVideo_SetBacklightCompensation(bool enable);
+void PersonalVideo_SetDesiredOptimization(uint32_t mode);
+void PersonalVideo_SetPrimaryUse(uint32_t mode);
+void PersonalVideo_SetOpticalImageStabilization(uint32_t mode);
+void PersonalVideo_SetHdrVideo(uint32_t mode);
+void PersonalVideo_SetRegionsOfInterest(bool clear, bool set, bool auto_exposure, bool auto_focus, bool bounds_normalized, float x, float y, float w, float h, uint32_t type, uint32_t weight);
