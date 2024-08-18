a9577cd9ae5d093a2c5e3ab58c6bfafc0b1253a4
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-18 18:56:01 -0400

    update unity client

---
+++ extensions/client_unity/Assets/Scripts/test_rm_vlc.cs
@@ -0,0 +1,51 @@
+
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class test_rm_vlc : MonoBehaviour
+{
+    public GameObject quad_rm_vlc;
+    public ushort port = hl2ss.stream_port.RM_VLC_LEFTFRONT;
+
+    private hl2ss.svc.source source_rm_vlc;
+    private Texture2D texture_rm_vlc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string host = run_once.host_address;
+
+        var configuration = hl2ss.svc.create_configuration_rm_vlc();
+
+        var calibration_handle = hl2ss.svc.download_calibration(host, port, configuration);
+        var calibration = Marshal.PtrToStructure<hl2ss.calibration_rm_vlc>(calibration_handle.address);
+        calibration_handle.destroy();
+
+        source_rm_vlc = hl2ss.svc.open_stream(host, port, 300, configuration);
+
+        texture_rm_vlc = new Texture2D(hl2ss.parameters_rm_vlc.WIDTH, hl2ss.parameters_rm_vlc.HEIGHT, TextureFormat.R8, false);
+        quad_rm_vlc.GetComponent<Renderer>().material.mainTexture = texture_rm_vlc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var packet = source_rm_vlc.get_by_index(-1);
+        if (packet.status != 0) { return; }
+
+        packet.unpack(out hl2ss.map_rm_vlc region);
+
+        var metadata = Marshal.PtrToStructure<hl2ss.rm_vlc_metadata>(region.metadata);
+        var pose = Marshal.PtrToStructure<hl2ss.matrix_4x4>(packet.pose);
+
+        texture_rm_vlc.LoadRawTextureData(region.image, (int)hl2ss.parameters_rm_vlc.PIXELS * sizeof(byte));
+        texture_rm_vlc.Apply();
+
+        packet.destroy();
+    }
+
+    void OnApplicationQuit()
+    {
+        source_rm_vlc.destroy();
+    }
+}
--- extensions/client_unity/Assets/Scripts/viewer.cs
+++ extensions/client_unity/Assets/Scripts/viewer.cs
@@ -1,20 +1,21 @@
 
 using System.Runtime.InteropServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class viewer : MonoBehaviour
 {
-    public string host;
     public GameObject quad_pv;
+    private string host;
     private hl2ss.svc.source source_pv;
     private int pv_frame_size;
-    
-
     private Texture2D tex_pv;
 
     // Start is called before the first frame update
     void Start()
     {
+        host = run_once.host_address;
+
         hl2ss.ulm.configuration_pv configuration = hl2ss.svc.create_configuration_pv();
 
         configuration.width = 1280;
@@ -53,8 +54,11 @@ public class viewer : MonoBehaviour
 
         pv_frame_size = configuration.width * configuration.height * bpp;
 
-        hl2ss.svc.initialize();
         hl2ss.svc.start_subsystem_pv(host, hl2ss.stream_port.PERSONAL_VIDEO);
+
+        var calibration_handle = hl2ss.svc.download_calibration(host, hl2ss.stream_port.PERSONAL_VIDEO, configuration);
+        var calibration = Marshal.PtrToStructure<hl2ss.calibration_pv>(calibration_handle.address);
+        calibration_handle.destroy();
 
         source_pv = hl2ss.svc.open_stream(host, hl2ss.stream_port.PERSONAL_VIDEO, 300, configuration);
 
@@ -73,6 +77,7 @@ public class viewer : MonoBehaviour
         hl2ss.pv_metadata metadata = Marshal.PtrToStructure<hl2ss.pv_metadata>(region.metadata);
         hl2ss.matrix_4x4 pose = Marshal.PtrToStructure<hl2ss.matrix_4x4>(packet.pose);
 
+        Debug.Log(packet.frame_stamp);
         Debug.Log(packet.timestamp);
         Debug.Log(metadata.f.x);
         Debug.Log(metadata.lens_position);
@@ -80,6 +85,7 @@ public class viewer : MonoBehaviour
         Debug.Log(metadata.white_balance_gains.x);
         Debug.Log(metadata.white_balance_gains.y);
         Debug.Log(metadata.white_balance_gains.z);
+        Debug.Log(pose.m[15]);
 
         tex_pv.LoadRawTextureData(region.image, pv_frame_size);
         tex_pv.Apply();
