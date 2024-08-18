83778a3a5f2da43c1ca8511927912a97ca2a6cb2
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-08-17 22:48:46 -0400

    update unity client

---
+++ extensions/client_unity/Assets/Scripts/hl2ss/hl2ss_svc.cs
@@ -0,0 +1,261 @@
+
+using System;
+using System.Runtime.InteropServices;
+
+public static partial class hl2ss
+{
+    public class svc
+    {
+        //-----------------------------------------------------------------------------
+        // Handle
+        //-----------------------------------------------------------------------------
+
+        public class handle
+        {
+            protected IntPtr m_handle;
+
+            protected static void check_result(IntPtr handle)
+            {
+                if (handle == IntPtr.Zero) { throw new ExternalException("ULM invalid handle"); }
+            }
+
+            protected handle(IntPtr h)
+            {
+                m_handle = h;
+                check_result(h);
+            }
+
+            public static void check_result(int v)
+            {
+                if (v < 0) { throw new ExternalException("ULM operation error"); }
+            }
+
+            public void destroy()
+            {
+                if (m_handle == IntPtr.Zero) { return; }
+                hl2ss.ulm.close_handle(m_handle);
+                m_handle = IntPtr.Zero;
+            }
+
+            ~handle()
+            {
+                destroy();
+            }
+        }
+
+        // BLOCK
+
+        //-----------------------------------------------------------------------------
+        // Stream
+        //-----------------------------------------------------------------------------
+
+        public class packet : handle
+        {
+            protected hl2ss.ulm.packet data;
+
+            protected packet(IntPtr h, hl2ss.ulm.packet data) : base(h)
+            {
+                this.data = data;
+            }
+
+            protected packet(IntPtr source, long frame_stamp, hl2ss.ulm.packet data) : this(hl2ss.ulm.get_by_index(source, frame_stamp, data), data)
+            {
+            }
+
+            protected packet(IntPtr source, ulong timestamp, int time_preference, int tiebreak_right, hl2ss.ulm.packet data) : this(hl2ss.ulm.get_by_timestamp(source, timestamp, time_preference, tiebreak_right, data), data)
+            {
+            }
+
+            public packet(IntPtr source, long frame_stamp) : this(source, frame_stamp, new hl2ss.ulm.packet())
+            {
+            }
+
+            public packet(IntPtr source, ulong timestamp, int time_preference, int tiebreak_right) : this(source, timestamp, time_preference, tiebreak_right, new hl2ss.ulm.packet())
+            {
+            }
+
+            public long frame_stamp { get { return data.frame_stamp; } }
+
+            public ulong timestamp { get { return data.timestamp; } }
+
+            public uint sz_payload { get { return data.sz_payload; } }
+
+            public int status { get { return data.status; } }
+
+            public IntPtr payload { get { return data.payload; } }
+
+            public IntPtr pose { get { return data.pose; } }
+        }
+
+        public class source : handle
+        {
+            public source(string host, ushort port, ulong buffer_size, IntPtr configuration) : base(hl2ss.ulm.open_stream(host, port, buffer_size, configuration))
+            {
+            }
+
+            public packet get_by_index(long frame_stamp)
+            {
+                return new packet(m_handle, frame_stamp);
+            }
+
+            public packet get_by_timestamp(ulong timestamp, int time_preference, int tiebreak_right)
+            {
+                return new packet(m_handle, timestamp, time_preference, tiebreak_right);
+            }
+        }
+
+        // BLOCK
+
+        //-----------------------------------------------------------------------------
+        // API
+        //-----------------------------------------------------------------------------
+
+        public static void initialize()
+        {
+            handle.check_result(hl2ss.ulm.initialize());
+        }
+
+        public static hl2ss.ulm.configuration_rm_vlc create_configuration_rm_vlc()
+        {
+            hl2ss.ulm.configuration_rm_vlc c = new hl2ss.ulm.configuration_rm_vlc();
+
+            c.chunk = hl2ss.chunk_size.RM_VLC;
+            c.mode = hl2ss.stream_mode.MODE_1;
+            c.divisor = 1;
+            c.profile = hl2ss.video_profile.H265_MAIN;
+            c.level = hl2ss.h26x_level.DEFAULT;
+            c.bitrate = 0;
+            c.options_data = IntPtr.Zero;
+            c.options_size = -1;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_rm_depth_ahat create_configuration_rm_depth_ahat()
+        {
+            hl2ss.ulm.configuration_rm_depth_ahat c = new hl2ss.ulm.configuration_rm_depth_ahat();
+
+            c.chunk = hl2ss.chunk_size.RM_DEPTH_AHAT;
+            c.mode = hl2ss.stream_mode.MODE_1;
+            c.divisor = 1;
+            c.profile_z = hl2ss.depth_profile.SAME;
+            c.profile_ab = hl2ss.video_profile.H265_MAIN;
+            c.level = hl2ss.h26x_level.DEFAULT;
+            c.bitrate = 0;
+            c.options_data = IntPtr.Zero;
+            c.options_size = -1;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_rm_depth_longthrow create_configuration_rm_depth_longthrow()
+        {
+            hl2ss.ulm.configuration_rm_depth_longthrow c = new hl2ss.ulm.configuration_rm_depth_longthrow();
+
+            c.chunk = hl2ss.chunk_size.RM_DEPTH_LONGTHROW;
+            c.mode = hl2ss.stream_mode.MODE_1;
+            c.divisor = 1;
+            c.png_filter = hl2ss.png_filter_mode.PAETH;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_rm_imu create_configuration_rm_imu()
+        {
+            hl2ss.ulm.configuration_rm_imu c = new hl2ss.ulm.configuration_rm_imu();
+
+            c.chunk = hl2ss.chunk_size.RM_IMU;
+            c.mode = hl2ss.stream_mode.MODE_1;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_pv create_configuration_pv()
+        {
+            hl2ss.ulm.configuration_pv c = new hl2ss.ulm.configuration_pv();
+
+            c.width = 1920;
+            c.height = 1080;
+            c.framerate = 30;
+            c.chunk = hl2ss.chunk_size.PERSONAL_VIDEO;
+            c.mode = hl2ss.stream_mode.MODE_1;
+            c.divisor = 1;
+            c.profile = hl2ss.video_profile.H265_MAIN;
+            c.level = hl2ss.h26x_level.DEFAULT;
+            c.bitrate = 0;
+            c.options_data = IntPtr.Zero;
+            c.options_size = -1;
+            c.decoded_format = hl2ss.pv_decoded_format.BGR;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_microphone create_configuration_microphone()
+        {
+            hl2ss.ulm.configuration_microphone c = new hl2ss.ulm.configuration_microphone();
+
+            c.chunk = hl2ss.chunk_size.MICROPHONE;
+            c.profile = hl2ss.audio_profile.AAC_24000;
+            c.level = hl2ss.aac_level.L2;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_si create_configuration_si()
+        {
+            hl2ss.ulm.configuration_si c = new hl2ss.ulm.configuration_si();
+
+            c.chunk = hl2ss.chunk_size.SPATIAL_INPUT;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_eet create_configuration_eet()
+        {
+            hl2ss.ulm.configuration_eet c = new hl2ss.ulm.configuration_eet();
+
+            c.chunk = hl2ss.chunk_size.EXTENDED_EYE_TRACKER;
+            c.framerate = hl2ss.eet_framerate.FPS_30;
+
+            return c;
+        }
+
+        public static hl2ss.ulm.configuration_extended_audio create_configuration_extended_audio()
+        {
+            hl2ss.ulm.configuration_extended_audio c = new hl2ss.ulm.configuration_extended_audio();
+
+            c.chunk = hl2ss.chunk_size.EXTENDED_AUDIO;
+            c.mixer_mode = hl2ss.mixer_mode.BOTH;
+            c.loopback_gain = 1.0f;
+            c.microphone_gain = 1.0f;
+            c.profile = hl2ss.audio_profile.AAC_24000;
+            c.level = hl2ss.aac_level.L2;
+
+            return c;
+        }
+
+        public static source open_stream<T>(string host, ushort port, ulong buffer_size, T configuration)
+        {
+            GCHandle h = GCHandle.Alloc(configuration, GCHandleType.Pinned);
+            source s = new source(host, port, buffer_size, h.AddrOfPinnedObject());
+            h.Free();
+            return s;
+        }
+
+        // BLOCK
+
+        public static void start_subsystem_pv(string host, ushort port, byte enable_mrc = 0, byte hologram_composition = 1, byte recording_indicator = 0, byte video_stabilization = 0, byte blank_protected = 0, byte show_mesh = 0, byte shared = 0, float global_opacity = 0.9f, float output_width = 0.0f, float output_height = 0.0f, uint video_stabilization_length = 0, uint hologram_perspective = hl2ss.hologram_perspective.PV)
+        {
+            handle.check_result(hl2ss.ulm.start_subsystem_pv(host, port, enable_mrc, hologram_composition, recording_indicator, video_stabilization, blank_protected, show_mesh, shared, global_opacity, output_width, output_height, video_stabilization_length, hologram_perspective));
+        }
+
+        public static void stop_subsystem_pv(string host, ushort port)
+        {
+            handle.check_result(hl2ss.ulm.stop_subsystem_pv(host, port));
+        }
+
+
+
+
+}
+}
--- extensions/client_unity/Assets/Scripts/viewer.cs
+++ extensions/client_unity/Assets/Scripts/viewer.cs
@@ -1,27 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class viewer : MonoBehaviour
 {
     public string host;
     public GameObject quad_pv;
+    public hl2ss.svc.source source_pv; 
 
     private Texture2D tex_pv;
 
     // Start is called before the first frame update
     void Start()
     {
-        ushort pv_width = 1280;
-        ushort pv_height = 720;
-        byte pv_framerate = 30;
+        hl2ss.ulm.configuration_pv configuration = hl2ss.svc.create_configuration_pv();
 
-        hl2ss.ulm.initialize();
-        hl2ss.ulm.start_subsystem_pv(host, hl2ss.stream_port.PERSONAL_VIDEO);
-        hl2ss.ulm.open_pv(host, hl2ss.stream_port.PERSONAL_VIDEO, pv_width, pv_height, pv_framerate, hl2ss.chunk_size.PERSONAL_VIDEO, hl2ss.stream_mode.MODE_1, 1, hl2ss.video_profile.H265_MAIN, hl2ss.h26x_level.DEFAULT, 0, 0, null, hl2ss.pv_decoded_format.RGB);
+        configuration.width = 1280;
+        configuration.height = 720;
+        configuration.framerate = 30;
+        configuration.decoded_format = hl2ss.pv_decoded_format.RGB;
 
-        tex_pv = new Texture2D(pv_width, pv_height, TextureFormat.RGB24, false);
+        hl2ss.svc.initialize();
+        hl2ss.svc.start_subsystem_pv(host, hl2ss.stream_port.PERSONAL_VIDEO);
+
+        source_pv = hl2ss.svc.open_stream(host, hl2ss.stream_port.PERSONAL_VIDEO, 300, configuration);
+
+        tex_pv = new Texture2D(configuration.width, configuration.height, TextureFormat.RGB24, false);
 
         quad_pv.GetComponent<Renderer>().material.mainTexture = tex_pv;
     }
@@ -29,31 +32,16 @@ public class viewer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        long frame_stamp = -1;
-        int status = -1;
-        IntPtr frame = IntPtr.Zero;
-        int valid = hl2ss.ulm.get_by_index(hl2ss.stream_port.PERSONAL_VIDEO, ref frame_stamp, ref status, ref frame);
-        if (valid < 0) { return; }
-        if (status != 0) { return; }
+        hl2ss.svc.packet packet = source_pv.get_by_index(-1);
+        if (packet.status != 0) { return; }
 
-        ulong timestamp = 0;
-        uint payload_size = 0;
-        IntPtr payload_data = IntPtr.Zero;
-        IntPtr pose_data = IntPtr.Zero;
-        IntPtr pv_image = IntPtr.Zero;
-        IntPtr pv_intrinsics = IntPtr.Zero;
-        hl2ss.ulm.unpack_frame(frame, ref timestamp, ref payload_size, ref payload_data, ref pose_data);
-        hl2ss.ulm.unpack_pv(payload_data, payload_size, ref pv_image, ref pv_intrinsics);
-
-        tex_pv.LoadRawTextureData(payload_data, (int)payload_size);
+        tex_pv.LoadRawTextureData(packet.payload, (int)packet.sz_payload);
         tex_pv.Apply();
-
-        hl2ss.ulm.release_frame(frame);
     }
 
     void OnApplicationQuit()
     {
-        hl2ss.ulm.close(hl2ss.stream_port.PERSONAL_VIDEO);
-        hl2ss.ulm.stop_subsystem_pv(host, hl2ss.stream_port.PERSONAL_VIDEO);        
+        source_pv.destroy();
+        hl2ss.svc.stop_subsystem_pv(host, hl2ss.stream_port.PERSONAL_VIDEO);
     }
 }
