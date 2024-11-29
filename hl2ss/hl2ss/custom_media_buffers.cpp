5c9818702a63251fee380b604cdfe6ec0f99509d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-11-29 09:29:50 -0500

    cleanup mediacapture buffer wrapper

--- hl2ss/hl2ss/custom_media_buffers.cpp
+++ hl2ss/hl2ss/custom_media_buffers.cpp
@@ -1,5 +1,4 @@
 
-#include <new>
 #include <mferror.h>
 #include "custom_media_buffers.h"
 
@@ -11,7 +10,7 @@ using namespace winrt::Windows::Media::Capture::Frames;
 using namespace winrt::Windows::Graphics::Imaging;
 
 //-----------------------------------------------------------------------------
-// SoftwareBitmapBuffer Methods
+// IUnknown Methods
 //-----------------------------------------------------------------------------
 
 // OK
@@ -35,18 +34,21 @@ HRESULT SoftwareBitmapBuffer::QueryInterface(REFIID iid, void** ppv)
 
     *ppv = NULL;
 
-    if (iid == IID_IUnknown) { *ppv = static_cast<IUnknown*>(this); }
+    if      (iid == IID_IUnknown)       { *ppv = static_cast<IUnknown*>(this); }
     else if (iid == IID_IMFMediaBuffer) { *ppv = static_cast<IMFMediaBuffer*>(this); }
-    else { return E_NOINTERFACE; }
+    else                                { return E_NOINTERFACE; }
 
     AddRef();
     return S_OK;
 }
 
+//-----------------------------------------------------------------------------
+// IMFMediaBuffer Methods
+//-----------------------------------------------------------------------------
+
 // OK
 HRESULT SoftwareBitmapBuffer::GetCurrentLength(DWORD* pcbCurrentLength)
 {
-    if (!pcbCurrentLength) { return E_INVALIDARG; }
     *pcbCurrentLength = m_curLength;
     return S_OK;
 }
@@ -54,7 +56,6 @@ HRESULT SoftwareBitmapBuffer::GetCurrentLength(DWORD* pcbCurrentLength)
 // OK
 HRESULT SoftwareBitmapBuffer::GetMaxLength(DWORD* pcbMaxLength)
 {
-    if (!pcbMaxLength) { return E_INVALIDARG; }
     *pcbMaxLength = m_maxLength;
     return S_OK;
 }
@@ -62,9 +63,6 @@ HRESULT SoftwareBitmapBuffer::GetMaxLength(DWORD* pcbMaxLength)
 // OK
 HRESULT SoftwareBitmapBuffer::Lock(BYTE** ppbBuffer, DWORD* pcbMaxLength, DWORD* pcbCurrentLength)
 {
-    if (!ppbBuffer) { return E_INVALIDARG; }
-    if (m_isLocked) { return MF_E_INVALIDREQUEST; }
-    m_isLocked = true;
     *ppbBuffer = m_pBase;
     if (pcbMaxLength) { *pcbMaxLength = m_maxLength; }
     if (pcbCurrentLength) { *pcbCurrentLength = m_curLength; }
@@ -81,10 +79,12 @@ HRESULT SoftwareBitmapBuffer::SetCurrentLength(DWORD cbCurrentLength)
 // OK
 HRESULT SoftwareBitmapBuffer::Unlock()
 {
-    if (!m_isLocked) { return MF_E_INVALIDREQUEST; }
-    m_isLocked = false;
     return S_OK;
 }
+
+//-----------------------------------------------------------------------------
+// SoftwareBitmapBuffer Methods
+//-----------------------------------------------------------------------------
 
 // OK
 SoftwareBitmapBuffer::SoftwareBitmapBuffer(MediaFrameReference const &ref) : m_bmp(nullptr), m_buf(nullptr)
@@ -92,13 +92,12 @@ SoftwareBitmapBuffer::SoftwareBitmapBuffer(MediaFrameReference const &ref) : m_b
     UINT32 length;
 
     m_nRefCount = 1;
-    m_isLocked  = false;
-    m_bmp       = ref.VideoMediaFrame().SoftwareBitmap(); // TODO: Error Handling
-    m_buf       = m_bmp.LockBuffer(BitmapBufferAccessMode::Read); // TODO: Error Handling
-    m_ref       = m_buf.CreateReference(); // TODO: Error Handling
-    m_bba       = m_ref.as<Windows::Foundation::IMemoryBufferByteAccess>(); // TODO: Error Handling
+    m_bmp       = ref.VideoMediaFrame().SoftwareBitmap();
+    m_buf       = m_bmp.LockBuffer(BitmapBufferAccessMode::Read);
+    m_ref       = m_buf.CreateReference();
+    m_bba       = m_ref.as<Windows::Foundation::IMemoryBufferByteAccess>();
 
-    m_bba->GetBuffer(&m_pBase, &length); // TODO: Error Handling
+    m_bba->GetBuffer(&m_pBase, &length);
 
     m_maxLength = length;
     m_curLength = length;
@@ -107,8 +106,6 @@ SoftwareBitmapBuffer::SoftwareBitmapBuffer(MediaFrameReference const &ref) : m_b
 // OK
 SoftwareBitmapBuffer::~SoftwareBitmapBuffer()
 {
-    //assert(!m_nRefcount);
-    //assert(!m_isLocked);
                    m_bba = nullptr;
     m_ref.Close(); m_ref = nullptr;
     m_buf.Close(); m_buf = nullptr;
@@ -118,111 +115,6 @@ SoftwareBitmapBuffer::~SoftwareBitmapBuffer()
 // OK
 HRESULT SoftwareBitmapBuffer::CreateInstance(SoftwareBitmapBuffer** ppBuffer, MediaFrameReference const& ref)
 {
-    if (!ppBuffer) { return E_INVALIDARG; }
-    *ppBuffer = new (std::nothrow) SoftwareBitmapBuffer(ref);
-    return S_OK;
-}
-
-//-----------------------------------------------------------------------------
-// WrappedBuffer Methods
-//-----------------------------------------------------------------------------
-
-// OK
-ULONG WrappedBuffer::AddRef()
-{
-    return InterlockedIncrement(&m_nRefCount);
-}
-
-// OK
-ULONG WrappedBuffer::Release()
-{
-    ULONG uCount = InterlockedDecrement(&m_nRefCount);
-    if (uCount == 0) { delete this; }
-    return uCount;
-}
-
-// OK
-HRESULT WrappedBuffer::QueryInterface(REFIID iid, void** ppv)
-{
-    if (!ppv) { return E_POINTER; }
-
-    *ppv = NULL;
-
-    if (iid == IID_IUnknown) { *ppv = static_cast<IUnknown*>(this); }
-    else if (iid == IID_IMFMediaBuffer) { *ppv = static_cast<IMFMediaBuffer*>(this); }
-    else { return E_NOINTERFACE; }
-
-    AddRef();
-    return S_OK;
-}
-
-// OK
-HRESULT WrappedBuffer::GetCurrentLength(DWORD* pcbCurrentLength)
-{
-    if (!pcbCurrentLength) { return E_INVALIDARG; }
-    *pcbCurrentLength = m_curLength;
-    return S_OK;
-}
-
-// OK
-HRESULT WrappedBuffer::GetMaxLength(DWORD* pcbMaxLength)
-{
-    if (!pcbMaxLength) { return E_INVALIDARG; }
-    *pcbMaxLength = m_maxLength;
-    return S_OK;
-}
-
-// OK
-HRESULT WrappedBuffer::Lock(BYTE** ppbBuffer, DWORD* pcbMaxLength, DWORD* pcbCurrentLength)
-{
-    if (!ppbBuffer) { return E_INVALIDARG; }
-    if (m_isLocked) { return MF_E_INVALIDREQUEST; }
-    m_isLocked = true;
-    *ppbBuffer = m_pBase;
-    if (pcbMaxLength) { *pcbMaxLength = m_maxLength; }
-    if (pcbCurrentLength) { *pcbCurrentLength = m_curLength; }
-    return S_OK;
-}
-
-// OK
-HRESULT WrappedBuffer::SetCurrentLength(DWORD cbCurrentLength)
-{
-    if (cbCurrentLength > m_maxLength) { return E_INVALIDARG; }
-    m_curLength = cbCurrentLength;
-    return S_OK;
-}
-
-// OK
-HRESULT WrappedBuffer::Unlock()
-{
-    if (!m_isLocked) { return MF_E_INVALIDREQUEST; }
-    m_isLocked = false;
-    return S_OK;
-}
-
-// OK
-WrappedBuffer::WrappedBuffer(BYTE* pBase, DWORD dwLength)
-{
-    m_nRefCount = 1;
-    m_isLocked = false;
-
-    m_pBase = pBase;
-
-    m_maxLength = dwLength;
-    m_curLength = dwLength;
-}
-
-// OK
-WrappedBuffer::~WrappedBuffer()
-{
-    //assert(!m_nRefcount);
-    //assert(!m_isLocked);
-}
-
-// OK
-HRESULT WrappedBuffer::CreateInstance(WrappedBuffer** ppBuffer, BYTE *pData, DWORD dwLength)
-{
-    if (!ppBuffer) { return E_INVALIDARG; }
-    *ppBuffer = new (std::nothrow) WrappedBuffer(pData, dwLength);
+    *ppBuffer = new SoftwareBitmapBuffer(ref);
     return S_OK;
 }
--- hl2ss/hl2ss/custom_media_buffers.h
+++ hl2ss/hl2ss/custom_media_buffers.h
@@ -3,16 +3,15 @@
 
 #include <MemoryBuffer.h>
 #include <mfidl.h>
+
 #include <winrt/Windows.Media.Capture.Frames.h>
 #include <winrt/Windows.Graphics.Imaging.h>
 
-// OK
 class SoftwareBitmapBuffer : public IMFMediaBuffer
 {
 private:
     ULONG m_nRefCount;
-    bool  m_isLocked;
-    
+
     winrt::Windows::Graphics::Imaging::SoftwareBitmap                  m_bmp;
     winrt::Windows::Graphics::Imaging::BitmapBuffer                    m_buf;
     winrt::Windows::Foundation::IMemoryBufferReference                 m_ref;
@@ -29,37 +28,8 @@ public:
     static HRESULT CreateInstance(SoftwareBitmapBuffer** ppBuffer, winrt::Windows::Media::Capture::Frames::MediaFrameReference const& ref);
 
     // IUnknown Methods
-    ULONG AddRef();
-    ULONG Release();
-    HRESULT QueryInterface(REFIID iid, void** ppv);
-
-    // IMFMediaBuffer Methods
-    HRESULT GetCurrentLength(DWORD* pcbCurrentLength);
-    HRESULT GetMaxLength(DWORD* pcbMaxLength);
-    HRESULT Lock(BYTE** ppbBuffer, DWORD* pcbMaxLength, DWORD* pcbCurrentLength);
-    HRESULT SetCurrentLength(DWORD cbCurrentLength);
-    HRESULT Unlock();
-};
-
-// OK
-class WrappedBuffer : public IMFMediaBuffer
-{
-private:
-    ULONG m_nRefCount;
-    bool  m_isLocked;
-    BYTE* m_pBase;
-    DWORD m_maxLength;
-    DWORD m_curLength;
-
-    WrappedBuffer(BYTE* pBase, DWORD dwLength);
-    ~WrappedBuffer();
-
-public:
-    static HRESULT CreateInstance(WrappedBuffer** ppBuffer, BYTE* pBase, DWORD dwLength);
-
-    // IUnknown Methods
-    ULONG AddRef();
-    ULONG Release();
+    ULONG   AddRef();
+    ULONG   Release();
     HRESULT QueryInterface(REFIID iid, void** ppv);
 
     // IMFMediaBuffer Methods
