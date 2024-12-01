c84a26370e79851c62b4dd82aa778f67c228dd56
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-01 18:18:32 -0500

    update encoder_rm_zlt

--- hl2ss/hl2ss/encoder_rm_zlt.cpp
+++ hl2ss/hl2ss/encoder_rm_zlt.cpp
@@ -1,7 +1,7 @@
 
 #include <MemoryBuffer.h>
 #include "encoder_rm_zlt.h"
-#include "neon.h"
+#include "research_mode.h"
 
 #include <winrt/Windows.Foundation.Collections.h>
 
@@ -14,10 +14,39 @@ using namespace winrt::Windows::Storage::Streams;
 //-----------------------------------------------------------------------------
 
 // OK
-Encoder_RM_ZLT::Encoder_RM_ZLT(HOOK_RM_ZLT_PROC pHookCallback, void* pHookParam, H26xFormat const& format, ZABFormat const& zabFormat) : m_softwareBitmap(nullptr)
+void Encoder_RM_ZLT::ToBGRA8(uint8_t const* pSigma, uint16_t const* pDepth, uint16_t const* pAb, uint32_t* pBGRA8)
 {
+	uint16x8_t threshold = vdupq_n_u16(RM_ZLT_MASK);
+
+	for (int i = 0; i < (RM_ZLT_PIXELS / 32); ++i)
+	{
+	uint8x8x4_t  s = vld1_u8_x4(pSigma);
+	uint16x8x4_t d = vld1q_u16_x4(pDepth);
+
+	d.val[0] = vandq_u16(d.val[0], vcltq_u16(vmovl_u8(s.val[0]), threshold));
+	d.val[1] = vandq_u16(d.val[1], vcltq_u16(vmovl_u8(s.val[1]), threshold));
+	d.val[2] = vandq_u16(d.val[2], vcltq_u16(vmovl_u8(s.val[2]), threshold));
+	d.val[3] = vandq_u16(d.val[3], vcltq_u16(vmovl_u8(s.val[3]), threshold));
+
+	vst1q_u16_x4(pBGRA8, d);
+
+	pSigma += 32;
+	pDepth += 32;
+	pBGRA8 += 16;
+	}
+
+	memcpy(pBGRA8, pAb, RM_ZLT_ABSIZE);
+}
+
+// OK
+Encoder_RM_ZLT::Encoder_RM_ZLT(HOOK_RM_ZLT_PROC pHookCallback, void* pHookParam, H26xFormat& format, ZABFormat& zabFormat) : m_softwareBitmap(nullptr)
+{
+    format.width     = RM_ZLT_WIDTH;
+    format.height    = RM_ZLT_HEIGHT;
+    format.framerate = RM_ZLT_FPS;
+
     m_pngProperties.Insert(L"FilterOption", BitmapTypedValue(winrt::box_value(zabFormat.filter), winrt::Windows::Foundation::PropertyType::UInt8));
-    m_softwareBitmap = SoftwareBitmap(BitmapPixelFormat::Bgra8, format.width, format.height, BitmapAlphaMode::Straight);
+    m_softwareBitmap = SoftwareBitmap(BitmapPixelFormat::Bgra8, RM_ZLT_WIDTH, RM_ZLT_HEIGHT, BitmapAlphaMode::Straight);
     m_pHookCallback = pHookCallback;
     m_pHookParam = pHookParam;
 }
@@ -35,7 +64,7 @@ void Encoder_RM_ZLT::WriteSample(BYTE const* pSigma, UINT16 const* pDepth, UINT1
 
     spMemoryBufferByteAccess->GetBuffer(&pixelBufferData, &pixelBufferDataLength);
 
-    Neon_ZLTToBGRA8(pSigma, pDepth, pAbImage, (u32*)pixelBufferData);
+    ToBGRA8(pSigma, pDepth, pAbImage, (uint32_t*)pixelBufferData);
     }
 
     auto stream = InMemoryRandomAccessStream();
--- hl2ss/hl2ss/encoder_rm_zlt.h
+++ hl2ss/hl2ss/encoder_rm_zlt.h
@@ -6,7 +6,7 @@
 #include <winrt/Windows.Graphics.Imaging.h>
 #include <winrt/Windows.Storage.Streams.h>
 
-typedef void(*HOOK_RM_ZLT_PROC)(winrt::Windows::Storage::Streams::Buffer const&, LONGLONG, UINT8*, UINT32, void*);
+typedef void(*HOOK_RM_ZLT_PROC)(winrt::Windows::Storage::Streams::Buffer&, LONGLONG, UINT8*, UINT32, void*);
 
 class Encoder_RM_ZLT
 {
@@ -16,8 +16,10 @@ private:
     HOOK_RM_ZLT_PROC m_pHookCallback;
     void* m_pHookParam;
 
+    static void ToBGRA8(uint8_t const* pSigma, uint16_t const* pDepth, uint16_t const* pAb, uint32_t* pBGRA8);
+
 public:
-    Encoder_RM_ZLT(HOOK_RM_ZLT_PROC pHookCallback, void* pHookParam, H26xFormat const& format, ZABFormat const& zabFormat);
+    Encoder_RM_ZLT(HOOK_RM_ZLT_PROC pHookCallback, void* pHookParam, H26xFormat& format, ZABFormat& zabFormat);
 
     void WriteSample(BYTE const* pSigma, UINT16 const* pDepth, UINT16 const* pAbImage, LONGLONG timestamp, UINT8* metadata, UINT32 metadata_size);
 };
