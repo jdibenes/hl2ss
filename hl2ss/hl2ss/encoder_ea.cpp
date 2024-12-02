cdd56924b2d8c7c6ad35be78be47a7ff16789496
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-02 00:03:25 -0500

    update encoder_ea

--- hl2ss/hl2ss/encoder_ea.cpp
+++ hl2ss/hl2ss/encoder_ea.cpp
@@ -1,7 +1,6 @@
 
 #include <mfapi.h>
 #include "encoder_ea.h"
-#include "neon.h"
 
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Media.h>
@@ -9,6 +8,7 @@
 #include <winrt/Windows.Media.Capture.Frames.h>
 
 using namespace winrt::Windows::Media;
+using namespace winrt::Windows::Media::MediaProperties;
 using namespace winrt::Windows::Media::Capture::Frames;
 
 //-----------------------------------------------------------------------------
@@ -16,8 +16,102 @@ using namespace winrt::Windows::Media::Capture::Frames;
 //-----------------------------------------------------------------------------
 
 // OK
-Encoder_EA::Encoder_EA(HOOK_SINK_PROC pHookCallback, void* pHookParam, AACFormat const& format)
+void Encoder_EA::AudioF32ToS16(int16_t* out, float const* in, int32_t out_bytes)
 {
+	float32x4_t s = vdupq_n_f32(32767.0f);
+
+	for (int i = 0; i < (out_bytes / (sizeof(int16_t) * 16)); ++i)
+	{
+	float32x4x4_t f = vld1q_f32_x4(in);
+	uint16x4x4_t d;
+
+	d.val[0] = vmovn_s32(vcvtq_s32_f32(vmulq_f32(f.val[0], s)));
+	d.val[1] = vmovn_s32(vcvtq_s32_f32(vmulq_f32(f.val[1], s)));
+	d.val[2] = vmovn_s32(vcvtq_s32_f32(vmulq_f32(f.val[2], s)));
+	d.val[3] = vmovn_s32(vcvtq_s32_f32(vmulq_f32(f.val[3], s)));
+
+	vst1_u16_x4(out, d);
+
+	in  += 16;
+	out += 16;
+	}
+}
+
+// OK
+void Encoder_EA::AudioS16MonoToStereo(int16_t* out, int16_t const* in, int32_t in_bytes)
+{
+	for (int i = 0; i < (in_bytes / (sizeof(int16_t) * 32)); ++i)
+	{
+	int16x8x4_t f = vld1q_s16_x4(in);
+
+	for (int j = 0; j < 4; ++j)
+	{
+	int16x8x2_t d;
+
+	d.val[0] = f.val[j];
+	d.val[1] = f.val[j];
+
+	vst2q_s16(out, d);
+    
+	out += 16;
+	}
+
+	in  += 32;
+	}
+}
+
+// OK
+void Encoder_EA::F32ToS16(int16_t* out, float const* in, int32_t out_bytes)
+{
+    AudioF32ToS16(out, in, out_bytes);
+}
+
+// OK
+void Encoder_EA::S16ToS16(int16_t* out, float const* in, int32_t out_bytes)
+{
+    memcpy(out, in, out_bytes);
+}
+
+// OK
+void Encoder_EA::Clear(int16_t* out, float const* in, int32_t out_bytes)
+{
+    (void)in;
+    memset(out, 0, out_bytes);
+}
+
+// OK
+void Encoder_EA::ExtendMonoToStereo(int16_t* out, int16_t const* in, int32_t in_bytes)
+{
+    AudioS16MonoToStereo(out, in, in_bytes);
+}
+
+// OK
+void Encoder_EA::Bypass(int16_t* out, int16_t const* in, int32_t in_bytes)
+{
+    (void)out;
+    (void)in;
+    (void)in_bytes;
+}
+
+// OK
+Encoder_EA::Encoder_EA(HOOK_SINK_PROC pHookCallback, void* pHookParam, AudioSubtype subtype, AACFormat& format, uint32_t channels)
+{
+    format.samplerate = 48000;
+    format.channels   = 2;
+
+    switch (subtype)
+    {
+    case AudioSubtype::AudioSubtype_S16: m_kernel_cast = S16ToS16; m_sample_bytes = sizeof(int16_t); break;
+    case AudioSubtype::AudioSubtype_F32: m_kernel_cast = F32ToS16; m_sample_bytes = sizeof(float);   break;
+    default:                             m_kernel_cast = Clear;    m_sample_bytes = sizeof(int16_t); break;
+    }
+
+    switch (channels)
+    {
+    case 1:  m_kernel_wide = ExtendMonoToStereo; m_fill = 1; break;
+    default: m_kernel_wide = Bypass;             m_fill = 0; break;
+    }
+
     m_pSinkWriter = CustomSinkWriter::CreateForAudio(pHookCallback, pHookParam, AudioSubtype::AudioSubtype_S16, format);
 }
 
@@ -28,40 +122,25 @@ void Encoder_EA::WriteSample(MediaFrameReference const& frame, int64_t timestamp
     IMFMediaBuffer* pBuffer; // Release
     BYTE* pDst;
 
-    AudioMediaFrame amf       = frame.AudioMediaFrame();
-    AudioFrame      audio     = amf.GetAudioFrame();
-    AudioBuffer     buffer    = audio.LockBuffer(AudioBufferAccessMode::Read);
-    auto const&     reference = buffer.CreateReference();
-    auto const&     aep       = amf.AudioEncodingProperties();
+    auto amf       = frame.AudioMediaFrame();
+    auto audio     = amf.GetAudioFrame();
+    auto buffer    = audio.LockBuffer(AudioBufferAccessMode::Read);
+    auto reference = buffer.CreateReference();
 
-    uint32_t bytes_per_sample = aep.BitsPerSample() / 8;
-    uint32_t channels         = aep.ChannelCount();
-
-    uint32_t input_bytes   = reference.Capacity();
-    uint32_t input_samples = input_bytes / bytes_per_sample;
-    uint32_t output_bytes  = input_samples * sizeof(int16_t);
-    uint32_t fill_bytes    = (channels == 1) ? output_bytes : 0;
-    uint32_t buffer_bytes  = output_bytes + fill_bytes;
+    uint32_t output_bytes = (reference.Capacity() / m_sample_bytes) * sizeof(int16_t);
+    uint32_t fill_bytes   = m_fill * output_bytes;
+    uint32_t buffer_bytes = output_bytes + fill_bytes;
 
     MFCreateMemoryBuffer(buffer_bytes, &pBuffer);
 
     pBuffer->Lock(&pDst, NULL, NULL);
 
-    float*   src_addr  = (float*)reference.data();
-    int16_t* base_addr = (int16_t*)pDst;
     int16_t* high_addr = (int16_t*)(pDst + fill_bytes);
 
-    switch (bytes_per_sample)
-    {
-    case 4:  Neon_F32ToS16(src_addr, input_samples, high_addr); break;
-    case 2:  memcpy(high_addr, src_addr, output_bytes);         break;
-    default: memset(high_addr, 0, output_bytes);
-    }
-
-    if (channels == 1) { Neon_S16MonoToStereo(high_addr, input_samples, base_addr); }
+    m_kernel_cast(high_addr,      (float*)reference.data(), output_bytes);
+    m_kernel_wide((int16_t*)pDst, high_addr,                output_bytes);
 
     pBuffer->Unlock();
-
     pBuffer->SetCurrentLength(buffer_bytes);
 
     reference.Close();
--- hl2ss/hl2ss/encoder_ea.h
+++ hl2ss/hl2ss/encoder_ea.h
@@ -5,13 +5,30 @@
 
 #include <winrt/Windows.Media.Capture.Frames.h>
 
+typedef void(*EA_CAST_KERNEL)(int16_t*, float   const*, int32_t);
+typedef void(*EA_WIDE_KERNEL)(int16_t*, int16_t const*, int32_t);
+
 class Encoder_EA
 {
 private:
     std::unique_ptr<CustomSinkWriter> m_pSinkWriter;
+    uint32_t m_sample_bytes;
+    uint32_t m_fill;
+    EA_CAST_KERNEL m_kernel_cast;
+    EA_WIDE_KERNEL m_kernel_wide;
+
+    static void AudioF32ToS16(int16_t* out, float const* in, int32_t out_bytes);
+    static void AudioS16MonoToStereo(int16_t* out, int16_t const* in, int32_t in_bytes);
+
+    static void F32ToS16(int16_t* out, float const* in, int32_t out_bytes);
+    static void S16ToS16(int16_t* out, float const* in, int32_t out_bytes);
+    static void Clear(int16_t* out, float const* in, int32_t out_bytes);
+
+    static void ExtendMonoToStereo(int16_t*, int16_t const*, int32_t);
+    static void Bypass(int16_t*, int16_t const*, int32_t);
 
 public:
-    Encoder_EA(HOOK_SINK_PROC pHookCallback, void* pHookParam, AACFormat const& format);
+    Encoder_EA(HOOK_SINK_PROC pHookCallback, void* pHookParam, AudioSubtype subtype, AACFormat& format, uint32_t channels);
 
     void WriteSample(winrt::Windows::Media::Capture::Frames::MediaFrameReference const& frame, int64_t timestamp);
 };
