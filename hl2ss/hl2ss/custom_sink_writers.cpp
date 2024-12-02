a2f5d9874f7c088d93139d8aa179e5a609725c57
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-02 14:50:06 -0500

    Update custom_sink_writers.cpp

--- hl2ss/hl2ss/custom_sink_writers.cpp
+++ hl2ss/hl2ss/custom_sink_writers.cpp
@@ -3,67 +3,9 @@
 #include <codecapi.h>
 #include "custom_sink_writers.h"
 
-struct AVOption
-{
-	GUID guid;
-	uint32_t vt;
-};
-
-//-----------------------------------------------------------------------------
-// Global Variables
-//-----------------------------------------------------------------------------
-
-static AVOption g_AVLUT[] =
-{
-	{CODECAPI_AVEncCommonRateControlMode, VT_UI4},
-	{CODECAPI_AVEncCommonQuality, VT_UI4},
-	{CODECAPI_AVEncAdaptiveMode, VT_UI4},
-	{CODECAPI_AVEncCommonBufferSize, VT_UI4},
-	{CODECAPI_AVEncCommonMaxBitRate, VT_UI4},
-	{CODECAPI_AVEncCommonMeanBitRate, VT_UI4},
-	{CODECAPI_AVEncCommonQualityVsSpeed, VT_UI4},
-	{CODECAPI_AVEncH264CABACEnable, VT_BOOL},
-	{CODECAPI_AVEncH264SPSID, VT_UI4},
-	{CODECAPI_AVEncMPVDefaultBPictureCount, VT_UI4},
-	{CODECAPI_AVEncMPVGOPSize, VT_UI4},
-	{CODECAPI_AVEncNumWorkerThreads, VT_UI4},
-	{CODECAPI_AVEncVideoContentType, VT_UI4},
-	{CODECAPI_AVEncVideoEncodeQP, VT_UI8},
-	{CODECAPI_AVEncVideoForceKeyFrame, VT_UI4},
-	{CODECAPI_AVEncVideoMinQP, VT_UI4},
-	{CODECAPI_AVLowLatencyMode, VT_BOOL},
-	{CODECAPI_AVEncVideoMaxQP, VT_UI4},
-	{CODECAPI_VideoEncoderDisplayContentType, VT_UI4},
-};
-
 //-----------------------------------------------------------------------------
 // Functions
 //-----------------------------------------------------------------------------
-
-// OK
-static void TranslateEncoderOptions(std::vector<uint64_t> const& options, IMFAttributes **pEncoderAttr)
-{
-	size_t size = options.size() & ~1ULL;
-
-	MFCreateAttributes(pEncoderAttr, static_cast<UINT32>(size / 2));
-
-	for (int i = 0; i < (int)size; i += 2)
-	{
-	uint64_t option = options[i];
-	uint64_t value  = options[i + 1];
-
-	if (option >= (sizeof(g_AVLUT) / sizeof(AVOption))) { continue; }
-
-	AVOption entry = g_AVLUT[option];
-
-	switch (entry.vt)
-	{
-	case VT_UI4:  (*pEncoderAttr)->SetUINT32(entry.guid, static_cast<UINT32>(value));                  break;
-	case VT_UI8:  (*pEncoderAttr)->SetUINT64(entry.guid, value);                                       break;
-	case VT_BOOL: (*pEncoderAttr)->SetUINT32(entry.guid, (value == 0) ? VARIANT_FALSE : VARIANT_TRUE); break;
-	}
-	}
-}
 
 // OK
 CustomSinkWriter::CustomSinkWriter(HOOK_SINK_PROC hookproc, void* hookparam, IMFMediaType* pInputType, IMFMediaType* pOutputType, IMFAttributes* pEncoderAttr)
