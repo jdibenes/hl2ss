69c04119f584369f3945ca55b9db89b1234aa21f
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-06 01:43:49 -0500

    Update ipc_sc.h

--- hl2ss/hl2ss/ipc_sc.h
+++ hl2ss/hl2ss/ipc_sc.h
@@ -6,11 +6,12 @@
 #include "custom_video_effect.h"
 #include "custom_audio_effect.h"
 
+bool ReceiveOperatingMode(SOCKET clientsocket, uint8_t& mode);
 bool ReceiveAACFormat_Profile(SOCKET clientsocket, AACFormat& profile);
 bool ReceiveH26xFormat_Video(SOCKET clientsocket, H26xFormat& format);
 bool ReceiveH26xFormat_Divisor(SOCKET clientsocket, H26xFormat& format);
 bool ReceiveH26xFormat_Profile(SOCKET clientsocket, H26xFormat& format);
-bool ReceiveH26xEncoder_Options(SOCKET clientsocket, std::vector<uint64_t>& options);
+bool ReceiveEncoderOptions(SOCKET clientsocket, std::vector<uint64_t>& options);
 bool ReceiveZABFormat_PNGFilter(SOCKET clientsocket, ZABFormat& format);
 bool ReceiveZABFormat_Profile(SOCKET clientsocket, ZABFormat& format);
 bool ReceiveMRCVideoOptions(SOCKET clientsocket, MRCVideoOptions& options);
