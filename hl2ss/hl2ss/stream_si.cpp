2741692a26f18c6b3c33faec63b5fb02c91cbf62
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-09 01:02:07 -0500

    Update stream_si.cpp

--- hl2ss/hl2ss/stream_si.cpp
+++ hl2ss/hl2ss/stream_si.cpp
@@ -1,16 +1,10 @@
 
 #include "spatial_input.h"
-#include "locator.h"
 #include "channel.h"
 #include "ports.h"
-#include "timestamps.h"
 
-#include <winrt/Windows.Perception.h>
-#include <winrt/Windows.Perception.Spatial.h>
 #include <winrt/Windows.Perception.People.h>
 
-using namespace winrt::Windows::Perception;
-using namespace winrt::Windows::Perception::Spatial;
 using namespace winrt::Windows::Perception::People;
 
 class Channel_SI : Channel
@@ -19,6 +13,12 @@ private:
     bool Startup();
     void Run();
     void Cleanup();
+
+    void Execute_Mode0();
+
+    void OnFrameArrived(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, JointPose* left_hand, JointPose* right_hand, UINT64 timestamp);
+
+    static void Thunk_Sensor(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, JointPose* left_hand, JointPose* right_hand, UINT64 timestamp, void* self);
 
 public:
     Channel_SI(char const* name, char const* port, uint32_t id);
@@ -35,6 +35,39 @@ static std::unique_ptr<Channel_SI> g_channel;
 //-----------------------------------------------------------------------------
 
 // OK
+void Channel_SI::Thunk_Sensor(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, JointPose* left_hand, JointPose* right_hand, UINT64 timestamp, void* self)
+{
+    static_cast<Channel_SI*>(self)->OnFrameArrived(valid, head_pose, eye_ray, left_hand, right_hand, timestamp);
+}
+
+// OK
+void Channel_SI::OnFrameArrived(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, JointPose* left_hand, JointPose* right_hand, UINT64 timestamp)
+{
+    int32_t const hand_size   = HAND_JOINTS * sizeof(JointPose);
+    int32_t const packet_size = sizeof(uint32_t) + sizeof(SpatialInput_Frame) + sizeof(SpatialInput_Ray) + (2 * hand_size);
+
+    WSABUF wsaBuf[7];
+    bool ok;
+
+    pack_buffer(wsaBuf, 0, &timestamp,   sizeof(timestamp));
+    pack_buffer(wsaBuf, 1, &packet_size, sizeof(packet_size));
+    pack_buffer(wsaBuf, 2, &valid,       sizeof(uint32_t));
+    pack_buffer(wsaBuf, 3, head_pose,    sizeof(SpatialInput_Frame));
+    pack_buffer(wsaBuf, 4, eye_ray,      sizeof(SpatialInput_Ray));
+    pack_buffer(wsaBuf, 5, left_hand,    hand_size);
+    pack_buffer(wsaBuf, 6, right_hand,   hand_size);
+
+    ok = send_multiple(m_socket_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
+    if (!ok) { SetEvent(m_event_client); }
+}
+
+// OK
+void Channel_SI::Execute_Mode0()
+{
+    SpatialInput_ExecuteSensorLoop(Thunk_Sensor, this, m_event_client);
+}
+
+// OK
 Channel_SI::Channel_SI(char const* name, char const* port, uint32_t id) :
 Channel(name, port, id)
 {
@@ -43,53 +76,13 @@ Channel(name, port, id)
 // OK
 bool Channel_SI::Startup()
 {
-    return SpatialInput_WaitForEyeConsent();
+    return SpatialInput_WaitForConsent();
 }
 
 // OK
 void Channel_SI::Run()
 {
-    int     const hand_size   = HAND_JOINTS * sizeof(JointPose);
-    int32_t const packet_size = sizeof(uint32_t) + sizeof(SpatialInput_Frame) + sizeof(SpatialInput_Ray) + (2 * hand_size);
-
-    PerceptionTimestamp ts = nullptr;
-    SpatialCoordinateSystem world = nullptr;
-    UINT64 qpc;
-    uint32_t valid;
-    SpatialInput_Frame head_pose;
-    SpatialInput_Ray eye_ray;
-    std::vector<JointPose> left_poses;
-    std::vector<JointPose> right_poses;    
-    WSABUF wsaBuf[7];
-    bool ok;
-    
-    left_poses.resize(HAND_JOINTS);
-    right_poses.resize(HAND_JOINTS);
-
-    pack_buffer(wsaBuf, 0, &qpc,               sizeof(qpc));
-    pack_buffer(wsaBuf, 1, &packet_size,       sizeof(packet_size));
-    pack_buffer(wsaBuf, 2, &valid,             sizeof(valid));
-    pack_buffer(wsaBuf, 3, &head_pose,         sizeof(head_pose));
-    pack_buffer(wsaBuf, 4, &eye_ray,           sizeof(eye_ray));
-    pack_buffer(wsaBuf, 5, left_poses.data(),  hand_size);
-    pack_buffer(wsaBuf, 6, right_poses.data(), hand_size);
-
-    do
-    {
-    Sleep(1000 / 30);
-
-    qpc   = GetCurrentQPCTimestamp();
-    ts    = QPCTimestampToPerceptionTimestamp(qpc);
-    world = Locator_GetWorldCoordinateSystem(ts);
-
-    int status1 = SpatialInput_GetHeadPoseAndEyeRay(world, ts, head_pose, eye_ray);
-    int status2 = SpatialInput_GetHandPose(world, ts, left_poses, right_poses);
-
-    valid = status1 | (status2 << 2);
-
-    ok = send_multiple(m_socket_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
-    }
-    while (ok);
+    Execute_Mode0();
 }
 
 // OK
