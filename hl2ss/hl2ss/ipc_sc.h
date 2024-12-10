b78fe99fa76b4746b3a7b7c1ae0301a6b56441bd
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-10 16:05:35 -0500

    Update ipc_sc.h

--- hl2ss/hl2ss/ipc_sc.h
+++ hl2ss/hl2ss/ipc_sc.h
@@ -6,13 +6,14 @@
 #include "custom_video_effect.h"
 #include "custom_audio_effect.h"
 
-bool ReceiveOperatingMode(SOCKET clientsocket, uint8_t& mode);
-bool ReceiveAACFormat_Profile(SOCKET clientsocket, AACFormat& profile);
-bool ReceiveH26xFormat_Video(SOCKET clientsocket, H26xFormat& format);
-bool ReceiveH26xFormat_Divisor(SOCKET clientsocket, H26xFormat& format);
-bool ReceiveH26xFormat_Profile(SOCKET clientsocket, H26xFormat& format);
-bool ReceiveEncoderOptions(SOCKET clientsocket, std::vector<uint64_t>& options);
-bool ReceiveZABFormat_PNGFilter(SOCKET clientsocket, ZABFormat& format);
-bool ReceiveZABFormat_Profile(SOCKET clientsocket, ZABFormat& format);
-bool ReceiveMRCVideoOptions(SOCKET clientsocket, MRCVideoOptions& options);
-bool ReceiveMRCAudioOptions(SOCKET clientsocket, MRCAudioOptions& options);
+bool ReceiveOperatingMode(SOCKET socket, HANDLE event_error, uint8_t& mode);
+bool ReceiveAACFormat_Profile(SOCKET socket, HANDLE event_error, AACFormat& profile);
+bool ReceiveH26xFormat_Video(SOCKET socket, HANDLE event_error, H26xFormat& format);
+bool ReceiveH26xFormat_Divisor(SOCKET socket, HANDLE event_error, H26xFormat& format);
+bool ReceiveH26xFormat_Profile(SOCKET socket, HANDLE event_error, H26xFormat& format);
+bool ReceiveEncoderOptions(SOCKET socket, HANDLE event_error, std::vector<uint64_t>& options);
+bool ReceiveZABFormat_PNGFilter(SOCKET socket, HANDLE event_error, ZABFormat& format);
+bool ReceiveZABFormat_Profile(SOCKET socket, HANDLE event_error, ZABFormat& format);
+bool ReceiveMRCVideoOptions(SOCKET socket, HANDLE event_error, MRCVideoOptions& options);
+bool ReceiveMRCAudioOptions(SOCKET socket, HANDLE event_error, MRCAudioOptions& options);
+bool ReceiveEETFramerate(SOCKET socket, HANDLE event_error, uint8_t& fps);
