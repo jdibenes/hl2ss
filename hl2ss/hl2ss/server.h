a0ba172e26a533404ae9d581f05e015a9d7d1659
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-10 16:08:47 -0500

    Update server.h

--- hl2ss/hl2ss/server.h
+++ hl2ss/hl2ss/server.h
@@ -3,21 +3,14 @@
 
 #include <winsock2.h>
 
-struct HookCallbackSocket
-{
-    SOCKET clientsocket;
-    HANDLE clientevent;
-    void* format;
-};
-
 bool InitializeSockets();
 SOCKET CreateSocket(char const* port);
 bool CleanupSockets();
 
-bool recv_u8(SOCKET socket, uint8_t& byte);
-bool recv_u16(SOCKET socket, uint16_t& word);
-bool recv_u32(SOCKET socket, uint32_t& dword);
-bool recv(SOCKET clientsocket, char* buf, int bytes);
+bool recv_u8(SOCKET socket, HANDLE event_error, uint8_t& byte);
+bool recv_u16(SOCKET socket, HANDLE event_error, uint16_t& word);
+bool recv_u32(SOCKET socket, HANDLE event_error, uint32_t& dword);
+bool recv(SOCKET socket, HANDLE event_error, void* buffer, int buffer_size);
 
-bool send_multiple(SOCKET s, LPWSABUF buffers, DWORD dwBufferCount);
-void pack_buffer(LPWSABUF dst, int index, void const* buffer, ULONG length);
+void pack_buffer(LPWSABUF buffers, int index, void const* buffer, ULONG buffer_size);
+bool send_multiple(SOCKET socket, HANDLE event_error, LPWSABUF buffers, DWORD buffer_count);
