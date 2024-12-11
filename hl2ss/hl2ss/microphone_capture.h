def976aa9b35fa55022ceb98702a02c5230873b0
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-11 18:01:12 -0500

    update microphone_capture

--- hl2ss/hl2ss/microphone_capture.h
+++ hl2ss/hl2ss/microphone_capture.h
@@ -1,39 +1,13 @@
 
 #pragma once
 
-#include <mmdeviceapi.h>
-#include <AudioClient.h>
-#include <mfidl.h>
-#include <mfreadwrite.h>
+#include <Windows.h>
 
-#include <winrt/Windows.Foundation.h>
+typedef void (*HOOK_MC_PROC)(BYTE*, UINT32, bool, UINT64, void*);
 
-// OK
-class MicrophoneCapture : public winrt::implements<MicrophoneCapture, IActivateAudioInterfaceCompletionHandler>
-{
-private:
-	winrt::com_ptr<IAudioClient3> m_audioClient;
-	winrt::com_ptr<IAudioCaptureClient> m_audioCaptureClient;
-	WAVEFORMATEX* m_wfx; // CoTaskMemFree
-	HANDLE m_eventActivate; // CloseHandle
-	HANDLE m_eventData; // CloseHandle
-	bool m_raw;
-	bool m_status;
-
-	HRESULT Configure(IActivateAudioInterfaceAsyncOperation* operation);
-
-public:
-	MicrophoneCapture();
-	~MicrophoneCapture();
-
-	bool Initialize(bool raw);
-	void Activate(); // Call from the main thread
-	bool WaitActivate(DWORD milliseconds);
-	void Start();
-	bool Status();
-	void Stop();
-	void WriteSample(IMFSinkWriter* pSinkWriter, DWORD dwAudioIndex);
-
-	// IActivateAudioInterfaceCompletionHandler Methods
-	STDMETHOD(ActivateCompleted)(IActivateAudioInterfaceAsyncOperation* operation);
-};
+void MicrophoneCapture_Startup();
+void MicrophoneCapture_Cleanup();
+void MicrophoneCapture_Open(bool raw);
+void MicrophoneCapture_Close();
+bool MicrophoneCapture_Status();
+void MicrophoneCapture_ExecuteSensorLoop(HOOK_MC_PROC hook, void* param, HANDLE event_stop);
