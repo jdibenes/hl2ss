ffa5910248cb7d30123839924662a37b8f63d509
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-12 17:46:10 -0500

    Update stream_rm_zht.cpp

--- hl2ss/hl2ss/stream_rm_zht.cpp
+++ hl2ss/hl2ss/stream_rm_zht.cpp
@@ -3,24 +3,14 @@
 // https://github.com/microsoft/HoloLens2ForCV/issues/133
 
 #include "research_mode.h"
-#include "locator.h"
-#include "channel.h"
-#include "ipc_sc.h"
-#include "ports.h"
+#include "server_channel.h"
+#include "server_settings.h"
 #include "encoder_rm_zht.h"
-#include "timestamps.h"
-
-#include <winrt/Windows.Perception.h>
-#include <winrt/Windows.Perception.Spatial.h>
-
-using namespace winrt::Windows::Perception;
-using namespace winrt::Windows::Perception::Spatial;
 
 class Channel_RM_ZHT : public Channel
 {
 private:
     IResearchModeSensor* m_sensor;
-    SpatialLocator m_locator = nullptr;
     std::unique_ptr<Encoder_RM_ZHT> m_pEncoder;
     bool m_enable_location;    
     uint32_t m_counter;
@@ -35,11 +25,11 @@ private:
 
     void OnFrameArrived(IResearchModeSensorFrame* frame);
     void OnFrameProcess(UINT16 const* depth, UINT16 const* ab, UINT64 host_ticks, UINT64 sensor_ticks);
-    void OnEncodingComplete(void* encoded, DWORD encoded_size, LONGLONG sample_time, void* metadata, UINT32 metadata_size);
+    void OnEncodingComplete(void* encoded, DWORD encoded_size, UINT32 clean_point, LONGLONG sample_time, void* metadata, UINT32 metadata_size);
 
     static void Thunk_Sensor(IResearchModeSensorFrame* frame, void* self);
     static void Thunk_Sample(UINT16 const* depth, UINT16 const* ab, UINT64 host_ticks, UINT64 sensor_ticks, void* self);
-    static void Thunk_Encoder(void* encoded, DWORD encoded_size, LONGLONG sample_time, void* metadata, UINT32 metadata_size, void* self);
+    static void Thunk_Encoder(void* encoded, DWORD encoded_size, UINT32 clean_point, LONGLONG sample_time, void* metadata, UINT32 metadata_size, void* self);
 
 public:
     Channel_RM_ZHT(char const* name, char const* port, uint32_t id);
@@ -68,9 +58,9 @@ void Channel_RM_ZHT::Thunk_Sample(UINT16 const* depth, UINT16 const* ab, UINT64 
 }
 
 // OK
-void Channel_RM_ZHT::Thunk_Encoder(void* encoded_frame, DWORD encoded_size, LONGLONG sample_time, void* metadata, UINT32 metadata_size, void* self)
+void Channel_RM_ZHT::Thunk_Encoder(void* encoded_frame, DWORD encoded_size, UINT32 clean_point, LONGLONG sample_time, void* metadata, UINT32 metadata_size, void* self)
 {
-    static_cast<Channel_RM_ZHT*>(self)->OnEncodingComplete(encoded_frame, encoded_size, sample_time, metadata, metadata_size);
+    static_cast<Channel_RM_ZHT*>(self)->OnEncodingComplete(encoded_frame, encoded_size, clean_point, sample_time, metadata, metadata_size);
 }
 
 // OK
@@ -83,20 +73,20 @@ void Channel_RM_ZHT::OnFrameArrived(IResearchModeSensorFrame* frame)
 // OK
 void Channel_RM_ZHT::OnFrameProcess(UINT16 const* depth, UINT16 const* ab, UINT64 host_ticks, UINT64 sensor_ticks)
 {
-    PerceptionTimestamp ts = QPCTimestampToPerceptionTimestamp(host_ticks);
     RM_ZHT_Metadata metadata;
 
     metadata.timestamp    = host_ticks;
     metadata.sensor_ticks = sensor_ticks;
     metadata.z            = NULL;
-    metadata.pose         = Locator_Locate(ts, m_locator, Locator_GetWorldCoordinateSystem(ts));
+    metadata.pose         = ResearchMode_GetRigNodeWorldPose(host_ticks);
 
     m_pEncoder->WriteSample(depth, ab, host_ticks, &metadata);
 }
 
 // OK
-void Channel_RM_ZHT::OnEncodingComplete(void* encoded, DWORD encoded_size, LONGLONG sample_time, void* metadata, UINT32 metadata_size)
+void Channel_RM_ZHT::OnEncodingComplete(void* encoded, DWORD encoded_size, UINT32 clean_point, LONGLONG sample_time, void* metadata, UINT32 metadata_size)
 {
+    (void)clean_point;
     (void)sample_time;
     (void)metadata_size;
 
@@ -104,7 +94,6 @@ void Channel_RM_ZHT::OnEncodingComplete(void* encoded, DWORD encoded_size, LONGL
     uint8_t* z;
     ULONG size_z;
     WSABUF wsaBuf[8];
-    bool ok;
 
     if (p->z != NULL) { z = p->z->data(); size_z = static_cast<uint32_t>(p->z->size()); }
     else              { z = NULL;         size_z = 0; }
@@ -121,8 +110,7 @@ void Channel_RM_ZHT::OnEncodingComplete(void* encoded, DWORD encoded_size, LONGL
     pack_buffer(wsaBuf, 6, &p->sensor_ticks, sizeof(p->sensor_ticks));
     pack_buffer(wsaBuf, 7, &p->pose,         sizeof(p->pose) * m_enable_location);
 
-    ok = send_multiple(m_socket_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
-    if (!ok) { SetEvent(m_event_client); }
+    send_multiple(m_socket_client, m_event_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
 }
 
 // OK
@@ -135,16 +123,16 @@ void Channel_RM_ZHT::Execute_Mode0(bool enable_location)
 
     Encoder_RM_ZHT::SetH26xFormat(format);
 
-    ok = ReceiveH26xFormat_Divisor(m_socket_client, format);
+    ok = ReceiveH26xFormat_Divisor(m_socket_client, m_event_client, format);
     if (!ok) { return; }
 
-    ok = ReceiveZABFormat_Profile(m_socket_client, zabformat);
+    ok = ReceiveZABFormat_Profile(m_socket_client, m_event_client, zabformat);
     if (!ok) { return; }
 
-    ok = ReceiveH26xFormat_Profile(m_socket_client, format);
+    ok = ReceiveH26xFormat_Profile(m_socket_client, m_event_client, format);
     if (!ok) { return; }
 
-    ok = ReceiveEncoderOptions(m_socket_client, options);
+    ok = ReceiveEncoderOptions(m_socket_client, m_event_client, options);
     if (!ok) { return; }
 
     m_pEncoder        = std::make_unique<Encoder_RM_ZHT>(Thunk_Encoder, this, format, zabformat, options);
@@ -183,15 +171,14 @@ void Channel_RM_ZHT::Execute_Mode2()
     pack_buffer(wsaBuf, 6, mapy.data(),         (ULONG)(mapy.size() * sizeof(float)));
     pack_buffer(wsaBuf, 7, K,                   sizeof(K));
 
-    send_multiple(m_socket_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
+    send_multiple(m_socket_client, m_event_client, wsaBuf, sizeof(wsaBuf) / sizeof(WSABUF));
 }
 
 // OK
 Channel_RM_ZHT::Channel_RM_ZHT(char const* name, char const* port, uint32_t id) :
 Channel(name, port, id)
 {
-    m_sensor  = ResearchMode_GetSensor(ResearchModeSensorType::DEPTH_AHAT);
-    m_locator = ResearchMode_GetLocator();
+    m_sensor = ResearchMode_GetSensor(ResearchModeSensorType::DEPTH_AHAT);
 }
 
 // OK
@@ -206,7 +193,7 @@ void Channel_RM_ZHT::Run()
     uint8_t mode;
     bool ok;
 
-    ok = ReceiveOperatingMode(m_socket_client, mode);
+    ok = ReceiveOperatingMode(m_socket_client, m_event_client, mode);
     if (!ok) { return; }
 
     switch (mode & 3)
@@ -223,9 +210,9 @@ void Channel_RM_ZHT::Cleanup()
 }
 
 // OK
-void RM_ZHT_Initialize()
+void RM_ZHT_Startup()
 {
-    g_channel = std::make_unique<Channel_RM_ZHT>("RM_ZHT", PORT_NAME_RM_ZHT, PORT_NUMBER_RM_ZHT - PORT_NUMBER_BASE);
+    g_channel = std::make_unique<Channel_RM_ZHT>("RM_ZHT", PORT_NAME_RM_ZHT, PORT_ID_RM_ZHT);
 }
 
 // OK
