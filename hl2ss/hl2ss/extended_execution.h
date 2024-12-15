79ad1e4138bfbcecef22c16b6e1cd1c4969f5c07
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-15 18:41:25 -0500

    Update extended_execution.h

--- hl2ss/hl2ss/extended_execution.h
+++ hl2ss/hl2ss/extended_execution.h
@@ -1,9 +1,12 @@
 
 #pragma once
 
-void ExtendedExecution_Request();
-bool ExtendedExecution_Status();
+#include <functional>
+#include <stdint.h>
+
+void ExtendedExecution_Initialize();
 void ExtendedExecution_SetFlatMode(bool flat);
 bool ExtendedExecution_GetFlatMode();
 void ExtendedExecution_SetInterfacePriority(uint32_t id, int32_t priority);
 int32_t ExtendedExecution_GetInterfacePriority(uint32_t id);
+void ExtendedExecution_RunOnMainThread(std::function<void()> f);
