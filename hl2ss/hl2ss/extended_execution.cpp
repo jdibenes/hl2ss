a65a74e7b04e1e44fb6bddb155c5f5b8821ab46d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-15 18:58:41 -0500

    update extended_execution

--- hl2ss/hl2ss/extended_execution.cpp
+++ hl2ss/hl2ss/extended_execution.cpp
@@ -1,17 +1,19 @@
 
+#include <Windows.h>
 #include <atomic>
+#include <functional>
 #include "log.h"
 
 #include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.ApplicationModel.h>
-#include <winrt/Windows.ApplicationModel.ExtendedExecution.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.ApplicationModel.ExtendedExecution.Foreground.h>
+#include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.Storage.h>
 
 using namespace winrt::Windows::Foundation;
-using namespace winrt::Windows::ApplicationModel;
-using namespace winrt::Windows::ApplicationModel::ExtendedExecution;
+using namespace winrt::Windows::ApplicationModel::Core;
 using namespace winrt::Windows::ApplicationModel::ExtendedExecution::Foreground;
+using namespace winrt::Windows::UI::Core;
 using namespace winrt::Windows::Storage;
 
 //-----------------------------------------------------------------------------
@@ -19,11 +21,10 @@ using namespace winrt::Windows::Storage;
 //-----------------------------------------------------------------------------
 
 static wchar_t const* g_flat_name = L"flat_mode.cfg";
-
+static int const g_interface_slots = 32;
 static ExtendedExecutionForegroundSession g_eefs = nullptr;
 static bool g_status = false;
-
-static std::atomic<int32_t> g_interface_priority[32];
+static std::atomic<int32_t> g_interface_priority[g_interface_slots];
 
 //-----------------------------------------------------------------------------
 // Functions
@@ -33,27 +34,25 @@ static std::atomic<int32_t> g_interface_priority[32];
 static void ExtendedExecution_OnRevoked(IInspectable const& sender, ExtendedExecutionForegroundRevokedEventArgs const& args)
 {
 	(void)sender;
-	ShowMessage("EEFS Revoked: %d", (int)args.Reason());
+	ShowMessage("EEFS Revoked: %d", static_cast<int>(args.Reason()));
 	g_status = false;
 }
 
 // OK
-void ExtendedExecution_Request()
+void ExtendedExecution_Initialize()
 {
 	g_eefs = ExtendedExecutionForegroundSession();
 	g_eefs.Reason(ExtendedExecutionForegroundReason::Unconstrained);
 	g_eefs.Description(L"Background Capture");
 	g_eefs.Revoked(ExtendedExecution_OnRevoked);
-
-	ExtendedExecutionForegroundResult result = g_eefs.RequestExtensionAsync().get();
-	if (result == ExtendedExecutionForegroundResult::Allowed) { g_status = true; }
-	ShowMessage("EEFS Result: %d", (int)result);	
+	g_status = g_eefs.RequestExtensionAsync().get() == ExtendedExecutionForegroundResult::Allowed;
+	ShowMessage("EEFS Result: %d", g_status);	
 }
 
 // OK
-bool ExtendedExecution_Status()
+void ExtendedExecution_RunOnMainThread(std::function<void()> f)
 {
-	return g_status;
+	CoreApplication::MainView().Dispatcher().RunAsync(CoreDispatcherPriority::High, f).get();
 }
 
 // OK
@@ -64,15 +63,15 @@ void ExtendedExecution_SetFlatMode(bool flat)
 
 	try
 	{
-		if (flat)
-		{
-			folder.CreateFileAsync(name).get();
-		}
-		else
-		{
-			StorageFile file = folder.GetFileAsync(name).get();
-			file.DeleteAsync().get();
-		}
+	if (flat)
+	{
+	folder.CreateFileAsync(name).get();
+	}
+	else
+	{
+	StorageFile file = folder.GetFileAsync(name).get();
+	file.DeleteAsync().get();
+	}
 	}
 	catch (...)
 	{
@@ -87,8 +86,8 @@ bool ExtendedExecution_GetFlatMode()
 
 	try
 	{
-		folder.GetFileAsync(name).get();
-		return true;
+	folder.GetFileAsync(name).get();
+	return true;
 	}
 	catch (...)
 	{
@@ -100,13 +99,14 @@ bool ExtendedExecution_GetFlatMode()
 // OK
 void ExtendedExecution_SetInterfacePriority(uint32_t id, int32_t priority)
 {
-	if (id >= 32) { return; }
-	if ((priority < -2) || (priority > 2)) { return; }
+	if (id >= g_interface_slots) { return; }
+	if ((priority < THREAD_PRIORITY_LOWEST) || (priority > THREAD_PRIORITY_HIGHEST)) { return; }
 	g_interface_priority[id] = priority;
 }
 
 // OK
 int32_t ExtendedExecution_GetInterfacePriority(uint32_t id)
 {
+	if (id >= g_interface_slots) { return THREAD_PRIORITY_NORMAL; }
 	return g_interface_priority[id];
 }
--- hl2ss/hl2ss/extended_execution.h
+++ hl2ss/hl2ss/extended_execution.h
@@ -5,8 +5,9 @@
 #include <stdint.h>
 
 void ExtendedExecution_Initialize();
+void ExtendedExecution_RunOnMainThread(std::function<void()> f);
+
 void ExtendedExecution_SetFlatMode(bool flat);
 bool ExtendedExecution_GetFlatMode();
 void ExtendedExecution_SetInterfacePriority(uint32_t id, int32_t priority);
 int32_t ExtendedExecution_GetInterfacePriority(uint32_t id);
-void ExtendedExecution_RunOnMainThread(std::function<void()> f);
