3bf867538210d1cee41a4f02c3a5d57e043ee8a3
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-14 22:30:53 -0500

    Update extended_video.h

--- hl2ss/hl2ss/extended_video.h
+++ hl2ss/hl2ss/extended_video.h
@@ -4,16 +4,16 @@
 #include <Windows.h>
 #include "custom_media_types.h"
 #include "custom_video_effect.h"
-#include "plugin.h"
 
 #include <winrt/Windows.Media.Capture.Frames.h>
 
-HL2SS_PLUGIN_EXPORT void ExtendedVideo_RegisterNamedMutex(wchar_t const* name);
+typedef void (*HOOK_EV_PROC)(winrt::Windows::Media::Capture::Frames::MediaFrameReference const&, void* param);
+
+void ExtendedVideo_RegisterNamedMutex(wchar_t const* name);
 
 void ExtendedVideo_QueryDevices(winrt::hstring& out);
-void ExtendedVideo_RegisterEvent(HANDLE h);
 void ExtendedVideo_Open(MRCVideoOptions const& options);
 void ExtendedVideo_Close();
 bool ExtendedVideo_Status();
 bool ExtendedVideo_SetFormat(uint16_t& width, uint16_t& height, uint8_t& framerate, VideoSubtype& subtype);
-winrt::Windows::Media::Capture::Frames::MediaFrameReader ExtendedVideo_CreateFrameReader();
+void ExtendedVideo_ExecuteSensorLoop(winrt::Windows::Media::Capture::Frames::MediaFrameReaderAcquisitionMode mode, HOOK_EV_PROC hook, void* param, HANDLE event_stop);
