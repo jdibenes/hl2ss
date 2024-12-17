01f3f33aedb5475fc80ea84cf008bd76a67b69f7
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-17 13:58:18 -0500

    update spatial_input

--- hl2ss/hl2ss/spatial_input.cpp
+++ hl2ss/hl2ss/spatial_input.cpp
@@ -1,20 +1,17 @@
 
-#include <Windows.h>
 #include "locator.h"
+#include "timestamp.h"
 #include "spatial_input.h"
 
 #include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.Numerics.h>
 #include <winrt/Windows.UI.Input.h>
 #include <winrt/Windows.UI.Input.Spatial.h>
-#include <winrt/Windows.Perception.h>
 #include <winrt/Windows.Perception.Spatial.h>
 #include <winrt/Windows.Perception.People.h>
 
-using namespace winrt::Windows::Foundation::Numerics;
+using namespace winrt::Windows::Foundation::Collections;
 using namespace winrt::Windows::UI::Input;
 using namespace winrt::Windows::UI::Input::Spatial;
-using namespace winrt::Windows::Perception;
 using namespace winrt::Windows::Perception::Spatial;
 using namespace winrt::Windows::Perception::People;
 
@@ -22,9 +19,6 @@ using namespace winrt::Windows::Perception::People;
 // Global Variables
 //-----------------------------------------------------------------------------
 
-static HANDLE g_thread_consent = NULL; // CloseHandle
-
-static GazeInputAccessStatus g_status_consent = GazeInputAccessStatus::Unspecified;
 static SpatialInteractionManager g_sim = nullptr;
 static std::vector<HandJointKind> g_joints;
 
@@ -33,26 +27,14 @@ static std::vector<HandJointKind> g_joints;
 //-----------------------------------------------------------------------------
 
 // OK
-static DWORD WINAPI SpatialInput_RequestEyeAccess(void* param)
+bool SpatialInput_WaitForConsent()
 {
-    (void)param;
-    g_status_consent = EyesPose::RequestAccessAsync().get();
-    return 0;
+    return EyesPose::RequestAccessAsync().get() == GazeInputAccessStatus::Allowed;
 }
 
 // OK
-bool SpatialInput_WaitForEyeConsent()
+void SpatialInput_Startup()
 {
-    WaitForSingleObject(g_thread_consent, INFINITE);
-    CloseHandle(g_thread_consent);
-    return g_status_consent == GazeInputAccessStatus::Allowed;
-}
-
-// OK
-void SpatialInput_Initialize()
-{
-    g_thread_consent = CreateThread(NULL, 0, SpatialInput_RequestEyeAccess, NULL, 0, NULL);
-
     g_joints.resize(HAND_JOINTS);
     for (int i = 0; i < HAND_JOINTS; ++i) { g_joints[i] = (HandJointKind)i; }
 
@@ -60,14 +42,20 @@ void SpatialInput_Initialize()
 }
 
 // OK
-int SpatialInput_GetHeadPoseAndEyeRay(SpatialCoordinateSystem const& world, PerceptionTimestamp const& ts, SpatialInput_Frame& head_pose, SpatialInput_Ray& eye_ray)
+void SpatialInput_Cleanup()
 {
-    SpatialPointerPose pointer = SpatialPointerPose::TryGetAtTimestamp(world, ts);
+    g_sim = nullptr;
+}
+
+// OK
+static int SpatialInput_GetHeadPoseAndEyeRay(SpatialCoordinateSystem const& world, UINT64 qpc, SpatialInput_Frame& head_pose, SpatialInput_Ray& eye_ray)
+{
+    auto pointer = SpatialPointerPose::TryGetAtTimestamp(world, Timestamp_QPCToPerception(qpc));
     int ret = 0;
 
     if (!pointer) { return ret; }
 
-    auto const& h = pointer.Head();
+    auto h = pointer.Head();
 
     head_pose.position = h.Position();
     head_pose.forward  = h.ForwardDirection();
@@ -75,13 +63,13 @@ int SpatialInput_GetHeadPoseAndEyeRay(SpatialCoordinateSystem const& world, Perc
 
     ret |= 1;
 
-    auto const& pose = pointer.Eyes();
+    auto pose = pointer.Eyes();
     if (!pose) { return ret; }
 
-    auto const& gaze = pose.Gaze();
+    auto gaze = pose.Gaze();
     if (!gaze) { return ret; }
 
-    auto const& spatialRay = gaze.Value();
+    auto spatialRay = gaze.Value();
 
     eye_ray.origin    = spatialRay.Origin;
     eye_ray.direction = spatialRay.Direction;
@@ -92,12 +80,12 @@ int SpatialInput_GetHeadPoseAndEyeRay(SpatialCoordinateSystem const& world, Perc
 }
 
 // OK
-int SpatialInput_GetHandPose(SpatialCoordinateSystem const& world, PerceptionTimestamp const& ts, std::vector<JointPose>& left_poses, std::vector<JointPose>& right_poses)
+static int SpatialInput_GetHandPose(SpatialCoordinateSystem const& world, UINT64 qpc, std::vector<JointPose>& left_poses, std::vector<JointPose>& right_poses)
 {
-    auto const& source_states = g_sim.GetDetectedSourcesAtTimestamp(ts);
+    auto source_states = g_sim.GetDetectedSourcesAtTimestamp(Timestamp_QPCToPerception(qpc));
     int ret = 0;
     std::vector<JointPose>* target;
-    int id;    
+    int id;
     bool ok;
 
     for (auto const& source_state : source_states)
@@ -111,14 +99,48 @@ int SpatialInput_GetHandPose(SpatialCoordinateSystem const& world, PerceptionTim
     default: continue;
     }
 
-    auto const& pose = source_state.TryGetHandPose();
+    auto pose = source_state.TryGetHandPose();
     if (!pose) { continue; }
 
     ok = pose.TryGetJoints(world, g_joints, *target);
     if (!ok) { continue; }
 
-    ret |= id;    
+    ret |= id;
     }
 
     return ret;
+}
+
+// OK
+void SpatialInput_ExecuteSensorLoop(HOOK_SI_PROC hook, void* param, HANDLE event_stop)
+{
+    HANDLE timer = CreateWaitableTimer(NULL, TRUE, NULL); // CloseHandle
+    LARGE_INTEGER delay;
+    SpatialInput_Frame head_pose;
+    SpatialInput_Ray eye_ray;
+    std::vector<JointPose> l_hand;
+    std::vector<JointPose> r_hand;
+
+    delay.QuadPart = -static_cast<int64_t>(HNS_BASE / 60);
+
+    l_hand.resize(HAND_JOINTS);
+    r_hand.resize(HAND_JOINTS);
+    
+    do
+    {
+    SetWaitableTimer(timer, &delay, 0, NULL, NULL, 0);
+
+    UINT64 qpc = Timestamp_GetCurrentQPC();
+    auto world = Locator_GetWorldCoordinateSystem();
+
+    int status1 = SpatialInput_GetHeadPoseAndEyeRay(world, qpc, head_pose, eye_ray);
+    int status2 = SpatialInput_GetHandPose(world, qpc, l_hand, r_hand);
+
+    hook((status1 << 0) | (status2 << 2), &head_pose, &eye_ray, l_hand.data(), r_hand.data(), qpc, param);
+
+    WaitForSingleObject(timer, INFINITE);
+    }
+    while (WaitForSingleObject(event_stop, 0) == WAIT_TIMEOUT);
+
+    CloseHandle(timer);
 }
--- hl2ss/hl2ss/spatial_input.h
+++ hl2ss/hl2ss/spatial_input.h
@@ -1,8 +1,9 @@
 
 #pragma once
 
+#include <Windows.h>
+
 #include <winrt/Windows.Foundation.Numerics.h>
-#include <winrt/Windows.Perception.Spatial.h>
 #include <winrt/Windows.Perception.People.h>
 
 int const HAND_JOINTS = 26;
@@ -20,7 +21,9 @@ struct SpatialInput_Ray
     winrt::Windows::Foundation::Numerics::float3 direction;
 };
 
-bool SpatialInput_WaitForEyeConsent();
-void SpatialInput_Initialize();
-int SpatialInput_GetHeadPoseAndEyeRay(winrt::Windows::Perception::Spatial::SpatialCoordinateSystem const& world, winrt::Windows::Perception::PerceptionTimestamp const& ts, SpatialInput_Frame& head_pose, SpatialInput_Ray& eye_ray);
-int SpatialInput_GetHandPose(winrt::Windows::Perception::Spatial::SpatialCoordinateSystem const& world, winrt::Windows::Perception::PerceptionTimestamp const& ts, std::vector<winrt::Windows::Perception::People::JointPose>& left_poses, std::vector<winrt::Windows::Perception::People::JointPose>& right_poses);
+typedef void (*HOOK_SI_PROC)(uint32_t, SpatialInput_Frame*, SpatialInput_Ray*, winrt::Windows::Perception::People::JointPose*, winrt::Windows::Perception::People::JointPose*, UINT64, void*);
+
+bool SpatialInput_WaitForConsent();
+void SpatialInput_Startup();
+void SpatialInput_Cleanup();
+void SpatialInput_ExecuteSensorLoop(HOOK_SI_PROC hook, void* param, HANDLE event_stop);
