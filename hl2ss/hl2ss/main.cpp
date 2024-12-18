cd7b321f58bf2ca9caf78e251dead5ed80aaa941
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-12-17 20:17:26 -0500

    Update main.cpp

--- hl2ss/hl2ss/main.cpp
+++ hl2ss/hl2ss/main.cpp
@@ -1,32 +1,13 @@
 
-#include <mfapi.h>
-#include "research_mode.h"
-#include "server.h"
-#include "ipc_rc.h"
-#include "stream_pv.h"
-#include "stream_rm.h"
-#include "stream_mc.h"
-#include "locator.h"
-#include "spatial_input.h"
-#include "stream_si.h"
-#include "holographic_space.h"
-#include "nfo.h"
-#include "personal_video.h"
-#include "spatial_mapping.h"
-#include "ipc_sm.h"
-#include "scene_understanding.h"
-#include "ipc_su.h"
-#include "voice_input.h"
-#include "ipc_vi.h"
-#include "stream_eet.h"
-#include "stream_ea.h"
-#include "extended_execution.h"
-#include "stream_ev.h"
+#include <Windows.h>
+#include "ipl.h"
 
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.ApplicationModel.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.UI.Core.h>
 
+using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::ApplicationModel;
 using namespace winrt::Windows::ApplicationModel::Core;
 using namespace winrt::Windows::UI::Core;
@@ -34,108 +15,91 @@ using namespace winrt::Windows::UI::Core;
 struct App : winrt::implements<App, IFrameworkViewSource, IFrameworkView>
 {
 	bool m_windowClosed = false;
-	bool m_init = false;
-	bool m_flat = false;
 
-	IFrameworkView CreateView()
-	{
-		return *this;
-	}
-
-	void OnSuspending(IInspectable const& sender, SuspendingEventArgs const& args)
-	{
-		(void)sender;
-		(void)args;
-
-		CoreApplication::Exit(); // Suspending is not supported
-	}
-
-	void Initialize(CoreApplicationView const &applicationView)
-	{
-		(void)applicationView;
-
-		CoreApplication::Suspending({ this, &App::OnSuspending });
-
-		InitializeSockets();
-		MFStartup(MF_VERSION);
-
-		m_flat = ExtendedExecution_GetFlatMode();
-	}
-
-	void Load(winrt::hstring const&)
-	{
-	}
-
-	void Uninitialize()
-	{
-	}
-
-	void SetWindow(CoreWindow const& window)
-	{
-		window.Closed({ this, &App::OnWindowClosed });
-
-		if (m_init) { return; }
-
-		if (!m_flat)
-		{
-		HolographicSpace_Initialize();
-		}
-
-		Locator_Initialize();
-		ResearchMode_Initialize();
-		SpatialInput_Initialize();
-		PersonalVideo_Initialize();
-		SpatialMapping_Initialize();
-		SceneUnderstanding_Initialize();
-		VoiceInput_Initialize();
-
-		RM_Initialize();
-		MC_Initialize();
-		PV_Initialize();
-		SI_Initialize();
-		RC_Initialize();
-		SM_Initialize();
-		SU_Initialize();
-		VI_Initialize();
-		EET_Initialize();
-		EA_Initialize();
-		EV_Initialize();
-
-		ExtendedExecution_Request();
-		
-		m_init = true;
-	}
-
-	void Run()
-	{
-		auto const& window = CoreWindow::GetForCurrentThread();
-		window.Activate();
-
-		while (!m_windowClosed)
-		{
-		window.Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
-
-		if (!m_flat)
-		{
-		HolographicSpace_Update();
-		HolographicSpace_Clear();
-		// Draw
-		HolographicSpace_Present();
-		}
-		}
-
-		CoreApplication::Exit();
-	}
-
-	void OnWindowClosed(CoreWindow const& sender, CoreWindowEventArgs const& args)
-	{
-		(void)sender;
-		(void)args;
-
-		m_windowClosed = true;
-	}
+	IFrameworkView CreateView();
+	void OnWindowClosed(CoreWindow const& sender, CoreWindowEventArgs const& args);
+	void OnSuspending(IInspectable const& sender, SuspendingEventArgs const& args);
+	void Initialize(CoreApplicationView const& applicationView);
+	void Load(winrt::hstring const& entryPoint);
+	void SetWindow(CoreWindow const& window);
+	void Run();
+	void Uninitialize();
 };
 
+//-----------------------------------------------------------------------------
+// Functions
+//-----------------------------------------------------------------------------
+
+// OK
+IFrameworkView App::CreateView()
+{
+	return *this;
+}
+
+// OK
+void App::OnWindowClosed(CoreWindow const& sender, CoreWindowEventArgs const& args)
+{
+	(void)sender;
+	(void)args;
+
+	m_windowClosed = true;
+}
+
+// OK
+void App::OnSuspending(IInspectable const& sender, SuspendingEventArgs const& args)
+{
+	(void)sender;
+	(void)args;
+
+	CoreApplication::Exit(); // Suspending is not supported
+}
+
+// OK
+void App::Initialize(CoreApplicationView const& applicationView)
+{
+	(void)applicationView;
+	CoreApplication::Suspending({ this, &App::OnSuspending });
+}
+
+// OK
+void App::Load(winrt::hstring const& entryPoint)
+{
+	(void)entryPoint;
+	HL2SS_Load(true);
+}
+
+// OK
+void App::SetWindow(CoreWindow const& window)
+{
+	window.Closed({ this, &App::OnWindowClosed });
+}
+
+// OK
+void App::Run()
+{
+	auto window = CoreWindow::GetForCurrentThread();
+	window.Activate();
+
+	while (!m_windowClosed)
+	{
+	window.Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+	HL2SS_Process_HS();
+	HL2SS_Process_MQ();
+	HL2SS_Process_MQX();
+	}
+}
+
+// OK
+void App::Uninitialize()
+{
+}
+
+//*****************************************************************************
+// Entry Point
+//*****************************************************************************
+
+// OK
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nShowCmd)
 {
 	(void)hInstance;
