da71b7bd9443c8e82b30d2540c0985d258ce1a6b
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-02-23 19:31:03 -0500

    normalize mrc api

--- viewer/hl2ss_dp.py
+++ viewer/hl2ss_dp.py
@@ -1,4 +1,5 @@
 
+import collections
 import requests
 import struct
 import hl2ss
@@ -112,13 +113,6 @@ def compute_timestamp(ct, et, tb):
     return ((ct + et) * hl2ss.TimeBase.HUNDREDS_OF_NANOSECONDS) // tb
 
 
-class _MRC_Frame:
-    def __init__(self, kind, sample, key_frame):
-        self.kind = kind
-        self.sample = sample
-        self.key_frame = key_frame
-
-
 class _gatherer:
     def open(self, host, port, user, password, chunk_size, configuration):
         self._client = _client()
@@ -172,7 +166,7 @@ class _gatherer:
                                                                     sps_data[0:2] = b'\x00\x00'
                                                                     pps_data[0:2] = b'\x00\x00'
                                                                     t = compute_timestamp(self._video_ct, self._video_et, self._video_tb)
-                                                                    packets.append(hl2ss._packet(t, _MRC_Frame(StreamKind.VIDEO, avcc_to_annex_b(sps_data + pps_data), True), None))
+                                                                    packets.append(hl2ss._packet(t, struct.pack('B', StreamKind.VIDEO | 0x04) + avcc_to_annex_b(sps_data + pps_data), None))
                                                                 elif (stbl_type == 'mp4a'):
                                                                     self._audio_id = id
                                                                     self._audio_ct = ct * tb
@@ -210,15 +204,15 @@ class _gatherer:
                             for j in range(0, count):
                                 span = spans[j]
                                 size = sizes[j]
-                                keyf = (flags[j] & 0x00010000) == 0
+                                keyf = (~flags[j] >> 14) & 0x04
                                 sample = data[offset:(offset+size)]
                                 if (id == self._video_id):
                                     t = compute_timestamp(self._video_ct, self._video_et, self._video_tb)
-                                    packets.append(hl2ss._packet(t, _MRC_Frame(StreamKind.VIDEO, avcc_to_annex_b(sample), keyf), None))
+                                    packets.append(hl2ss._packet(t, struct.pack('B', StreamKind.VIDEO | keyf) + avcc_to_annex_b(sample), None))
                                     self._video_et += span
                                 elif (id == self._audio_id):
                                     t = compute_timestamp(self._audio_ct, self._audio_et, self._audio_tb)
-                                    packets.append(hl2ss._packet(t, _MRC_Frame(StreamKind.AUDIO, raw_aac_to_adts(sample), keyf), None))
+                                    packets.append(hl2ss._packet(t, struct.pack('B', StreamKind.AUDIO | keyf) + raw_aac_to_adts(sample), None))
                                     self._audio_et += span
                                 offset += size
                         self._state = 1
@@ -273,13 +267,48 @@ class rx_mrc(hl2ss._context_manager):
         self.configuration = configuration
 
     def open(self):
+        self._buffer = collections.deque()
         self._client = _connect_client_mrc(self.host, self.port, self.user, self.password, self.chunk, self.configuration)
 
     def get_next_packet(self):
-        return self._client.get_next_packet()
+        if (len(self._buffer) <= 0):
+            self._buffer.extend(self._client.get_next_packet())
+        return self._buffer.popleft()
 
     def close(self):
         self._client.close()
+
+
+#------------------------------------------------------------------------------
+# Decoder
+#------------------------------------------------------------------------------
+
+class _MRC_Frame:
+    def __init__(self, kind, sample, key_frame):
+        self.kind = kind
+        self.sample = sample
+        self.key_frame = key_frame
+
+
+def unpack_mrc(payload):
+    flag = struct.unpack('B', payload[0:1])[0]
+    data = payload[1:]
+    kind = flag & 3
+    keyf = (flag & 0x04) != 0
+    return _MRC_Frame(kind, data, keyf)
+
+
+class decode_mrc:
+    def __init__(self):
+        self._video_codec = hl2ss.decode_pv(hl2ss.VideoProfile.H264_MAIN)
+        self._audio_codec = hl2ss.decode_microphone(hl2ss.AudioProfile.AAC_12000, hl2ss.AACLevel.L2)
+
+    def create(self):
+        self._video_codec.create(0, 0)
+        self._audio_codec.create()
+
+    def decode(self, payload, kind, format):
+        return self._video_codec.decode(payload, format) if (kind == StreamKind.VIDEO) else self._audio_codec.decode(payload) if (kind == StreamKind.AUDIO) else None
 
 
 #------------------------------------------------------------------------------
@@ -290,22 +319,24 @@ class rx_decoded_mrc(rx_mrc):
     def __init__(self, host, port, user, password, chunk, configuration, format):
         super().__init__(host, port, user, password, chunk, configuration)
         self.format = format
-        self._video_codec = hl2ss.decode_pv(hl2ss.VideoProfile.H264_MAIN)
-        self._audio_codec = hl2ss.decode_microphone(hl2ss.AudioProfile.AAC_12000, hl2ss.AACLevel.L2)
+        self._codec = decode_mrc()
 
     def open(self):
-        self._video_codec.create(0, 0)
-        self._audio_codec.create()
+        self._d_t = 0
+        self._d_k = False
+        self._codec.create()
         super().open()
 
     def get_next_packet(self):
-        packets = super().get_next_packet()
-        decoded = []
-        for packet in packets:
-            packet.payload.sample = self._video_codec.decode(packet.payload.sample, self.format) if (packet.payload.kind == StreamKind.VIDEO) else self._audio_codec.decode(packet.payload.sample) if (packet.payload.kind == StreamKind.AUDIO) else None
-            if (packet.payload.sample is not None):
-                decoded.append(packet)
-        return decoded
+        while (True):
+            data = super().get_next_packet()
+            data.payload = unpack_mrc(data.payload)
+            data.payload.sample = self._codec.decode(data.payload.sample, data.payload.kind, self.format)
+            if (data.payload.kind == StreamKind.VIDEO):
+                data.timestamp,         self._d_t = (self._d_t, data.timestamp)
+                data.payload.key_frame, self._d_k = (self._d_k, data.payload.key_frame)
+            if (data.payload.sample is not None):
+                return data
 
     def close(self):
         super().close()
