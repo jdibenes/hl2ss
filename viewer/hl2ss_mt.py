e29e632d2d876cb89a56a1d316721fc47fa58384
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-03-18 18:12:22 -0400

    Update hl2ss_mt.py

--- viewer/hl2ss_mt.py
+++ viewer/hl2ss_mt.py
@@ -95,7 +95,7 @@ class _rx(hl2ss._context_manager):
         self.close()
 
 
-def unpack_rm_vlc(payload):
+def _unpack_rm_vlc(payload):
     image        = np.frombuffer(payload, dtype=np.uint8,  offset=0,                                   count=hl2ss.Parameters_RM_VLC.PIXELS).reshape(hl2ss.Parameters_RM_VLC.SHAPE)
     sensor_ticks = np.frombuffer(payload, dtype=np.uint64, offset=hl2ss.Parameters_RM_VLC.PIXELS +  0, count=1)
     exposure     = np.frombuffer(payload, dtype=np.uint64, offset=hl2ss.Parameters_RM_VLC.PIXELS +  8, count=1)
@@ -104,7 +104,7 @@ def unpack_rm_vlc(payload):
     return hl2ss._RM_VLC_Frame(image, sensor_ticks, exposure, gain)
 
 
-def unpack_rm_depth_ahat_block(payload):
+def _unpack_rm_depth_ahat(payload):
     depth        = np.frombuffer(payload, dtype=np.uint16, offset=0,                                                              count=hl2ss.Parameters_RM_DEPTH_AHAT.PIXELS).reshape(hl2ss.Parameters_RM_DEPTH_AHAT.SHAPE)
     ab           = np.frombuffer(payload, dtype=np.uint16, offset=hl2ss.Parameters_RM_DEPTH_AHAT.PIXELS * hl2ss._SIZEOF.WORD,     count=hl2ss.Parameters_RM_DEPTH_AHAT.PIXELS).reshape(hl2ss.Parameters_RM_DEPTH_AHAT.SHAPE)
     sensor_ticks = np.frombuffer(payload, dtype=np.uint64, offset=hl2ss.Parameters_RM_DEPTH_AHAT.PIXELS * hl2ss._SIZEOF.WORD * 2, count=1)
@@ -112,7 +112,7 @@ def unpack_rm_depth_ahat_block(payload):
     return hl2ss._RM_Depth_Frame(depth, ab, sensor_ticks)
 
 
-def unpack_rm_depth_longthrow_block(payload):
+def _unpack_rm_depth_longthrow(payload):
     depth        = np.frombuffer(payload, dtype=np.uint16, offset=0,                                                                   count=hl2ss.Parameters_RM_DEPTH_LONGTHROW.PIXELS).reshape(hl2ss.Parameters_RM_DEPTH_LONGTHROW.SHAPE)
     ab           = np.frombuffer(payload, dtype=np.uint16, offset=hl2ss.Parameters_RM_DEPTH_LONGTHROW.PIXELS * hl2ss._SIZEOF.WORD,     count=hl2ss.Parameters_RM_DEPTH_LONGTHROW.PIXELS).reshape(hl2ss.Parameters_RM_DEPTH_LONGTHROW.SHAPE)
     sensor_ticks = np.frombuffer(payload, dtype=np.uint64, offset=hl2ss.Parameters_RM_DEPTH_LONGTHROW.PIXELS * hl2ss._SIZEOF.WORD * 2, count=1)
@@ -120,7 +120,7 @@ def unpack_rm_depth_longthrow_block(payload):
     return hl2ss._RM_Depth_Frame(depth, ab, sensor_ticks)
 
 
-def unpack_pv(payload, width, height, bpp):
+def _unpack_pv(payload, width, height, bpp):
     shape = (height, width, bpp)
     b = width * height * bpp
 
@@ -140,15 +140,15 @@ def unpack_pv(payload, width, height, bpp):
     return hl2ss._PV_Frame(image, focal_length, principal_point, exposure_time, exposure_compensation, lens_position, focus_state, iso_speed, white_balance, iso_gains, white_balance_gains, resolution)
 
 
-def unpack_microphone(payload, profile, level):
+def _unpack_microphone(payload, profile, level):
     return np.frombuffer(payload, dtype=np.float32).reshape((2, -1)) if (profile != hl2ss.AudioProfile.RAW) else np.frombuffer(payload, dtype=np.int16).reshape((1, -1)) if (level != hl2ss.AACLevel.L5) else np.frombuffer(payload, dtype=np.float32).reshape((1, -1))
 
 
-def unpack_extended_audio(payload, profile):
+def _unpack_extended_audio(payload, profile):
     return np.frombuffer(payload, dtype=np.float32).reshape((2, -1)) if (profile != hl2ss.AudioProfile.RAW) else np.frombuffer(payload, dtype=np.int16).reshape((1, -1))
 
 
-def unpack_extended_depth(payload, width, height):
+def _unpack_extended_depth(payload, width, height):
     shape = (height, width)
     count = width * height
     b = count * hl2ss._SIZEOF.WORD
@@ -161,17 +161,17 @@ def unpack_extended_depth(payload, width, height):
 
 class rx_decoded_rm_vlc(_rx):
     def _unpack_payload(self, payload):
-        return unpack_rm_vlc(payload)
+        return _unpack_rm_vlc(payload)
 
 
 class rx_decoded_rm_depth_ahat(_rx):
     def _unpack_payload(self, payload):
-        return unpack_rm_depth_ahat_block(payload)
+        return _unpack_rm_depth_ahat(payload)
 
 
 class rx_decoded_rm_depth_longthrow(_rx):
     def _unpack_payload(self, payload):
-        return unpack_rm_depth_longthrow_block(payload)
+        return _unpack_rm_depth_longthrow(payload)
 
 
 class rx_rm_imu(_rx):
@@ -192,12 +192,12 @@ class rx_decoded_pv(_rx):
             self._width, self._height = self._get_pv_dimensions()
             self._bpp = rx_decoded_pv._format_bpp[self._configuration['decoded_format'] % 5]
             self._pv_wxh = True
-        return unpack_pv(payload, self._width, self._height, self._bpp)
+        return _unpack_pv(payload, self._width, self._height, self._bpp)
 
 
 class rx_decoded_microphone(_rx):
     def _unpack_payload(self, payload):
-        return unpack_microphone(payload, self._configuration['profile'], self._configuration['level'])
+        return _unpack_microphone(payload, self._configuration['profile'], self._configuration['level'])
 
 
 class rx_si(_rx):
@@ -210,7 +210,7 @@ class rx_eet(_rx):
 
 class rx_decoded_extended_audio(_rx):
     def _unpack_payload(self, payload):
-        return unpack_extended_audio(payload, self._configuration['profile'])
+        return _unpack_extended_audio(payload, self._configuration['profile'])
 
 
 class rx_decoded_extended_depth(_rx):
@@ -218,5 +218,5 @@ class rx_decoded_extended_depth(_rx):
         if (not self._pv_wxh):
             self._width, self._height = self._get_pv_dimensions()
             self._pv_wxh = True
-        return unpack_extended_depth(payload, self._width, self._height)
+        return _unpack_extended_depth(payload, self._width, self._height)
     
