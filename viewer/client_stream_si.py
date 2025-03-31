3c858d1c01cf5cc3fd02f2559cf99c8dc4c90de6
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-03-30 23:02:06 -0400

    Update client_stream_si.py

--- viewer/client_stream_si.py
+++ viewer/client_stream_si.py
@@ -14,6 +14,9 @@ import hl2ss_lnm
 # HoloLens address
 host = "192.168.1.7"
 
+# Hand Joint to print data for
+hand_joint = hl2ss.SI_HandJointKind.Wrist
+
 #------------------------------------------------------------------------------
 
 enable = True
@@ -26,25 +29,27 @@ def on_press(key):
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
+hand_join_name = hl2ss.si_get_joint_name(hand_joint)
+
 client = hl2ss_lnm.rx_si(host, hl2ss.StreamPort.SPATIAL_INPUT)
 client.open()
 
 while (enable):
     data = client.get_next_packet()
-    si = hl2ss.unpack_si(data.payload)
+    si = data.payload
 
     print(f'Tracking status at time {data.timestamp}')
 
-    if (si.is_valid_head_pose()):
-        head_pose = si.get_head_pose()
+    if (si.head_pose_valid):
+        head_pose = si.head_pose
         print(f'Head pose: Position={head_pose.position} Forward={head_pose.forward} Up={head_pose.up}')
         # right = cross(up, -forward)
         # up => y, forward => -z, right => x
     else:
         print('No head pose data')
 
-    if (si.is_valid_eye_ray()):
-        eye_ray = si.get_eye_ray()
+    if (si.eye_ray_valid):
+        eye_ray = si.eye_ray
         print(f'Eye ray: Origin={eye_ray.origin} Direction={eye_ray.direction}')
     else:
         print('No eye tracking data')
@@ -53,17 +58,23 @@ while (enable):
     # https://learn.microsoft.com/en-us/uwp/api/windows.perception.people.jointpose?view=winrt-22621
     # for hand data details
 
-    if (si.is_valid_hand_left()):
-        hand_left = si.get_hand_left()
-        pose = hand_left.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
-        print(f'Left wrist pose: Position={pose.position} Orientation={pose.orientation} Radius={pose.radius} Accuracy={pose.accuracy}')
+    if (si.hand_left_valid):
+        hand = si.hand_left
+        joint_orientation = hand.orientation[hand_joint, :]
+        joint_position    = hand.position[hand_joint, :]
+        joint_radius      = hand.radius[hand_joint]
+        joint_accuracy    = hand.accuracy[hand_joint]
+        print(f'Left {hand_join_name} pose: Position={joint_position} Orientation={joint_orientation} Radius={joint_radius} Accuracy={joint_accuracy}')
     else:
         print('No left hand data')
 
-    if (si.is_valid_hand_right()):
-        hand_right = si.get_hand_right()
-        pose = hand_right.get_joint_pose(hl2ss.SI_HandJointKind.Wrist)
-        print(f'Right wrist pose: Position={pose.position} Orientation={pose.orientation} Radius={pose.radius} Accuracy={pose.accuracy}')
+    if (si.hand_right_valid):
+        hand = si.hand_right
+        joint_orientation = hand.orientation[hand_joint, :]
+        joint_position    = hand.position[hand_joint, :]
+        joint_radius      = hand.radius[hand_joint]
+        joint_accuracy    = hand.accuracy[hand_joint]
+        print(f'Right {hand_join_name} pose: Position={joint_position} Orientation={joint_orientation} Radius={joint_radius} Accuracy={joint_accuracy}')
     else:
         print('No right hand data')
 
