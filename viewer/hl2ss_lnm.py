cb62863fb8707b738a61c570981db7cf7dffd9bd
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-04-02 21:50:37 -0400

    Update hl2ss_lnm.py

--- viewer/hl2ss_lnm.py
+++ viewer/hl2ss_lnm.py
@@ -35,6 +35,14 @@ def create_configuration_for_dp_mrc(pv=True, holo=False, mic=True, loopback=Fals
     return hl2ss_dp.create_configuration_for_mrc(pv, holo, mic, loopback, RenderFromCamera, vstab, vstabbuffer)
 
 
+def create_sockopt(settimeout=None, tcp_nodelay=1):
+    sockopt = {
+        'settimeout' : settimeout,
+        'setsockopt.IPPROTO_TCP.TCP_NODELAY' : tcp_nodelay,
+    }
+    return sockopt
+
+
 #------------------------------------------------------------------------------
 # Stream Sync Period
 #------------------------------------------------------------------------------
@@ -82,19 +90,28 @@ def get_sync_period(rx):
 # Control
 #------------------------------------------------------------------------------
 
-def start_subsystem_pv(host, port, enable_mrc=False, hologram_composition=True, recording_indicator=False, video_stabilization=False, blank_protected=False, show_mesh=False, shared=False, global_opacity=0.9, output_width=0.0, output_height=0.0, video_stabilization_length=0, hologram_perspective=hl2ss.HologramPerspective.PV):
-    hl2ss.start_subsystem_pv(host, port, enable_mrc, hologram_composition, recording_indicator, video_stabilization, blank_protected, show_mesh, shared, global_opacity, output_width, output_height, video_stabilization_length, hologram_perspective)
+def start_subsystem_pv(host, port, sockopt=None, enable_mrc=False, hologram_composition=True, recording_indicator=False, video_stabilization=False, blank_protected=False, show_mesh=False, shared=False, global_opacity=0.9, output_width=0.0, output_height=0.0, video_stabilization_length=0, hologram_perspective=hl2ss.HologramPerspective.PV):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    hl2ss.start_subsystem_pv(host, port, sockopt, enable_mrc, hologram_composition, recording_indicator, video_stabilization, blank_protected, show_mesh, shared, global_opacity, output_width, output_height, video_stabilization_length, hologram_perspective)
 
 
-def stop_subsystem_pv(host, port):
-    hl2ss.stop_subsystem_pv(host, port)
+def stop_subsystem_pv(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    hl2ss.stop_subsystem_pv(host, port, sockopt)
 
 
 #------------------------------------------------------------------------------
 # Modes 0, 1
 #------------------------------------------------------------------------------
 
-def rx_rm_vlc(host, port, chunk=hl2ss.ChunkSize.RM_VLC, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+def rx_rm_vlc(host, port, sockopt=None, chunk=hl2ss.ChunkSize.RM_VLC, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
     if (bitrate is None):
         bitrate = get_video_codec_default_bitrate(hl2ss.Parameters_RM_VLC.WIDTH, hl2ss.Parameters_RM_VLC.HEIGHT, hl2ss.Parameters_RM_VLC.FPS, divisor, profile)
 
@@ -105,10 +122,13 @@ def rx_rm_vlc(host, port, chunk=hl2ss.ChunkSize.RM_VLC, mode=hl2ss.StreamMode.MO
     else:
         options[hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize] = options.get(hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize, get_video_codec_default_gop_size(hl2ss.Parameters_RM_VLC.FPS, divisor, profile))
     
-    return hl2ss.rx_decoded_rm_vlc(host, port, chunk, mode, divisor, profile, level, bitrate, options) if (decoded) else hl2ss.rx_rm_vlc(host, port, chunk, mode, divisor, profile, level, bitrate, options)
+    return hl2ss.rx_decoded_rm_vlc(host, port, sockopt, chunk, mode, divisor, profile, level, bitrate, options) if (decoded) else hl2ss.rx_rm_vlc(host, port, sockopt, chunk, mode, divisor, profile, level, bitrate, options)
 
 
-def rx_rm_depth_ahat(host, port, chunk=hl2ss.ChunkSize.RM_DEPTH_AHAT, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile_z=hl2ss.DepthProfile.SAME, profile_ab=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+def rx_rm_depth_ahat(host, port, sockopt=None, chunk=hl2ss.ChunkSize.RM_DEPTH_AHAT, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile_z=hl2ss.DepthProfile.SAME, profile_ab=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
     if (bitrate is None):
         bitrate = get_video_codec_default_bitrate(hl2ss.Parameters_RM_DEPTH_AHAT.WIDTH, hl2ss.Parameters_RM_DEPTH_AHAT.HEIGHT, hl2ss.Parameters_RM_DEPTH_AHAT.FPS, divisor, profile_ab) * (16 if (profile_z == hl2ss.DepthProfile.SAME) else 1)
 
@@ -117,18 +137,27 @@ def rx_rm_depth_ahat(host, port, chunk=hl2ss.ChunkSize.RM_DEPTH_AHAT, mode=hl2ss
     else:
         options[hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize] = options.get(hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize, get_video_codec_default_gop_size(hl2ss.Parameters_RM_DEPTH_AHAT.FPS, divisor, profile_ab))
     
-    return hl2ss.rx_decoded_rm_depth_ahat(host, port, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options) if (decoded) else hl2ss.rx_rm_depth_ahat(host, port, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options)
+    return hl2ss.rx_decoded_rm_depth_ahat(host, port, sockopt, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options) if (decoded) else hl2ss.rx_rm_depth_ahat(host, port, sockopt, chunk, mode, divisor, profile_z, profile_ab, level, bitrate, options)
 
 
-def rx_rm_depth_longthrow(host, port, chunk=hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, mode=hl2ss.StreamMode.MODE_1, divisor=1, png_filter=hl2ss.PNGFilterMode.PAETH, decoded=True):
-    return hl2ss.rx_decoded_rm_depth_longthrow(host, port, chunk, mode, divisor, png_filter) if (decoded) else hl2ss.rx_rm_depth_longthrow(host, port, chunk, mode, divisor, png_filter)
+def rx_rm_depth_longthrow(host, port, sockopt=None, chunk=hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, mode=hl2ss.StreamMode.MODE_1, divisor=1, png_filter=hl2ss.PNGFilterMode.PAETH, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.rx_decoded_rm_depth_longthrow(host, port, sockopt, chunk, mode, divisor, png_filter) if (decoded) else hl2ss.rx_rm_depth_longthrow(host, port, sockopt, chunk, mode, divisor, png_filter)
 
 
-def rx_rm_imu(host, port, chunk=hl2ss.ChunkSize.RM_IMU, mode=hl2ss.StreamMode.MODE_1, decoded=True):
-    return hl2ss.rx_decoded_rm_imu(host, port, chunk, mode) if (decoded) else hl2ss.rx_rm_imu(host, port, chunk, mode)
+def rx_rm_imu(host, port, sockopt=None, chunk=hl2ss.ChunkSize.RM_IMU, mode=hl2ss.StreamMode.MODE_1, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.rx_decoded_rm_imu(host, port, sockopt, chunk, mode) if (decoded) else hl2ss.rx_rm_imu(host, port, sockopt, chunk, mode)
 
 
-def rx_pv(host, port, chunk=hl2ss.ChunkSize.PERSONAL_VIDEO, mode=hl2ss.StreamMode.MODE_1, width=1920, height=1080, framerate=30, divisor=1, profile=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded_format='bgr24'):
+def rx_pv(host, port, sockopt=None, chunk=hl2ss.ChunkSize.PERSONAL_VIDEO, mode=hl2ss.StreamMode.MODE_1, width=1920, height=1080, framerate=30, divisor=1, profile=hl2ss.VideoProfile.H265_MAIN, level=hl2ss.H26xLevel.DEFAULT, bitrate=None, options=None, decoded_format='bgr24'):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+    
     if (bitrate is None):
         bitrate = get_video_codec_default_bitrate(width, height, framerate, divisor, profile)
 
@@ -137,31 +166,46 @@ def rx_pv(host, port, chunk=hl2ss.ChunkSize.PERSONAL_VIDEO, mode=hl2ss.StreamMod
     else:
         options[hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize] = options.get(hl2ss.H26xEncoderProperty.CODECAPI_AVEncMPVGOPSize, get_video_codec_default_gop_size(framerate, divisor, profile))
     
-    return hl2ss.rx_decoded_pv(host, port, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options, decoded_format) if (decoded_format) else hl2ss.rx_pv(host, port, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options)
+    return hl2ss.rx_decoded_pv(host, port, sockopt, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options, decoded_format) if (decoded_format) else hl2ss.rx_pv(host, port, sockopt, chunk, mode, width, height, framerate, divisor, profile, level, bitrate, options)
 
 
-def rx_microphone(host, port, chunk=hl2ss.ChunkSize.MICROPHONE, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2, decoded=True):
-    return hl2ss.rx_decoded_microphone(host, port, chunk, profile, level) if (decoded) else hl2ss.rx_microphone(host, port, chunk, profile, level)
+def rx_microphone(host, port, sockopt=None, chunk=hl2ss.ChunkSize.MICROPHONE, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.rx_decoded_microphone(host, port, sockopt, chunk, profile, level) if (decoded) else hl2ss.rx_microphone(host, port, sockopt, chunk, profile, level)
 
 
-def rx_si(host, port, chunk=hl2ss.ChunkSize.SPATIAL_INPUT, decoded=True):
-    return hl2ss.rx_decoded_si(host, port, chunk) if (decoded) else hl2ss.rx_si(host, port, chunk)
+def rx_si(host, port, sockopt=None, chunk=hl2ss.ChunkSize.SPATIAL_INPUT, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.rx_decoded_si(host, port, sockopt, chunk) if (decoded) else hl2ss.rx_si(host, port, sockopt, chunk)
 
 
-def rx_eet(host, port, chunk=hl2ss.ChunkSize.EXTENDED_EYE_TRACKER, fps=30, decoded=True):
-    return hl2ss.rx_decoded_eet(host, port, chunk, fps) if (decoded) else hl2ss.rx_eet(host, port, chunk, fps)
+def rx_eet(host, port, sockopt=None, chunk=hl2ss.ChunkSize.EXTENDED_EYE_TRACKER, fps=30, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.rx_decoded_eet(host, port, sockopt, chunk, fps) if (decoded) else hl2ss.rx_eet(host, port, sockopt, chunk, fps)
 
 
-def rx_extended_audio(host, port, chunk=hl2ss.ChunkSize.EXTENDED_AUDIO, mixer_mode=hl2ss.MixerMode.BOTH, loopback_gain=1.0, microphone_gain=1.0, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2, decoded=True):
-    return hl2ss.rx_decoded_extended_audio(host, port, chunk, mixer_mode, loopback_gain, microphone_gain, profile, level) if (decoded) else hl2ss.rx_extended_audio(host, port, chunk, mixer_mode, loopback_gain, microphone_gain, profile, level)
+def rx_extended_audio(host, port, sockopt=None, chunk=hl2ss.ChunkSize.EXTENDED_AUDIO, mixer_mode=hl2ss.MixerMode.BOTH, loopback_gain=1.0, microphone_gain=1.0, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+    
+    return hl2ss.rx_decoded_extended_audio(host, port, sockopt, chunk, mixer_mode, loopback_gain, microphone_gain, profile, level) if (decoded) else hl2ss.rx_extended_audio(host, port, sockopt, chunk, mixer_mode, loopback_gain, microphone_gain, profile, level)
 
 
-def rx_extended_depth(host, port, chunk=hl2ss.ChunkSize.EXTENDED_DEPTH, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile_z=hl2ss.DepthProfile.ZDEPTH, media_index=0xFFFFFFFF, stride_mask=0x3F, decoded=True):
+def rx_extended_depth(host, port, sockopt=None, chunk=hl2ss.ChunkSize.EXTENDED_DEPTH, mode=hl2ss.StreamMode.MODE_1, divisor=1, profile_z=hl2ss.DepthProfile.ZDEPTH, media_index=0xFFFFFFFF, stride_mask=0x3F, decoded=True):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+    
     options = dict()
     options[hl2ss.H26xEncoderProperty.HL2SSAPI_VideoMediaIndex] = media_index
     options[hl2ss.H26xEncoderProperty.HL2SSAPI_VideoStrideMask] = stride_mask
 
-    return hl2ss.rx_decoded_extended_depth(host, port, chunk, mode, divisor, profile_z, options) if (decoded) else hl2ss.rx_extended_depth(host, port, chunk, mode, divisor, profile_z, options)
+    return hl2ss.rx_decoded_extended_depth(host, port, sockopt, chunk, mode, divisor, profile_z, options) if (decoded) else hl2ss.rx_extended_depth(host, port, sockopt, chunk, mode, divisor, profile_z, options)
 
 
 def rx_dp_mrc(host, port, user, password, chunk=hl2ss_dp.ChunkSize.MRC, configuration=None, decoded_format='bgr24'):
@@ -175,58 +219,97 @@ def rx_dp_mrc(host, port, user, password, chunk=hl2ss_dp.ChunkSize.MRC, configur
 # Mode 2
 #------------------------------------------------------------------------------
 
-def download_calibration_rm_vlc(host, port):
-    return hl2ss.download_calibration_rm_vlc(host, port)
+def download_calibration_rm_vlc(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_calibration_rm_vlc(host, port, sockopt)
 
 
-def download_calibration_rm_depth_ahat(host, port):
-    return hl2ss.download_calibration_rm_depth_ahat(host, port)
+def download_calibration_rm_depth_ahat(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_calibration_rm_depth_ahat(host, port, sockopt)
 
 
-def download_calibration_rm_depth_longthrow(host, port):
-    return hl2ss.download_calibration_rm_depth_longthrow(host, port)
+def download_calibration_rm_depth_longthrow(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_calibration_rm_depth_longthrow(host, port, sockopt)
 
 
-def download_calibration_rm_imu(host, port):
-    return hl2ss.download_calibration_rm_imu(host, port)
+def download_calibration_rm_imu(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_calibration_rm_imu(host, port, sockopt)
 
 
-def download_calibration_pv(host, port, width, height, framerate):
-    return hl2ss.download_calibration_pv(host, port, width, height, framerate)
+def download_calibration_pv(host, port, sockopt=None, width=1920, height=1080, framerate=30):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_calibration_pv(host, port, sockopt, width, height, framerate)
 
 
-def download_devicelist_extended_audio(host, port, profile, level):
-    return hl2ss.download_devicelist_extended_audio(host, port, profile, level)
+def download_devicelist_extended_audio(host, port, sockopt=None, profile=hl2ss.AudioProfile.AAC_24000, level=hl2ss.AACLevel.L2):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_devicelist_extended_audio(host, port, sockopt, profile, level)
 
 
-def download_devicelist_extended_video(host, port):
-    return hl2ss.download_devicelist_extended_video(host, port)
+def download_devicelist_extended_video(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.download_devicelist_extended_video(host, port, sockopt)
 
 
 #------------------------------------------------------------------------------
 # IPC
 #------------------------------------------------------------------------------
 
-def ipc_rc(host, port):
-    return hl2ss.ipc_rc(host, port)
+def ipc_rc(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_rc(host, port, sockopt)
 
 
-def ipc_sm(host, port):
-    return hl2ss.ipc_sm(host, port)
+def ipc_sm(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_sm(host, port, sockopt)
 
 
-def ipc_su(host, port):
-    return hl2ss.ipc_su(host, port)
+def ipc_su(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_su(host, port, sockopt)
 
 
-def ipc_vi(host, port):
-    return hl2ss.ipc_vi(host, port)
+def ipc_vi(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_vi(host, port, sockopt)
 
 
-def ipc_umq(host, port):
-    return hl2ss.ipc_umq(host, port)
+def ipc_umq(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_umq(host, port, sockopt)
 
 
-def ipc_gmq(host, port):
-    return hl2ss.ipc_gmq(host, port)
+def ipc_gmq(host, port, sockopt=None):
+    if (sockopt is None):
+        sockopt = create_sockopt()
+
+    return hl2ss.ipc_gmq(host, port, sockopt)
 
