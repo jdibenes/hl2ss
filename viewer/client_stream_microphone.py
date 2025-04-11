7bcc17ddc4228e49ddd6cf6662f77236567a9f0d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-04-10 22:59:14 -0400

    Update client_stream_microphone.py

--- viewer/client_stream_microphone.py
+++ viewer/client_stream_microphone.py
@@ -23,24 +23,25 @@ profile = hl2ss.AudioProfile.AAC_24000
 
 #------------------------------------------------------------------------------
 
-audio_subtype     = np.int16 if (profile == hl2ss.AudioProfile.RAW) else np.float32
-audio_planar      = profile != hl2ss.AudioProfile.RAW
-audio_channels    = hl2ss.Parameters_MICROPHONE.CHANNELS
-audio_sample_rate = hl2ss.Parameters_MICROPHONE.SAMPLE_RATE
+if __name__ == '__main__':
+    audio_subtype     = np.int16 if (profile == hl2ss.AudioProfile.RAW) else np.float32
+    audio_planar      = profile != hl2ss.AudioProfile.RAW
+    audio_channels    = hl2ss.Parameters_MICROPHONE.CHANNELS
+    audio_sample_rate = hl2ss.Parameters_MICROPHONE.SAMPLE_RATE
 
-listener = hl2ss_utilities.key_listener(keyboard.Key.esc)
-listener.open()
+    listener = hl2ss_utilities.key_listener(keyboard.Key.esc)
+    listener.open()
 
-player = hl2ss_utilities.audio_player(audio_subtype, audio_planar, audio_channels, audio_sample_rate)
-player.open()
+    player = hl2ss_utilities.audio_player(audio_subtype, audio_planar, audio_channels, audio_sample_rate)
+    player.open()
 
-client = hl2ss_lnm.rx_microphone(host, hl2ss.StreamPort.MICROPHONE, profile=profile)
-client.open()
+    client = hl2ss_lnm.rx_microphone(host, hl2ss.StreamPort.MICROPHONE, profile=profile)
+    client.open()
 
-while (not listener.pressed()):
-    data = client.get_next_packet()
-    player.put(data.timestamp, data.payload)
+    while (not listener.pressed()):
+        data = client.get_next_packet()
+        player.put(data.timestamp, data.payload)
 
-client.close()
-player.close()
-listener.close()
+    client.close()
+    player.close()
+    listener.close()
