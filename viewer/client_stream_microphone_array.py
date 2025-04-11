5db727965ac9ec36a7602c7fc025f08cbfed4c01
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-04-10 22:59:17 -0400

    Update client_stream_microphone_array.py

--- viewer/client_stream_microphone_array.py
+++ viewer/client_stream_microphone_array.py
@@ -29,26 +29,27 @@ channel = hl2ss.Parameters_MICROPHONE.ARRAY_TOP_LEFT
 
 #------------------------------------------------------------------------------
 
-audio_subtype     = np.float32
-audio_planar      = False
-audio_channels    = 1 # audio_player only supports 1 or 2 channels
-audio_sample_rate = hl2ss.Parameters_MICROPHONE.SAMPLE_RATE
+if __name__ == '__main__':
+    audio_subtype     = np.float32
+    audio_planar      = False
+    audio_channels    = 1 # audio_player only supports 1 or 2 channels
+    audio_sample_rate = hl2ss.Parameters_MICROPHONE.SAMPLE_RATE
 
-listener = hl2ss_utilities.key_listener(keyboard.Key.esc)
-listener.open()
+    listener = hl2ss_utilities.key_listener(keyboard.Key.esc)
+    listener.open()
 
-player = hl2ss_utilities.audio_player(audio_subtype, audio_planar, audio_channels, audio_sample_rate)
-player.open()
+    player = hl2ss_utilities.audio_player(audio_subtype, audio_planar, audio_channels, audio_sample_rate)
+    player.open()
 
-client = hl2ss_lnm.rx_microphone(host, hl2ss.StreamPort.MICROPHONE, profile=hl2ss.AudioProfile.RAW, level=hl2ss.AACLevel.L5)
-client.open()
+    client = hl2ss_lnm.rx_microphone(host, hl2ss.StreamPort.MICROPHONE, profile=hl2ss.AudioProfile.RAW, level=hl2ss.AACLevel.L5)
+    client.open()
 
-while (not listener.pressed()): 
-    data = client.get_next_packet()
-    # Extract channel to play
-    data.payload = data.payload[:, channel::hl2ss.Parameters_MICROPHONE.ARRAY_CHANNELS]
-    player.put(data.timestamp, data.payload)
+    while (not listener.pressed()): 
+        data = client.get_next_packet()
+        # Extract channel to play
+        data.payload = data.payload[:, channel::hl2ss.Parameters_MICROPHONE.ARRAY_CHANNELS]
+        player.put(data.timestamp, data.payload)
 
-client.close()
-player.close()
-listener.close()
+    client.close()
+    player.close()
+    listener.close()
