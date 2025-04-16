1dffec076e7a09fa737dddabebbea04b86cfe22a
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-04-15 22:59:00 -0400

    encoder_si

---
+++ hl2ss/hl2ss/encoder_si.cpp
@@ -0,0 +1,39 @@
+
+#include "encoder_si.h"
+
+using namespace winrt::Windows::Perception::People;
+
+//-----------------------------------------------------------------------------
+// Functions
+//-----------------------------------------------------------------------------
+
+// OK
+Encoder_SI::Encoder_SI(HOOK_ENCODER_PROC pHookCallback, void* pHookParam) :
+CustomEncoder(pHookCallback, pHookParam, NULL, 0)
+{
+}
+
+// OK
+void Encoder_SI::WriteSample(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, JointPose* left_hand, JointPose* right_hand, UINT64 timestamp)
+{
+    int32_t const packet_size = sizeof(uint32_t) + sizeof(SpatialInput_Frame) + sizeof(SpatialInput_Ray) + (2 * HAND_SIZE);
+
+    IMFMediaBuffer* pBuffer; // Release
+    BYTE* pDst;
+
+    CreateBuffer(&pBuffer, packet_size);
+
+    pBuffer->Lock(&pDst, NULL, NULL);
+
+    memcpy(pDst, &valid,     sizeof(valid));              pDst += sizeof(valid);
+    memcpy(pDst, head_pose,  sizeof(SpatialInput_Frame)); pDst += sizeof(SpatialInput_Frame);
+    memcpy(pDst, eye_ray,    sizeof(SpatialInput_Ray));   pDst += sizeof(SpatialInput_Ray);
+    memcpy(pDst, left_hand,  HAND_SIZE);                  pDst += HAND_SIZE;
+    memcpy(pDst, right_hand, HAND_SIZE);                  pDst += HAND_SIZE;
+
+    pBuffer->Unlock();
+
+    WriteBuffer(pBuffer, timestamp, 0, NULL);
+
+    pBuffer->Release();
+}
