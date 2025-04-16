e41e74c308a84e6ba001a7022a1fa65566e039f0
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-04-15 23:21:47 -0400

    Update encoder_si.h

--- hl2ss/hl2ss/encoder_si.h
+++ hl2ss/hl2ss/encoder_si.h
@@ -11,5 +11,5 @@ class Encoder_SI : public CustomEncoder
 public:
     Encoder_SI(HOOK_ENCODER_PROC pHookCallback, void* pHookParam);
 
-    void WriteSample(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, winrt::Windows::Perception::People::JointPose* left_hand, winrt::Windows::Perception::People::JointPose* right_hand, UINT64 timestamp);
+    void WriteSample(uint32_t valid, SpatialInput_Frame* head_pose, SpatialInput_Ray* eye_ray, winrt::Windows::Perception::People::JointPose* left_hand, winrt::Windows::Perception::People::JointPose* right_hand, LONGLONG timestamp);
 };
