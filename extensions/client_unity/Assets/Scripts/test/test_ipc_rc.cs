1fb7cc9f6527253ff0a456c49043dcb8e591f4bf
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-16 04:23:28 -0400

    Update test_ipc_rc.cs

--- extensions/client_unity/Assets/Scripts/test/test_ipc_rc.cs
+++ extensions/client_unity/Assets/Scripts/test/test_ipc_rc.cs
@@ -11,7 +11,7 @@ public class test_ipc_rc : MonoBehaviour
         hl2ss.svc.open_ipc(host, hl2ss.ipc_port.REMOTE_CONFIGURATION, out hl2ss.svc.ipc_rc ipc);
 
         hl2ss.version version = ipc.ee_get_application_version();
-        Debug.Log(string.Format("version {0}.{1}.{2}.{3}", version.field[0], version.field[1], version.field[2], version.field[3]));
+        Debug.Log(string.Format("version {0}.{1}.{2}.{3}", version.field_0, version.field_1, version.field_2, version.field_3));
         Debug.Log(string.Format("utf_offset: {0}", ipc.ts_get_utc_offset()));
         ipc.hs_set_marker_state(hl2ss.hs_marker_state.Disable);
         Debug.Log(string.Format("pv status: {0}", ipc.pv_get_subsystem_status()));
@@ -32,7 +32,7 @@ public class test_ipc_rc : MonoBehaviour
         ipc.pv_set_optical_image_stabilization(hl2ss.pv_optical_image_stabilization_mode.On);
         ipc.pv_set_hdr_video(hl2ss.pv_hdr_video_mode.Off);
         ipc.pv_set_regions_of_interest(true, true, true, true, true, hl2ss.pv_region_of_interest_type.Unknown, 100, 0.0f, 0.0f, 1.0f, 1.0f);
-        ipc.ee_set_interface_priority(hl2ss.stream_port.PERSONAL_VIDEO, hl2ss.ee_interface_priority.ABOVE_NORMAL);
+        ipc.ee_set_interface_priority(hl2ss.stream_port.PERSONAL_VIDEO, hl2ss.ee_interface_priority.NORMAL);
         ipc.ee_set_quiet_mode(false);
 
         ipc.Dispose();
