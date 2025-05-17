7f4cfb4ebbd510895d15971f124b9c7cfe50c375
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-17 00:52:07 -0400

    Update test_si.cs

--- extensions/client_unity/Assets/Scripts/test/test_si.cs
+++ extensions/client_unity/Assets/Scripts/test/test_si.cs
@@ -9,43 +9,33 @@ public class test_si : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string host = run_once.host_address;
-
         hl2ss.ulm.configuration_si configuration = new hl2ss.ulm.configuration_si();
 
-        hl2ss.svc.open_stream(host, hl2ss.stream_port.SPATIAL_INPUT, 300, configuration, true, out source_si);
+        hl2ss.svc.open_stream(run_once.host_address, hl2ss.stream_port.SPATIAL_INPUT, 300, configuration, true, out source_si);
     }
 
     // Update is called once per frame
     void Update()
     {
-        using (var packet = source_si.get_by_index(-1))
-        {
-            if (packet.status != 0) { return; }
+        using var packet = source_si.get_by_index(-1);
 
-            packet.unpack(out hl2ss.map_si region);
+        if (packet.status != 0) { return; }
 
-            var data = Marshal.PtrToStructure<hl2ss.si_frame>(region.tracking);
+        packet.unpack(out hl2ss.map_si region);
+        
+        var data = Marshal.PtrToStructure<hl2ss.si_frame>(region.tracking);
 
-            Debug.Log(string.Format("valid {0}", data.valid));
-            Debug.Log(string.Format("head_pose position [{0}, {1}, {2}]", data.head_pose.position.x, data.head_pose.position.y, data.head_pose.position.z));
-            Debug.Log(string.Format("head_pose up [{0}, {1}, {2}]", data.head_pose.up.x, data.head_pose.up.y, data.head_pose.up.z));
-            Debug.Log(string.Format("head_pose forward [{0}, {1}, {2}]", data.head_pose.forward.x, data.head_pose.forward.y, data.head_pose.forward.z));
-            Debug.Log(string.Format("eye_ray origin [{0}, {1}, {2}]", data.eye_ray.origin.x, data.eye_ray.origin.y, data.eye_ray.origin.z));
-            Debug.Log(string.Format("eye_ray direction [{0}, {1}, {2}]", data.eye_ray.direction.x, data.eye_ray.direction.y, data.eye_ray.direction.z));
+        Debug.Log(string.Format("valid {0}", data.valid));
+        Debug.Log(string.Format("head_pose position [{0}, {1}, {2}]", data.head_pose.position.x, data.head_pose.position.y, data.head_pose.position.z));
+        Debug.Log(string.Format("head_pose up [{0}, {1}, {2}]",       data.head_pose.up.x,       data.head_pose.up.y,       data.head_pose.up.z));
+        Debug.Log(string.Format("head_pose forward [{0}, {1}, {2}]",  data.head_pose.forward.x,  data.head_pose.forward.y,  data.head_pose.forward.z));
+        Debug.Log(string.Format("eye_ray origin [{0}, {1}, {2}]",     data.eye_ray.origin.x,     data.eye_ray.origin.y,     data.eye_ray.origin.z));
+        Debug.Log(string.Format("eye_ray direction [{0}, {1}, {2}]",  data.eye_ray.direction.x,  data.eye_ray.direction.y,  data.eye_ray.direction.z));
 
-            hl2ss.si_hand_joint left_wrist = data.left_hand[hl2ss.si_hand_joint_kind.Wrist];
-            hl2ss.si_hand_joint right_wrist = data.right_hand[hl2ss.si_hand_joint_kind.Wrist];
+        hl2ss.si_hand_joint l_wrist = data.left_hand[hl2ss.si_hand_joint_kind.Wrist];
+        hl2ss.si_hand_joint r_wrist = data.right_hand[hl2ss.si_hand_joint_kind.Wrist];
 
-            Debug.Log(string.Format("left_wrist position [{0}, {1}, {2}]", left_wrist.position.x, left_wrist.position.y, left_wrist.position.z));
-            Debug.Log(string.Format("right_wrist position [{0}, {1}, {2}]", right_wrist.position.x, right_wrist.position.y, right_wrist.position.z));
-        }
-    }
-
-    void OnApplicationQuit()
-    {
-        if (source_si == null) { return; }
-
-        source_si.Dispose();
+        Debug.Log(string.Format("l_wrist position [{0}, {1}, {2}] orientation [{3}, {4}, {5} {6}] radius {7} accuracy {8}", l_wrist.position.x, l_wrist.position.y, l_wrist.position.z, l_wrist.orientation.x, l_wrist.orientation.y, l_wrist.orientation.z, l_wrist.orientation.w, l_wrist.radius, l_wrist.accuracy));
+        Debug.Log(string.Format("r_wrist position [{0}, {1}, {2}] orientation [{3}, {4}, {5} {6}] radius {7} accuracy {8}", r_wrist.position.x, r_wrist.position.y, r_wrist.position.z, r_wrist.orientation.x, r_wrist.orientation.y, r_wrist.orientation.z, r_wrist.orientation.w, r_wrist.radius, r_wrist.accuracy));
     }
 }
