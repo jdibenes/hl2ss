002b8ba61cbc44a192082051b145847259594115
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-17 02:52:46 -0400

    Update test_ev.cs

--- extensions/client_unity/Assets/Scripts/test/test_ev.cs
+++ extensions/client_unity/Assets/Scripts/test/test_ev.cs
@@ -11,54 +11,28 @@ public class test_ev : MonoBehaviour
     public float profile_index = 4.0f;
 
     private string host;
-    private hl2ss.shared.source source_pv;
+    private hl2ss.shared.source source_ev;
     private int pv_frame_size;
     private Texture2D tex_pv;
+    TextureFormat texture_format;
+    int bpp;
 
     // Start is called before the first frame update
     void Start()
     {
         host = run_once.host_address;
 
-        hl2ss.ulm.configuration_pv configuration = new hl2ss.ulm.configuration_pv();
+        var configuration = new hl2ss.ulm.configuration_pv();
 
         configuration.width = 1280;
         configuration.height = 720;
         configuration.framerate = 30;
-        byte decoded_format = hl2ss.pv_decoded_format.RGB;
 
-        TextureFormat texture_format;
-        int bpp;
+        var decoded_format = hl2ss.pv_decoded_format.RGB;
+        texture_format = TextureFormat.RGB24;
+        bpp = 3;
 
-        switch (decoded_format)
-        {
-        case hl2ss.pv_decoded_format.RGB:
-            texture_format = TextureFormat.RGB24;
-            bpp = 3;
-            break;
-        case hl2ss.pv_decoded_format.BGR:
-            texture_format = TextureFormat.RGB24; // no BGR24 ?
-            bpp = 3;
-            break;
-        case hl2ss.pv_decoded_format.RGBA:
-            texture_format = TextureFormat.RGBA32;
-            bpp = 4;
-            break;
-        case hl2ss.pv_decoded_format.BGRA:
-            texture_format = TextureFormat.BGRA32;
-            bpp = 4;
-            break;
-        case hl2ss.pv_decoded_format.GRAY:
-            texture_format = TextureFormat.R8;
-            bpp = 1;
-            break;
-        default:
-            throw new System.Exception("Unsupported decoded format");
-        }
-
-        pv_frame_size = configuration.width * configuration.height * bpp;
-
-        hl2ss.ulm.configuration_pv_subsystem configuration_subsystem = new hl2ss.ulm.configuration_pv_subsystem();
+        var configuration_subsystem = new hl2ss.ulm.configuration_pv_subsystem();
 
         configuration_subsystem.global_opacity = group_index;
         configuration_subsystem.output_width   = source_index;
@@ -66,38 +40,50 @@ public class test_ev : MonoBehaviour
 
         hl2ss.svc.start_subsystem_pv(host, hl2ss.stream_port.EXTENDED_VIDEO, configuration_subsystem);
 
-        using (var device_list_handle = hl2ss.svc.download_device_list(host, hl2ss.stream_port.EXTENDED_VIDEO, configuration))
-        {
-            var string_bytes = new byte[device_list_handle.size];
-            Marshal.Copy(device_list_handle.data, string_bytes, 0, (int)device_list_handle.size);
-            Debug.Log(Encoding.Unicode.GetString(string_bytes));
-        }
+        using var device_list_handle = hl2ss.svc.download_device_list(host, hl2ss.stream_port.EXTENDED_VIDEO, configuration);
+        var string_bytes = new byte[device_list_handle.size];
+        Marshal.Copy(device_list_handle.data, string_bytes, 0, (int)device_list_handle.size);
+        Debug.Log(Encoding.Unicode.GetString(string_bytes));
 
-        hl2ss.svc.open_stream(host, hl2ss.stream_port.EXTENDED_VIDEO, 300, configuration, decoded_format, out source_pv);
-
-        tex_pv = new Texture2D(configuration.width, configuration.height, texture_format, false);
-
-        quad_pv.GetComponent<Renderer>().material.mainTexture = tex_pv;
+        hl2ss.svc.open_stream(host, hl2ss.stream_port.EXTENDED_VIDEO, 300, configuration, decoded_format, out source_ev);
     }
 
     // Update is called once per frame
     void Update()
     {
-        using (var packet = source_pv.get_by_index(-1))
-        {
-            if (packet.status != 0) { return; }
-            packet.unpack(out hl2ss.map_pv region);
+        using var packet = source_ev.get_by_index(-1);
 
-            tex_pv.LoadRawTextureData(region.image, pv_frame_size);
-            tex_pv.Apply();
+        if (packet.status != hl2ss.mt.status.OK) { return; }
+
+        packet.unpack(out hl2ss.map_pv region);
+
+        var metadata = Marshal.PtrToStructure<hl2ss.pv_metadata>(region.metadata);
+        var pose = Marshal.PtrToStructure<hl2ss.matrix_4x4>(packet.pose);
+
+        Debug.Log(string.Format("timestamp {0}", packet.timestamp));
+        Debug.Log(string.Format("pose [{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}]", pose.m_00, pose.m_01, pose.m_02, pose.m_03, pose.m_10, pose.m_11, pose.m_12, pose.m_13, pose.m_20, pose.m_21, pose.m_22, pose.m_23, pose.m_30, pose.m_31, pose.m_32, pose.m_33));
+        
+        Debug.Log(string.Format("resolution {0}x{1}", metadata.width, metadata.height));
+
+        if (!tex_pv)
+        {
+            var width = metadata.width;
+            var height = metadata.height;
+
+            pv_frame_size = width * height * bpp;
+            tex_pv = new Texture2D(width, height, texture_format, false);
+            quad_pv.GetComponent<Renderer>().material.mainTexture = tex_pv;
         }
+
+        tex_pv.LoadRawTextureData(region.image, pv_frame_size);
+        tex_pv.Apply();
     }
 
     void OnApplicationQuit()
     {
-        if (source_pv == null) { return; }
+        if (source_ev == null) { return; }
 
-        source_pv.Dispose();
+        source_ev.Dispose();
         hl2ss.svc.stop_subsystem_pv(host, hl2ss.stream_port.EXTENDED_VIDEO);
     }
 }
