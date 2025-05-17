3e6a311463cc827d6c5e182dbf006113775010c1
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-17 00:42:42 -0400

    Update test_eet.cs

--- extensions/client_unity/Assets/Scripts/test/test_eet.cs
+++ extensions/client_unity/Assets/Scripts/test/test_eet.cs
@@ -9,44 +9,34 @@ public class test_eet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string host = run_once.host_address;
-
         hl2ss.ulm.configuration_eet configuration = new hl2ss.ulm.configuration_eet();
 
         configuration.fps = 90;
 
-        hl2ss.svc.open_stream(host, hl2ss.stream_port.EXTENDED_EYE_TRACKER, 900, configuration, true, out source_eet);
+        hl2ss.svc.open_stream(run_once.host_address, hl2ss.stream_port.EXTENDED_EYE_TRACKER, 900, configuration, true, out source_eet);
     }
 
     // Update is called once per frame
     void Update()
     {
-        using (var packet = source_eet.get_by_index(-1))
-        {
-            if (packet.status != 0) { return; }
+        using var packet = source_eet.get_by_index(-1);
 
-            packet.unpack(out hl2ss.map_eet region);
+        if (packet.status != hl2ss.mt.status.OK) { return; }
 
-            var pose = Marshal.PtrToStructure<hl2ss.matrix_4x4>(packet.pose);
-            var data = Marshal.PtrToStructure<hl2ss.eet_frame>(region.tracking);
+        packet.unpack(out hl2ss.map_eet region);
 
-            Debug.Log(string.Format("valid {0}", data.valid));
+        var pose = Marshal.PtrToStructure<hl2ss.matrix_4x4>(packet.pose);
+        var data = Marshal.PtrToStructure<hl2ss.eet_frame>(region.tracking);
 
-            Debug.Log(string.Format("combined_ray origin [{0}, {1}, {2}]", data.combined_ray.origin.x, data.combined_ray.origin.y, data.combined_ray.origin.z));
-            Debug.Log(string.Format("combined_ray direction [{0}, {1}, {2}]", data.combined_ray.direction.x, data.combined_ray.direction.y, data.combined_ray.direction.z));
+        Debug.Log(string.Format("valid {0}", data.valid));
 
-            Debug.Log(string.Format("left_ray origin [{0}, {1}, {2}]", data.left_ray.origin.x, data.left_ray.origin.y, data.left_ray.origin.z));
-            Debug.Log(string.Format("left_ray direction [{0}, {1}, {2}]", data.left_ray.direction.x, data.left_ray.direction.y, data.left_ray.direction.z));
+        Debug.Log(string.Format("combined_ray origin [{0}, {1}, {2}]",    data.combined_ray.origin.x,    data.combined_ray.origin.y,    data.combined_ray.origin.z));
+        Debug.Log(string.Format("combined_ray direction [{0}, {1}, {2}]", data.combined_ray.direction.x, data.combined_ray.direction.y, data.combined_ray.direction.z));
 
-            Debug.Log(string.Format("right_ray origin [{0}, {1}, {2}]", data.right_ray.origin.x, data.right_ray.origin.y, data.right_ray.origin.z));
-            Debug.Log(string.Format("right_ray direction [{0}, {1}, {2}]", data.right_ray.direction.x, data.right_ray.direction.y, data.right_ray.direction.z));
-        }
-    }
+        Debug.Log(string.Format("left_ray origin [{0}, {1}, {2}]",    data.left_ray.origin.x,   data.left_ray.origin.y,     data.left_ray.origin.z));
+        Debug.Log(string.Format("left_ray direction [{0}, {1}, {2}]", data.left_ray.direction.x, data.left_ray.direction.y, data.left_ray.direction.z));
 
-    void OnApplicationQuit()
-    {
-        if (source_eet == null) { return; }
-
-        source_eet.Dispose();
+        Debug.Log(string.Format("right_ray origin [{0}, {1}, {2}]",    data.right_ray.origin.x,    data.right_ray.origin.y,    data.right_ray.origin.z));
+        Debug.Log(string.Format("right_ray direction [{0}, {1}, {2}]", data.right_ray.direction.x, data.right_ray.direction.y, data.right_ray.direction.z));
     }
 }
