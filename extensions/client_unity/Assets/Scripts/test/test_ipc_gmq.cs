045fad18d417c49b25c888e12750aca41d56ad3b
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-17 03:12:26 -0400

    Update test_ipc_gmq.cs

--- extensions/client_unity/Assets/Scripts/test/test_ipc_gmq.cs
+++ extensions/client_unity/Assets/Scripts/test/test_ipc_gmq.cs
@@ -9,25 +9,22 @@ public class test_ipc_gmq : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string host = run_once.host_address;
+        hl2ss.svc.open_ipc(run_once.host_address, hl2ss.ipc_port.GUEST_MESSAGE_QUEUE, out hl2ss.shared.ipc_gmq ipc);
 
-        hl2ss.svc.open_ipc(host, hl2ss.ipc_port.GUEST_MESSAGE_QUEUE, out hl2ss.shared.ipc_gmq ipc);
+        var response = new uint[1] { 1 };
 
-        uint[] response = new uint[1] { 1 };
+        using var msg = ipc.pull();
 
-        using (var msg = ipc.pull())
+        Debug.Log(string.Format("command id: {0} size: {1}", msg.command, msg.size));
+
+        if ((msg.command == 0xFFFFFFFE) && (msg.size > 0))
         {
-            Debug.Log(string.Format("command id: {0} size: {1}", msg.command, msg.size));
-            if ((msg.command == 0xFFFFFFFE) && (msg.size > 0))
-            {
-                byte[] data = new byte[msg.size];
-                Marshal.Copy(msg.data, data, 0, (int)msg.size);
-                Debug.Log(Encoding.UTF8.GetString(data));
-            }
-
-            ipc.push(response, (uint)response.Length);
+            byte[] data = new byte[msg.size];
+            Marshal.Copy(msg.data, data, 0, (int)msg.size);
+            Debug.Log(Encoding.UTF8.GetString(data));
         }
 
+        ipc.push(response, (uint)response.Length);
         ipc.Dispose();
     }
 
