be92da70d84abe05229e9036846415e415dc9a43
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-18 02:44:52 -0400

    Update extended_audio_raw_configuration.m

--- extensions/client_matlab/+hl2ss/extended_audio_raw_configuration.m
+++ extensions/client_matlab/+hl2ss/extended_audio_raw_configuration.m
@@ -1,6 +1,6 @@
 
 function [mode] = extended_audio_raw_configuration(media_category, shared, audio_raw, disable_effect, enable_passthrough)
-b0_2 = bitshift(bitand(uint8(media_category), 7), 0);
+b0_2 = bitshift(bitand(uint8(media_category), uint8(7)), 0);
 b3_3 = bitshift(uint8(shared), 3);
 b4_4 = bitshift(uint8(0), 4);
 b5_5 = bitshift(uint8(audio_raw), 5);
