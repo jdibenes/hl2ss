ab6faf4723488ca5e0208149eec9832d740f6a59
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-18 01:48:23 -0400

    Create extended_audio_raw_configuration.m

---
+++ extensions/client_matlab/+hl2ss/extended_audio_raw_configuration.m
@@ -0,0 +1,11 @@
+
+function [mode] = extended_audio_raw_configuration(media_category, shared, audio_raw, disable_effect, enable_passthrough)
+b0_2 = bitshift(bitand(uint8(media_category), 7), 0);
+b3_3 = bitshift(uint8(shared), 3);
+b4_4 = bitshift(uint8(0), 4);
+b5_5 = bitshift(uint8(audio_raw), 5);
+b6_6 = bitshift(uint8(disable_effect), 6);
+b7_7 = bitshift(uint8(enable_passthrough), 7);
+
+mode = bitor(b7_7, bitor(b6_6, bitor(b5_5, bitor(b4_4, bitor(b3_3, b0_2)))));
+end
