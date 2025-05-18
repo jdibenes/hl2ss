9685b9c11d8613d74e0a8dddca1a6356e2261120
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-18 01:48:43 -0400

    Create time_base.m

---
+++ extensions/client_matlab/+hl2ss/time_base.m
@@ -0,0 +1,8 @@
+
+classdef time_base
+properties (Constant)
+    HUNDREDS_OF_NANOSECONDS = uint64(10000000);
+    FILETIME_EPOCH          = int64(0);
+    UNIX_EPOCH              = int64(116444736000000000);
+end
+end
