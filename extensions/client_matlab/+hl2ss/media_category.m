e0cf488039832b364ecb7f690baef51d3e32e7aa
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-18 01:48:32 -0400

    Create media_category.m

---
+++ extensions/client_matlab/+hl2ss/media_category.m
@@ -0,0 +1,10 @@
+
+classdef media_category
+properties (Constant)
+    Other          = uint8(0);
+    Communications = uint8(1);
+    Media          = uint8(2);
+    GameChat       = uint8(3);
+    Speech         = uint8(4);
+end
+end
