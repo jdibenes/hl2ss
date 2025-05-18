e836d8377e603aef764a10c23b33c55c0f2e105f
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-05-18 01:48:21 -0400

    Update extended_audio_device_mixer_mode.m

--- extensions/client_matlab/+hl2ss/extended_audio_device_mixer_mode.m
+++ extensions/client_matlab/+hl2ss/extended_audio_device_mixer_mode.m
@@ -1,5 +1,8 @@
 
-function [opmode] = extended_audio_device_mixer_mode(mixer_mode, device)
-DEVICE_BASE = uint32(0x00000004);
-opmode = uint32(mixer_mode) + (DEVICE_BASE * uint32(device + 1));
+function [opmode] = extended_audio_device_mixer_mode(mixer_mode, device_index, source_index, format_index)
+di = bitshift(bitand(uint32(device_index + 1), 0x3FF),  2);
+ds = bitshift(bitand(uint32(source_index),     0x3FF), 12);
+df = bitshift(bitand(uint32(format_index),     0x3FF), 22);
+
+opmode = bitor(uint32(mixer_mode), bitor(di, bitor(ds, df)));
 end
